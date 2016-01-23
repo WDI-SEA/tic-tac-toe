a09d2a17dbe6a221ad6147dcf58c01a04a4d8048
Scott <scott.heron@outlook.com>
2016-01-23 15:32:04 -0800

    working game with out array

--- script/script.js
+++ script/script.js
@@ -1,34 +1,197 @@
 
 var squareArray = [];
-squareArray[1] = document.getElementById('1');
-var square2 = document.getElementById('2');
-var square3 = document.getElementById('3');
-var square4 = document.getElementById('4');
-var square5 = document.getElementById('5');
-var square6 = document.getElementById('6');
-var square7 = document.getElementById('7');
-var square8 = document.getElementById('8');
-var square9 = document.getElementById('9');
-console.log(squareArray[1]);
+squareArray[0] = document.getElementById('1');
+squareArray[1] = document.getElementById('2');
+squareArray[2] = document.getElementById('3');
+squareArray[3] = document.getElementById('4');
+squareArray[4] = document.getElementById('5');
+squareArray[5] = document.getElementById('6');
+squareArray[6] = document.getElementById('7');
+squareArray[7] = document.getElementById('8');
+squareArray[8] = document.getElementById('9');
+
 
 var xOrO = true;
 
+while ()
 
+	console.log(squareArray[0]);
+	squareArray[0].addEventListener('click', function(){
+		
+		if (squareArray[0].value === ''){
 
-squareArray[1].addEventListener('click', function(){
-	if (squareArray[1].value === ''){
-
-		if (xOrO === true){
-			squareArray[1].value = 'X';
-			xOrO = false;	
+			if (xOrO === true){
+				squareArray[0].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[0].value = 'O';
+				xOrO = true;
+			}
 		}
-		else if(xOrO === false){
-			squareArray[1].value = 'O';
-			xOrO = true;
+		else {
+			window.alert("Please choose an empty space :D");
 		}
-	}
-	else {
-		window.alert("Please choose an empty space :D");
-	}
+		
 
-})
+	});
+
+	squareArray[1].addEventListener('click', function(){
+		
+		if (squareArray[1].value === ''){
+
+			if (xOrO === true){
+				squareArray[1].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[1].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[2].addEventListener('click', function(){
+		
+		if (squareArray[2].value === ''){
+
+			if (xOrO === true){
+				squareArray[2].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[2].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[3].addEventListener('click', function(){
+		
+		if (squareArray[3].value === ''){
+
+			if (xOrO === true){
+				squareArray[3].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[3].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[4].addEventListener('click', function(){
+		
+		if (squareArray[4].value === ''){
+
+			if (xOrO === true){
+				squareArray[4].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[4].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[5].addEventListener('click', function(){
+		
+		if (squareArray[5].value === ''){
+
+			if (xOrO === true){
+				squareArray[5].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[5].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[6].addEventListener('click', function(){
+		
+		if (squareArray[6].value === ''){
+
+			if (xOrO === true){
+				squareArray[6].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[6].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[7].addEventListener('click', function(){
+		
+		if (squareArray[7].value === ''){
+
+			if (xOrO === true){
+				squareArray[7].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[7].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
+
+	squareArray[8].addEventListener('click', function(){
+		
+		if (squareArray[8].value === ''){
+
+			if (xOrO === true){
+				squareArray[8].value = 'X';
+				xOrO = false;	
+			}
+			else if(xOrO === false){
+				squareArray[8].value = 'O';
+				xOrO = true;
+			}
+		}
+		else {
+			window.alert("Please choose an empty space :D");
+		}
+		
+
+	});
