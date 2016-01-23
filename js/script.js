549c342966f67ec4b5fbec21bac6a311629971d5
evwilkin <evan.wilkinson@gmail.com>
2016-01-23 14:51:22 -0800

    added functions to add X/O and color to tiles when clicked, or alert if tile is already taken

--- js/script.js
+++ js/script.js
@@ -16,18 +16,140 @@ var hello = function() {
 }
 
 //Add Event Listeners
-topLeftTile.addEventListener("click", function) {
-	clickCount += 1;
-	if (topLeftTile.class = "blank") {
+topLeftTile.addEventListener("click", function() {
+	if (topLeftTile.className === "blank") {
+		clickCount += 1;
 		if (clickCount % 2) {
-			topLeftTile.backgroundColor = "red";
+			topLeftTile.className = "red";
 			topLeftTile.innerHTML = "X";
 		} else {
-			topLeftTile.backgroundColor = "green";
-			topLeftTile.
+			topLeftTile.className = "green";
+			topLeftTile.innerHTML = "O";
 		}
+	} else {
+		alert("That tile is already taken, please select another");
 	}
-}
+})
+
+topCenterTile.addEventListener("click", function() {
+	if (topCenterTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			topCenterTile.className = "red";
+			topCenterTile.innerHTML = "X";
+		} else {
+			topCenterTile.className = "green";
+			topCenterTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+topRightTile.addEventListener("click", function() {
+	if (topRightTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			topRightTile.className = "red";
+			topRightTile.innerHTML = "X";
+		} else {
+			topRightTile.className = "green";
+			topRightTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+middleLeftTile.addEventListener("click", function() {
+	if (middleLeftTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			middleLeftTile.className = "red";
+			middleLeftTile.innerHTML = "X";
+		} else {
+			middleLeftTile.className = "green";
+			middleLeftTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+middleCenterTile.addEventListener("click", function() {
+	if (middleCenterTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			middleCenterTile.className = "red";
+			middleCenterTile.innerHTML = "X";
+		} else {
+			middleCenterTile.className = "green";
+			middleCenterTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+middleRightTile.addEventListener("click", function() {
+	if (middleRightTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			middleRightTile.className = "red";
+			middleRightTile.innerHTML = "X";
+		} else {
+			middleRightTile.className = "green";
+			middleRightTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+bottomLeftTile.addEventListener("click", function() {
+	if (bottomLeftTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			bottomLeftTile.className = "red";
+			bottomLeftTile.innerHTML = "X";
+		} else {
+			bottomLeftTile.className = "green";
+			bottomLeftTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+bottomCenterTile.addEventListener("click", function() {
+	if (bottomCenterTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			bottomCenterTile.className = "red";
+			bottomCenterTile.innerHTML = "X";
+		} else {
+			bottomCenterTile.className = "green";
+			bottomCenterTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
+
+bottomRightTile.addEventListener("click", function() {
+	if (bottomRightTile.className === "blank") {
+		clickCount += 1;
+		if (clickCount % 2) {
+			bottomRightTile.className = "red";
+			bottomRightTile.innerHTML = "X";
+		} else {
+			bottomRightTile.className = "green";
+			bottomRightTile.innerHTML = "O";
+		}
+	} else {
+		alert("That tile is already taken, please select another");
+	}
+})
 
 
 
