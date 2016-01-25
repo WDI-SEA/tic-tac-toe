d2357afd44e9e3ca9c0b77259ed84fce9dbf6a87
katiosity <lovan.kathy@gmail.com>
2016-01-24 19:11:55 -0800

    renamed files

--- tictactoe.js
+++ tictactoe.js
@@ -1,154 +1,139 @@
-var x = "X";
-var o = "O";
-var player = 1;
-var cell = document.getElementsByTagName('input');
-// console.log(getCells);
+var count = 0;
 
-// play cell 1
-cell[0].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[0].value = x;
-		cell[0].style.backgroundColor = "green";
-		cell[0].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[0].value = o;
-		cell[0].style.backgroundColor = "yellow";
-		cell[0].style.color = "green";
-		player -= 1;
-	}
-} );
+var box1 = document.querySelector("#tile1");
+var box2 = document.querySelector("#tile2");
+var box3 = document.querySelector("#tile3");
+var box4 = document.querySelector("#tile4");
+var box5 = document.querySelector("#tile5");
+var box6 = document.querySelector("#tile6");
+var box7 = document.querySelector("#tile7");
+var box8 = document.querySelector("#tile8");
+var box9 = document.querySelector("#tile9");
 
-// // play cell 2
-cell[1].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[1].value = x;
-		cell[1].style.backgroundColor = "green";
-		cell[1].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[1].value = o;
-		cell[1].style.backgroundColor = "yellow";
-		cell[1].style.color = "green";
-		player -= 1;
-	}
-} );
+function click(box) {
+	box.addEventListener("click", function() {
+		if (box.innerHTML === "") {
+			
+			if (count % 2 ===0) {
 
-// // play cell 3
-cell[2].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[2].value = x;
-		cell[2].style.backgroundColor = "green";
-		cell[2].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[2].value = o;
-		cell[2].style.backgroundColor = "yellow";
-		cell[2].style.color = "green";
-		player -= 1;
-	}
-} );
+				box.style.background = "yellow";
+				box.style.color = "green";
+				box.innerHTML = "X";
+				count++;
+			} else {
+				box.style.background = "green";
+				box.style.color = "yellow";
+				box.innerHTML = "O";
+				count++;
+			}
+		}
+	})
+}
 
-// // play cell 4
-cell[3].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[3].value = x;
-		cell[3].style.backgroundColor = "green";
-		cell[3].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[3].value = o;
-		cell[3].style.backgroundColor = "yellow";
-		cell[3].style.color = "green";
-		player -= 1;
-	}
-} );
-
-// // play cell 5
-cell[4].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[4].value = x;
-		cell[4].style.backgroundColor = "green";
-		cell[4].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[4].value = o;
-		cell[4].style.backgroundColor = "yellow";
-		cell[4].style.color = "green";
-		player -= 1;
-	}
-} );
-
-// // play cell 6
-cell[5].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[5].value = x;
-		cell[5].style.backgroundColor = "green";
-		cell[5].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[5].value = o;
-		cell[5].style.backgroundColor = "yellow";
-		cell[5].style.color = "green";
-		player -= 1;
-	}
-} );
-
-// // play cell 7
-cell[6].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[6].value = x;
-		cell[6].style.backgroundColor = "green";
-		cell[6].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[6].value = o;
-		cell[6].style.backgroundColor = "yellow";
-		cell[6].style.color = "green";
-		player -= 1;
-	}
-} );
-
-// // play cell 8
-cell[7].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[7].value = x;
-		cell[7].style.backgroundColor = "green";
-		cell[7].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[7].value = o;
-		cell[7].style.backgroundColor = "yellow";
-		cell[7].style.color = "green";
-		player -= 1;
-	}
-} );
-
-// // play cell 9
-cell[8].addEventListener('click', function (event) {
-	event.preventDefault();
-	if (player == 1) {
-		cell[8].value = x;
-		cell[8].style.backgroundColor = "green";
-		cell[8].style.color = "yellow";
-		player += 1;
-	} else {
-		cell[8].value = o;
-		cell[8].style.backgroundColor = "yellow";
-		cell[8].style.color = "green";
-		player -= 1;
-	}
-} );
+click(box1);
+click(box2);
+click(box3);
+click(box4);
+click(box5);
+click(box6);
+click(box7);
+click(box8);
+click(box9);
 
 // clear cells when reset button clicked
-document.getElementById('reset').reset();
+var tiles = document.querySelectorAll('.tile');
+
+document.getElementById('reset').addEventListener ('click', function (event) {
+	event.preventDefault();
+	for (var i = 0; i < tiles.length; i++) {
+		tiles[i].innerHTML = "";
+		tiles[i].style.backgroundColor = "#eeeeee";
+	}
+} );
+
+// document.getElementById('tile2').innerHTML = "?";
+
+// document.getElementById('tile2').addEventListener('click', function (event) {
+// 	event.preventDefault();
+// 	if (turn == 1) {
+// 		document.getElementById('tile2').innerHTML = "X";
+// 		document.getElementById('tile2').style.backgroundColor = "green";
+// 		document.getElementById('tile2').style.color = "yellow";
+// 		turn += 1;
+// 	} else {
+// 		document.getElementById('tile2').innerHTML = "O";
+// 		document.getElementById('tile2').style.backgroundColor = "yellow";
+// 		document.getElementById('tile2').style.color = "green";
+// 		turn -= 1;
+// 	}
+// } );
+// do not delete above this line
 
 
+/*
+I tried looping it all below but kept getting this console error:
+"Uncaught TypeError: Cannot set property 'innerHTML' of undefined"
+*/
+
+
+// var tiles = document.querySelectorAll('div');
+// var turn = 1;
+
+// for (i = 0; i < tiles.length; i++) {
+// 	tiles[i].addEventListener('click', function (event) {
+// 	event.preventDefault();
+// 	if (turn == 1) {
+// 		tiles[i].innerHTML = "X";
+// 		tiles[i].style.backgroundColor = "green";
+// 		tiles[i].style.color = "yellow";
+// 		turn += 1;
+// 	} else {
+// 		tiles[i].innerHTML = "O";
+// 		tiles[i].style.backgroundColor = "yellow";
+// 		tiles[i].style.color = "green";
+// 		turn -= 1;
+// 	}
+// } );
+// }
+
+// this works
+
+// document.getElementById('tile1').innerHTML = "?";
+
+// var turn = 1
+
+
+// var tiles = document.getElementsByClassName('tile');
+// console.log(tiles);
+
+
+// tiles.addEventListener('click', function (event) {
+// 	event.preventDefault();
+// 	if (turn == 1) {
+// 		document.getElementById('tile1').innerHTML = "X";
+// 		document.getElementById('tile1').style.backgroundColor = "green";
+// 		document.getElementById('tile1').style.color = "yellow";
+// 		turn += 1;
+// 	} else {
+// 		document.getElementById('tile1').innerHTML = "O";
+// 		document.getElementById('tile1').style.backgroundColor = "yellow";
+// 		document.getElementById('tile1').style.color = "green";
+// 		turn -= 1;
+// 	}
+// });
+
+// var wasClicked = function(i){
+// 	if (turn == 1) {
+// 		document.getElementById('tile'+i).innerHTML = "X";
+// 		document.getElementById('tile'+i).style.backgroundColor = "green";
+// 		document.getElementById('tile'+i).style.color = "yellow";
+// 		console.log("tile"+i+"was clicked");
+// 		turn += 1;
+// 	} else {
+// 		document.getElementById('tile'+i).innerHTML = "O";
+// 		document.getElementById('tile'+i).style.backgroundColor = "yellow";
+// 		document.getElementById('tile'+i).style.color = "green";
+// 		console.log("tile"+i+"was clicked");
+
+// 		turn -= 1;
+// 	
