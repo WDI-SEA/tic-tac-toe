04e89ff8f6f33fbb842156d491063dee94633203
cassicakes <aguiar.cas@gmail.com>
2016-01-25 08:08:27 -0800

    more winning options added

--- js/script.js
+++ js/script.js
@@ -152,6 +152,27 @@ function testForWin(){
 	if(squares[0]===squares[1]&&squares[1]===squares[2]){
 		thereIsAWinner= true;
 	}
+	if (squares[3]===squares[4]&&squares[4]===squares[5]){
+		thereIsAWinner=== true;
+	}
+	if(squares[6]===squares[7]&&squares[7]===squares[8]){
+		thereIsAWinner= true;
+	}
+	if (squares[0]===squares[3]&&squares[3]===squares[6]){
+		thereIsAWinner=== true;
+	}
+	if(squares[1]===squares[4]&&squares[4]===squares[7]){
+		thereIsAWinner= true;
+	}
+	if (squares[2]===squares[5]&&squares[5]===squares[8]){
+		thereIsAWinner=== true;
+	}
+	if(squares[0]===squares[4]&&squares[4]===squares[8]){
+		thereIsAWinner= true;
+	}
+	if (squares[2]===squares[4]&&squares[4]===squares[6]){
+		thereIsAWinner=== true;
+	}
 	if (thereIsAWinner){
 		if (turn==="X"){
 			console.log("Winner is X");
