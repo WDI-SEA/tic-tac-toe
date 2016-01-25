4f5aa8d98b9cd6f2e1b8de873d80774ad2fd06ab
navyayvan <navyayvan@gmail.com>
2016-01-25 09:25:09 -0800

    look at this version, includes more questions

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ var resetGame = function () {
 
 var clickCell = function() {
 	
-	if (document.getElementsByClassName("cell").className.indexOf("playedSquare") === -1 ) { //checks to see if square is unplayed 
+	if (document.getElementsByClassName("cell").className.indexOf("playedSquare") === -1 ) { //checks to see if square is unplayed
+
+	//do I have to declare the class of playedSquare somewhere?! 
 		
 		if (playerOne === true) {
 			document.getElementsByClassName("cell").className += " x"; //asigns value of x (powderblue)
@@ -33,5 +35,14 @@ document.getElementById('reset').addEventListener('click',function(){resetGame()
  
 
 document.getElementById("1").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("2").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("3").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("4").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("5").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("6").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("7").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("8").addEventListener('click',function(){clickCell}); //listens for click on board
+document.getElementById("9").addEventListener('click',function(){clickCell}); //listens for click on board
 
+// is there a more efficient way to write all this? can you write "1 2 3 .. 9 "in one parantheses ? 
 
