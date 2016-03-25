8f52724ed29b2eab673ca571ecce13e2e4700053
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-25 16:59:57 -0700

    Add reset button and create turn assignment function

--- script.js
+++ script.js
@@ -2,47 +2,49 @@ var container = document.getElementById('container');
 var squares = document.querySelectorAll('.square')
 var player = document.querySelector("#player");
 var turn = "X";
+var reset = document.querySelector('#reset');
 
-
+var setTurn = function(x) {
+	turn = x;
+    player.innerHTML = x;
+};
 
 // var checkForWin = function(player, square) {
 
 // };
 
-for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseover', function(e) {
-    	e.target.style.backgroundColor = "#7D7E82";
-    });
-    squares[i].addEventListener('mouseout', function(e){
-    	e.target.style.backgroundColor = "#C2BDB9";
-    })
-}
+reset.addEventListener('click', function(e) {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].innerHTML = "";
+		setTurn("X");
+	};
+});
 
 for (var i = 0; i < squares.length; i++) {
+	 squares[i].addEventListener('mouseover', function(e) {
+    	e.target.classList.add('hover-gray');
+    });
+    squares[i].addEventListener('mouseout', function(e){
+    	e.target.classList.remove('hover-gray');
+    })
     squares[i].addEventListener('click', function(e) {
     	console.log(e.target.innerHTML);
     	if (e.target.innerHTML === "") {
     		e.target.innerHTML = turn;
     		e.target.classList.add('played');
     		if (turn === "X") {
-    			turn = "O";
-    			player.innerHTML = "O";
+    			setTurn("O");
     		} else {
-    			turn = "X"
-    			player.innerHTML = "X";
+    			setTurn("X");
     		};
     	} else {
     		e.target.classList.add('red');
-    		setTimeout(function(){ e.target.classList.remove('red'); }, 500);
+    		setTimeout(function(){ e.target.classList.remove('red'); }, 250);
     	}
     	console.log(e.innerHTML);
     	//console.log(document.getElementsByClassName('played'));
 	})
 };
-
-
-
-
 
 
 
