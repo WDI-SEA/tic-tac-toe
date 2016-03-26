2ba2ae5f8ccd3d6d5afe6eb8d84e00c5fba0f1de
Hannah Barnes <hlmbarnes@me.com>
2016-03-26 16:50:43 -0700

    just altered the click event to change background image for box_1 to an x

--- js/style.js
+++ js/style.js
@@ -3,7 +3,8 @@ var clearGame = function (){
 }
 
 var playMove = function (){
-   document.getElementById('box_1').style.backgroundColor = 'black';
+   document.getElementById('box_1').style.backgroundImage='url(../images/x.png)';
+
   }
 
 document.getElementById("box_1").addEventListener("click", function() {playMove()});
