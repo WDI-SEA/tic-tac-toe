a85fbca7c2c9008f99395abc8de834267775a276
Hannah Barnes <hlmbarnes@me.com>
2016-03-26 16:29:24 -0700

    added clickEvent in js for background color to change on box_1

--- js/style.js
+++ js/style.js
@@ -1,9 +1,12 @@
 var clearGame = function (){
 	alert("test");
-
 }
 
+var playMove = function (){
+   document.getElementById('box_1').style.backgroundColor = 'black';
+  }
 
+document.getElementById("box_1").addEventListener("click", function() {playMove()});
 
 
 document.getElementById("Clear").addEventListener("click", function() {clearGame()});
