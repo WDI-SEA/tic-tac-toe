25ce935faafab7b84884744995ef9bc823820de5
montehoover <monte.b.hoover@gmail.com>
2016-03-26 15:36:20 -0700

    Added clear button.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var squares = document.getElementsByClassName('board-squares');
+var button = document.getElementById('clear-button');
+
 for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function(e) {
-        e.target.innerHTML = '<img class="x-o-markers" src="o.png" alt="o">';
+        e.target.innerHTML = '<img class="x-o-markers" src="x.png" alt="o">';
     });
 };
+
+button.addEventListener('click', function(e) {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].innerHTML = '';
+    }
+});
