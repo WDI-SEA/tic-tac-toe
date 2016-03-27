2a32855fe2cf402b26110fa36f3913f31b7cceea
Julia Hensley <jh.julia@gmail.com>
2016-03-26 19:19:21 -0700

    et up index.html, script.js, style.css files; created background color and box

---
+++ script.js
@@ -0,0 +1,10 @@
+// // var container = document.getElementById('container')
+// var playerX = "X";
+// var playerO = "O";
+
+
+// // var playermove = function {if }
+
+// var squares = function(){
+	
+// }
