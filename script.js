b14e7c875224047f614f26104820ee7e42effbda
Julia Hensley <jh.julia@gmail.com>
2016-03-27 13:09:11 -0700

    attempted to add click > return X/O function in JS, doesn't work

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-// var container = document.getElementById('container');
+var container = document.getElementById('container');
 
-// var playerX = "X";
-// var playerO = "O";
+var cell = document.getElementById('cells')
 
-// var playermove = function {if }
+var cellsArray = ['cell1', 'cell2' 'cell3', 'cell4', 'cell5', 'cell6', 'cell7', 'cell8', 'cell9' ];
 
-// }
+cell = function() {
+	if (addEventListener.'click'.cellsArray !% 2){
+		console.log = "X";
+	else if (addEventListener.'click'.cellsArray % 2)
+		console.log  = "O";
+	else console.log  = "X";
+	}
+}
 
 
 
 
 
 
+// A user should be able to click on different squares to make a move.
 
+// Every click will alternate between marking an X and O
 
+// Upon marking of an individual cell, use javascript to add a class to each cell to display separate colors.
 
-// var gameboard = function(){
-// 	if (document.querySelectorAll('div') * squares === 9) {
-// 		return;
-// 	}
+// A cell should not be able to be replayed once marked.
+
+// Add a reset button that will clear the contents of the board. -->
