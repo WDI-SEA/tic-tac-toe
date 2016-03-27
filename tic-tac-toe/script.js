7c2cc20e0f9261fdb22499d49eea08418ea1ed75
keidra <keidrabaldwin@gmail.com>
2016-03-26 22:37:57 -0700

    got x/o to show up on click and restart function to work

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,10 +1,38 @@
- // document.getElementById('box1').addEventListener('click'), function() {
- //    function 
+ // var nextTurn = 'X';
 
+ // if (this.id == "s1")
+ // {
+ //      if(document.getElementById("s1").innerHTML == ""){ 
+ //           document.getElementById("s1").innerHTML = nextTurn;
+ //           changeTurn();
+ //      }
+ // }  
+ // function changeTurn(){
+ //      if(nextTurn == 'X'){
+ //           nextTurn = 'O';
+ //      } else {
+ //           nextTurn = 'X';
+ //      }
  // }
 
 
+var turn = 'X';
+function next() {
+    turn = turn === 'X' ? 'O' : 'X';
+}
+function play() {
+    if (this.innerHTML === '') {
+        this.innerHTML = turn;
+        next();
+    }
+}
 
-
-
-
+var squares = document.getElementById("board").getElementsByTagName("td");
+for (var i in squares) {
+   squares[i].onclick = play;
+}
+document.getElementById("restart").onclick = function() {
+    for (var i in squares) {
+       squares[i].innerHTML = '';
+    }
+}
