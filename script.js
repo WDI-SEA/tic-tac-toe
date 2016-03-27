17e66a0c74968032eeac3b78d7df6f5afc0cddd3
Julia Hensley <jh.julia@gmail.com>
2016-03-27 10:25:00 -0700

    added DOM commands to try to build gameboard

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// // var container = document.getElementById('container')
+var container = document.getElementById('container');
+var squares = 9;
+
+
+var gameboard = function(){
+	if (document.querySelectorAll('div') * squares === 9) {
+		return;
+	}
+
+}
+
 // var playerX = "X";
 // var playerO = "O";
 
@@ -6,5 +16,5 @@
 // // var playermove = function {if }
 
 // var squares = function(){
-	
+
 // }
