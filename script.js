c0c1aee720b5b92740d52ee04d3aca8b2be45360
Julia Hensley <jh.julia@gmail.com>
2016-03-27 12:17:11 -0700

    created reset button

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // var container = document.getElementById('container');
-// var squares = 9;
+
+// var playerX = "X";
+// var playerO = "O";
+
+// var playermove = function {if }
+
+// }
+
+
+
+
+
+
+
 
 
 // var gameboard = function(){
 // 	if (document.querySelectorAll('div') * squares === 9) {
 // 		return;
 // 	}
-
-// }
-
-// var playerX = "X";
-// var playerO = "O";
-
-
-// // var playermove = function {if }
-
-// var squares = function(){
-
-// }
