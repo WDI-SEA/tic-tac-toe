11ee2d52cdb617ea64178c3e6d207136495b0aab
bengus101 <bgustafson29@yahoo.com>
2016-03-27 20:03:14 -0700

    Added button, background image & some JS.

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 var board = ['Cell 1', 'Cell 2', 'Cell 3', 'Cell 4', 'Cell 5', 'Cell 6', 'Cell 7', 'Cell 8', 'Cell 9'];
 document.getElementById('Cell1').addEventListener('click', function(event){
-	alert(event.target.innerHTML)
+	event.target.innerHTML = "X";
 })
 document.getElementById('Cell2').addEventListener('click', function(event){
-	event.target.innerHTML = "X" || "O"
+	event.target.innerHTML = "X";
 })
+document.getElementById('Cell3').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+document.getElementById('Cell4').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+document.getElementById('Cell5').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+document.getElementById('Cell6').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+document.getElementById('Cell7').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+document.getElementById('Cell8').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+document.getElementById('Cell9').addEventListener('click', function(event){
+	event.target.innerHTML = "X";
+})
+
+
+
