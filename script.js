7eb54f7a728656369bff47cbb1e15486f1d80832
Drew Rudebusch <drewrudebusch@gmail.com>
2016-03-27 23:43:14 -0700

    Working on checkForWin function

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 var container = document.getElementById('container');
 var squares = document.querySelectorAll('.square')
-var player = document.querySelector("#player");
-var turn = "X";
+var player = document.querySelector('#player');
+var playedSquares;
+var turn = 'X';
+var gameOver = 0;
 var reset = document.querySelector('#reset');
+var win = [['sq1', 'sq2', 'sq3'],
+			['sq1', 'sq5', 'sq9'],
+			['sq1', 'sq4', 'sq7'],
+			['sq4', 'sq5', 'sq6'],
+			['sq7', 'sq5', 'sq3'],
+			['sq7', 'sq8', 'sq9'],
+			['sq2', 'sq5', 'sq8'],
+			['sq3', 'sq6', 'sq9']]
 
 var setTurn = function(x) {
 	turn = x;
     player.innerHTML = x;
 };
 
-// var checkForWin = function(player, square) {
+var resetPlayedObject = function() {
+	playedObject = {'sq1': "", 'sq2': "", 'sq3': "", 'sq4': "", 'sq5': "", 'sq6': "", 'sq7': "", 'sq8': "", 'sq9': ""};
+}
 
-// };
+var checkForWin = function(player, square) {
+	
+	playedSquares = document.getElementsByClassName('played');
+	resetPlayedObject();
+	for (var i = 0; i < playedSquares.length; i++) {
+		sq = playedSquares[i];
+		playedObject[sq.id] = sq.innerHTML;
+		console.log(playedObject[sq.id].innerHTML);
+		console.log(playedObject);
+	};
+	playedObject.forEach(function(win) {
+		for (var i = 0; i < win.length; i++) {
+			if (playedObject[win[0]] === turn && playedObject[win[1]] === turn && playedObject[win[2]] === turn) {
+
+			}
+		}
+	});
+};
 
 reset.addEventListener('click', function(e) {
 	for (var i = 0; i < squares.length; i++) {
-		squares[i].innerHTML = "";
-		setTurn("X");
+		squares[i].innerHTML = '';
+		setTurn('X');
 	};
 });
 
@@ -29,20 +58,20 @@ for (var i = 0; i < squares.length; i++) {
     })
     squares[i].addEventListener('click', function(e) {
     	console.log(e.target.innerHTML);
-    	if (e.target.innerHTML === "") {
+    	if (e.target.innerHTML === '') {
     		e.target.innerHTML = turn;
     		e.target.classList.add('played');
-    		if (turn === "X") {
-    			setTurn("O");
+    		if (turn === 'X') {
+    			setTurn('O');
     		} else {
-    			setTurn("X");
+    			setTurn('X');
     		};
     	} else {
     		e.target.classList.add('red');
     		setTimeout(function(){ e.target.classList.remove('red'); }, 250);
     	}
-    	console.log(e.innerHTML);
-    	//console.log(document.getElementsByClassName('played'));
+
+    	checkForWin("X", e['#id']);
 	})
 };
 
