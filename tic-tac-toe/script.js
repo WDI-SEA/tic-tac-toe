a273e94c6f26f7149402a333e10d973699583570
keidra <keidrabaldwin@gmail.com>
2016-03-27 20:27:52 -0700

    added funtionality for gameplay

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,21 +1,3 @@
- // var nextTurn = 'X';
-
- // if (this.id == "s1")
- // {
- //      if(document.getElementById("s1").innerHTML == ""){ 
- //           document.getElementById("s1").innerHTML = nextTurn;
- //           changeTurn();
- //      }
- // }  
- // function changeTurn(){
- //      if(nextTurn == 'X'){
- //           nextTurn = 'O';
- //      } else {
- //           nextTurn = 'X';
- //      }
- // }
-
-
 var turn = 'X';
 function next() {
     turn = turn === 'X' ? 'O' : 'X';
@@ -23,6 +5,7 @@ function next() {
 function play() {
     if (this.innerHTML === '') {
         this.innerHTML = turn;
+        checkForWinner();
         next();
     }
 }
@@ -36,3 +19,119 @@ document.getElementById("restart").onclick = function() {
        squares[i].innerHTML = '';
     }
 }
+
+function checkForWinner(){
+
+    /* Rows */
+
+    if(document.getElementById("s1").innerHTML == 'X' && document.getElementById("s2").innerHTML == 'X' && document.getElementById("s3").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s1").innerHTML == 'O' && document.getElementById("s2").innerHTML == 'O' && document.getElementById("s3").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+      if(document.getElementById("s4").innerHTML == 'X' && document.getElementById("s5").innerHTML == 'X' && document.getElementById("s6").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s4").innerHTML == 'O' && document.getElementById("s5").innerHTML == 'O' && document.getElementById("s6").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+
+      if(document.getElementById("s7").innerHTML == 'X' && document.getElementById("s8").innerHTML == 'X' && document.getElementById("s9").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s7").innerHTML == 'O' && document.getElementById("s8").innerHTML == 'O' && document.getElementById("s9").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+
+    /* Colummns */
+
+    if(document.getElementById("s1").innerHTML == 'X' && document.getElementById("s4").innerHTML == 'X' && document.getElementById("s7").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s1").innerHTML == 'O' && document.getElementById("s4").innerHTML == 'O' && document.getElementById("s7").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+      if(document.getElementById("s2").innerHTML == 'X' && document.getElementById("s5").innerHTML == 'X' && document.getElementById("s8").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s2").innerHTML == 'O' && document.getElementById("s5").innerHTML == 'O' && document.getElementById("s8").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+
+      if(document.getElementById("s3").innerHTML == 'X' && document.getElementById("s6").innerHTML == 'X' && document.getElementById("s9").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s3").innerHTML == 'O' && document.getElementById("s6").innerHTML == 'O' && document.getElementById("s9").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+    /* Diagonals */
+
+      if(document.getElementById("s1").innerHTML == 'X' && document.getElementById("s5").innerHTML == 'X' && document.getElementById("s9").innerHTML == 'X'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s3").innerHTML == 'O' && document.getElementById("s5").innerHTML == 'O' && document.getElementById("s7").innerHTML == 'O'){
+        alert("O WINS YO!!!");
+    }
+
+          if(document.getElementById("s1").innerHTML == 'O' && document.getElementById("s5").innerHTML == 'O' && document.getElementById("s9").innerHTML == 'O'){
+        alert("X WINS YO!!!");
+    }
+
+     if(document.getElementById("s3").innerHTML == 'X' && document.getElementById("s5").innerHTML == 'X' && document.getElementById("s7").innerHTML == 'X'){
+        alert("O WINS YO!!!");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
