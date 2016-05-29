c57fa2ce95f2af841062f11973bcb64f47440722
Jamie Karlovich <jamierkarlovich@gmail.com>
2016-05-28 22:58:57 -0700

    Completed Requirements of Deliverable
    Made container div with a header and 3 row divs containing three square divs
    used flexbox to center container div in body element
    made if else statement to change class, add x or o, and remove event listener
    made reset button

--- src/js/app.js
+++ src/js/app.js
@@ -2,43 +2,55 @@
 document.addEventListener('DOMContentLoaded', function() {
   console.log('dom loaded');
 
-var container = document.getElementById('container');
+  var container = document.getElementById('container');
 
-var count = 0;
+  var count = 0;
 
-var a = document.getElementById('a');
-var b = document.getElementById('b');
-var c = document.getElementById('c');
-var d = document.getElementById('d');
-var e = document.getElementById('e');
-var f = document.getElementById('f');
-var g = document.getElementById('g');
-var h = document.getElementById('h');
+  var a = document.getElementById('a');
+  var b = document.getElementById('b');
+  var c = document.getElementById('c');
+  var d = document.getElementById('d');
+  var e = document.getElementById('e');
+  var f = document.getElementById('f');
+  var g = document.getElementById('g');
+  var h = document.getElementById('h');
+  var j = document.getElementById('j');
 
-container.addEventListener('click', function(event){
-  event.preventDefault();
-  count = count + 1;
-  console.log(count);
-});
+  var reset = document.getElementById('reset');
 
-a.addEventListener('click', function(event){
-  event.preventDefault();
-  if(count % 2 === 0) {
-    a.classList.add('o');
-    a.textContent = '';
+  var array = [a, b, c, d, e, f, g, h, j];
+
+  function move(event) {
+    var square = event.currentTarget;
+    if(count % 2 === 0) {
+    square.classList.add('o');
+    square.textContent = 'O';
+    count = count + 1;
+    console.log(count);
+    square.removeEventListener('click', move);
   } else {
-    a.classList.add('x');
-    a.textContent = '';
+    square.classList.add('x');
+    square.textContent = 'X';
+    count = count + 1;
+    console.log(count);
+    square.removeEventListener('click', move);
   }
+  };
+
+for (var i = 0; i < array.length; i++) {
+  array[i].addEventListener('click', move);
+};
+
+reset.addEventListener('click', function(event){
+  for (var i = 0; i < array.length; i++){
+    array[i].classList.remove('x');
+    array[i].classList.remove('o');
+    array[i].innerHTML = "&nbsp;";
+    count = 0;
+    array[i].addEventListener('click', move);
+  };
 });
 
 });
-
- a.removeEventListener('click', function(event){
-    a.classList.add('o');
-  });
-
-
-//add x or o in box, keeps shifing boxes down
 
 //center board
