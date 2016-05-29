a35b3669a6c9afba5ae776b676d398c112cb8dec
Krista Calderon <kamsci@gmail.com>
2016-05-28 17:09:26 -0700

    replaced individual cell id in makeMark function with (mark) id which is obtained from EventListener

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,47 @@
 console.log('Hello front end');
 document.addEventListener('DOMContentLoaded', function() {
 
-var cell1 = document.getElementById('cell1');
-var cell2 = document.getElementById('cell2');
-var cell3 = document.getElementById('cell3');
-var cell4 = document.getElementById('cell4');
-var cell5 = document.getElementById('cell5');
-var cell6 = document.getElementById('cell6');
-var cell7 = document.getElementById('cell7');
-var cell8 = document.getElementById('cell8');
-var cell9 = document.getElementById('cell9');
+  var cell1 = document.getElementById('cell1');
+  var cell2 = document.getElementById('cell2');
+  var cell3 = document.getElementById('cell3');
+  var cell4 = document.getElementById('cell4');
+  var cell5 = document.getElementById('cell5');
+  var cell6 = document.getElementById('cell6');
+  var cell7 = document.getElementById('cell7');
+  var cell8 = document.getElementById('cell8');
+  var cell9 = document.getElementById('cell9');
 
-var inactive = cell1.classList.contains('inactive');
+  var inactive = document.getElementsByClassName('inactive');
 
-var counter = 0;
+  // console.log(inactive);
 
-  cell1.addEventListener('click', function(event){
+  var counter = 0;
+
+  cell1.addEventListener('click', function(){
     alert('X goes first!');
-    event.preventDefault(event);
-    mark();
-
-
+    // event.preventDefault(event);
+    makeMark(cell1);
   });
 
-function mark() {
-  if (counter % 2 === 0 && inactive) {
-      cell1.innerHTML = 'X';
+  cell2.addEventListener('click', function(){
+    alert('X goes first!');
+    // event.preventDefault(event);
+    makeMark(cell2);
+  });
+
+function makeMark(mark) {
+  if (counter % 2 === 0 && mark.innerHTML === '') {
+      mark.innerHTML = 'X';
       counter += 1;
       console.log(counter);
       cell1.classList.remove('inactive');
-      cell1.classList.add('highlightorange');
-    } else if (counter % 2 !== 0 && inactive){
-      cell1.innerHTML = 'O';
+      mark.classList.add('highlightorange');
+    } else if (counter % 2 !== 0 && mark.innerHTML === '') {
+      mark.innerHTML = 'O';
       counter += 1;
       console.log(counter);
-      cell1.classList.remove('inactive');
-      cell1.classList.add('highlightpurple');
+      mark.classList.remove('inactive');
+      mark.classList.add('highlightpurple');
     }
   }
 
