749a7284d493592a4c132c5d202ad113bcc44e34
darkenvy <darkenvy6@gmail.com>
2016-05-29 18:48:35 -0700

    Only random-AI. Back to drawing board

--- src/js/app.js
+++ src/js/app.js
@@ -75,10 +75,10 @@ function clicked(cell) {
 }
 
 
-// ======================================= //
+// =============== Mini Functions ================= //
 
 function getCell(num) {
-  console.log("num: " + num);
+  // console.log("num: " + num);
   return board[Math.floor(num / 3)][num % 3];
 }
 
@@ -100,19 +100,6 @@ function randomCell() {
   return Math.floor(Math.random()*9)
 }
 
-function computerPlay() {
-  // if (turn < 3) {
-  //   console.log("Earlygame Random Move")
-  //   randomPlay();
-  //   return;
-  // }
-  if (aiCalc() == 1) {
-    console.log("PERFORMING RANDOM MOVE")
-    randomPlay();
-    return;
-  }
-}
-
 function randomPlay() {
   while (turn < 9) {
     rand = randomCell();
@@ -124,81 +111,26 @@ function randomPlay() {
   }
 }
 
-function aiCalc() {
-  if (turn > 3) {
-    var xTotal = 0;
-    // cycle through all cells from 0 to 8. Perform neighbor search
-    for (var i=0; i<8;i++) {
-      if (getCell(i) == "" || getCell(i) == "O"){
-        continue; // if cell is empty or me, continue search now
-      }
-      else {
-        // scan every direction
-        for (var dir=0; dir<8; dir++) {
-          // neighbor returns best move to play or -1 on error.
-          // console.log( "dir & i: ", dir, i);
-          var bestPlay = neighbor(dir, i);
-          // console.log("bestplay: ", bestPlay);
-          if (bestPlay == -1){continue;}
-          else if (getCell(bestPlay) === null){
-            playMove(1,null, bestPlay);
-            return 0; // worked
-          }
-          else {
-            continue;
-          }
-        }
-      }
-    }
-  }
-  return 1; // error
+// ==================================== //
 
-}
-
-function neighbor(direction, cell) {
-  var compareX = 0;
-  var compareY = 0;
-  switch (direction) {
-    case 0:
-      compareY = -1;
-    break;
-    case 1:
-      compareX = 1;
-      compareY = -1;
-    break;
-    case 2:
-      compareX = 1;
-    break;
-    case 3:
-      compareX = 1;
-      compareY = 1;
-    break;
-    case 4:
-      compareY = 1;
-    break;
-    case 5:
-      compareX = 1;
-      compareY = -1;
-    break;
-    case 6:
-      compareX = -1;
-    break;
-    case 7:
-      compareX = -1;
-      compareY = -1;
-    break;
+function computerPlay() {
+  if (turn < 3) {
+    console.log("Earlygame Random Move")
+    randomPlay();
+    return;
   }
-  // console.log( "compare X Y: ", compareX, compareY);
-  // if (board[cell + compareX][cell + compareY] === undefined) {
-  //   return -1; // If outside of the board, return 0 to continue loop
+  // if (aiCalc() == 1) {
+  //   console.log("PERFORMING RANDOM MOVE")
+  //   randomPlay();
+  //   return;
   // }
-  if (board[cell + (compareX*2)][cell + (compareY*2)] !== undefined &&
-      board[cell + compareX][cell + compareY] == "X") {
-        console.log("found best square!", compareX*2, compareY*2);
-        return getNum(compareX*2, compareY*2); // return the square to play on
-  }
-  return -1;
 }
+
+
+
+
+
+
 
 function playMove(player, htmlpath, computerCell=null) {
   // If the computer is playing, it neds to pass in the cell number
