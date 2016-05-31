5e21d7a729ec07c29f16d28c51c28ce6dceb5b12
Krista Calderon <kamsci@gmail.com>
2016-05-30 17:25:10 -0700

    separated process out into smaller functions; added logic for reset button; added css styling.. yea, i should have had more commits

--- src/js/app.js
+++ src/js/app.js
@@ -1,81 +1,148 @@
-console.log('Hello front end');
-document.addEventListener('DOMContentLoaded', function() {
+console.log("Hello front end");
 
 // Variables
-  var cell1 = document.getElementById('cell1');
-  var cell2 = document.getElementById('cell2');
-  var cell3 = document.getElementById('cell3');
-  var cell4 = document.getElementById('cell4');
-  var cell5 = document.getElementById('cell5');
-  var cell6 = document.getElementById('cell6');
-  var cell7 = document.getElementById('cell7');
-  var cell8 = document.getElementById('cell8');
-  var cell9 = document.getElementById('cell9');
+var turnCounter = 1;
+var currentPlayer = "X";
+var haveWinner = false;
+var msg = document.getElementById("msg");
 
-  var msg = document.getElementById('msg');
+var cell1 = document.getElementById('cell1');
+var cell2 = document.getElementById('cell2');
+var cell3 = document.getElementById('cell3');
+var cell4 = document.getElementById('cell4');
+var cell5 = document.getElementById('cell5');
+var cell6 = document.getElementById('cell6');
+var cell7 = document.getElementById('cell7');
+var cell8 = document.getElementById('cell8');
+var cell9 = document.getElementById('cell9');
 
-  var counter = 0;
+var reset = document.getElementById('reset');
 
-// Events
-  cell1.addEventListener('click', function(){
-    makeMark(cell1);
-    console.log('cell clicked')
-  });
+// Event Listeners
+cell1.addEventListener('click', function () {
+  processTurn(cell1);
+});
 
-  cell2.addEventListener('click', function(){
-    makeMark(cell2);
-  });
+cell2.addEventListener('click', function () {
+  processTurn(cell2);
+});
 
-  cell3.addEventListener('click', function(){
-    makeMark(cell3);
-  });
+cell3.addEventListener('click', function() {
+  processTurn(cell3);
+});
 
-  cell4.addEventListener('click', function(){
-    makeMark(cell4);
-  });
+cell4.addEventListener('click', function () {
+  processTurn(cell4);
+});
 
-  cell5.addEventListener('click', function(){
-    makeMark(cell5);
-  });
+cell5.addEventListener('click', function () {
+  processTurn(cell5);
+});
 
-  cell6.addEventListener('click', function(){
-    makeMark(cell6);
-  });
+cell6.addEventListener('click', function() {
+  processTurn(cell6);
+});
 
-  cell7.addEventListener('click', function(){
-    makeMark(cell7);
-  });
+cell7.addEventListener('click', function () {
+  processTurn(cell7);
+});
 
-  cell8.addEventListener('click', function(){
-    makeMark(cell8);
-  });
+cell8.addEventListener('click', function () {
+  processTurn(cell8);
+});
 
-  cell9.addEventListener('click', function(){
-    makeMark(cell9);
-  });
+cell9.addEventListener('click', function() {
+  processTurn(cell9);
+});
 
-// Game Play Functions
-function makeMark(mark) {
-  if (counter % 2 === 0 && mark.innerHTML === '') {
-      mark.innerHTML = 'X';
-      counter += 1;
-      console.log(counter);
-      cell1.classList.remove('inactive');
-      mark.classList.add('highlightorange');
-      msg.innerHTML = 'Player O, your turn...';
+reset.addEventListener("click", function() {
+  resetBoard();
+})
 
-    } else if (counter % 2 !== 0 && mark.innerHTML === '') {
-      mark.innerHTML = 'O';
-      counter += 1;
-      console.log(counter);
-      mark.classList.remove('inactive');
-      mark.classList.add('highlightpurple');
-      msg.innerHTML = 'Player X, your turn...';
+// Turn Steps
+setMessage("Player X Starts");
+
+function processTurn(move) {
+  // process turn only if valid
+  var valid = validMove(move);
+  // process turn only if valid
+  if (valid) {
+    updateBoard(move); // update board
+    haveWinner = checkWin(move); // check for winner
+    if (haveWinner) {
+      // msg "Winner!"
+      setMessage("Player " + currentPlayer + " Wins!");
+    } else if (turnCounter === 9) {
+    // stalemate msg
+      setMessage("Stalemate. Try again.");
+    } else {
+      switchPlayer(); // switch currentPlayer
+      // update msg
+      setMessage("Player " + currentPlayer + "\"s turn...");
     }
-  } // end makeMark
+    // increment turn counter at end of valid move
+    turnCounter++;
+  }
+}
 
+// identify clicked -  valid move?
+function validMove(move) {
+  return move.innerHTML === ""
+    && turnCounter <= 9
+    && haveWinner === false;
+}
 
+// mark x in html and assign correct highlight class
+function updateBoard(move) {
+    move.innerHTML = currentPlayer;
+    move.classList.add("highlight" + currentPlayer);
+}
 
+function checkWin(move) {
+  // check for winning combos
+  var win = checkCombos(1, 2, 3)
+    || checkCombos(4, 5, 6)
+    || checkCombos(7, 8, 9)
+    || checkCombos(1, 4, 7)
+    || checkCombos(2, 5, 8)
+    || checkCombos(3, 6, 9)
+    || checkCombos(1, 5, 9)
+    || checkCombos(3, 5, 7);
+  return win;
+}
 
+function checkCombos(a, b, c) {
+  // check if cells match player and 2 others
+  return currentPlayer === getCell(a).innerHTML
+    && getCell(a).innerHTML=== getCell(b).innerHTML
+    && getCell(b).innerHTML === getCell(c).innerHTML;
+}
 
-}); // end of DOMContentLoad
+function getCell(num) {
+  // get a cell to check
+  return document.getElementById("cell" + num);
+}
+
+function switchPlayer() {
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+}
+
+function setMessage(message) {
+  msg.innerHTML = message;
+}
+
+// reset gameboard
+function resetBoard() {
+  for (var i = 1; i <= 9; i++) {
+    var cell = document.getElementById('cell' + i);
+    cell.classList.remove("highlightX");
+    cell.classList.remove("highlightO");
+    cell.innerHTML = "";
+    turnCounter = 0;
+  }
+  setMessage("Player X Starts");
+}
