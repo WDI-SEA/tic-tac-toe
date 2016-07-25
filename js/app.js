4b488639b30686727875f7976c5e18113ff5f8cb
shaneehlert <shane@esperas.org>
2016-07-25 08:25:39 -0700

    couldn't get eventlistener to work on board and my rows displayed inline-block down not across

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('Hello frontend');
+var topleft = document.getElementById('top1')
+var centerleft = document.getElementById('middle4')
+var bottomleft = document.getElementById('bottom7')
+var middletop = document.getElementById('top2')
+var middlecenter = document.getElementById('middle5')
+var middlebottom = document.getElementById('bottom8')
+var topright = document.getElementById('top3')
+var centerright = document.getElementById('middle6')
+var bottomright = document.getElementById('bottom9')
+var firstplay = 'X'
+var nextplay = 'O'
+
+top1.addEventListener("click", function(){
+    if () {
+
+    }
+});
+
+btn.addEventListener("click", function(){
+    location.reload()
+});
+
