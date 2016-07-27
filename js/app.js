ee758b76f44e0f5178fdf59a6c680a617f3dd560
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-07-26 19:56:31 -0700

    created runCheck function

--- js/app.js
+++ js/app.js
@@ -9,161 +9,116 @@ var bottom2 = document.getElementById('bottom-2');
 var bottom3 = document.getElementById('bottom-3');
 
 var currentPlayer = 'x';
-var currentPlayero = '<img src="img/o.jpg" alt="o" />';
-var currentPlayerx = '<img src="img/x.jpg" alt="x" />';
+var currentPlayero = '<img src="img/o.jpg" alt="o">';
+var currentPlayerx = '<img src="img/x.jpg" alt="x">';
 
 var newGame = document.getElementById('btn');
 
 
 
-var winComb = [ ['top1', 'top2', 'top3'], 
-                ['middle1', 'middle2', 'middle3'], 
-                ['bottom1', 'bottom2', 'bottom3'],
-        	    ['top1', 'middle1', 'bottom1'], 
-        	    ['top2', 'middle2', 'bottom2'], 
-        		['top3', 'middle3', 'bottom3'],
-        		['top1', 'middle2', 'bottom3'], 
-        		['top3', 'middle2', 'bottom1'] ];
+// var winComb = [ ['top1', 'top2', 'top3'], 
+//                 ['middle1', 'middle2', 'middle3'], 
+//                 ['bottom1', 'bottom2', 'bottom3'],
+//         	       ['top1', 'middle1', 'bottom1'], 
+//         	       ['top2', 'middle2', 'bottom2'], 
+//         		     ['top3', 'middle3', 'bottom3'],
+//         		     ['top1', 'middle2', 'bottom3'], 
+//         		     ['top3', 'middle2', 'bottom1'] ];
 
 
 
 function checkWinner(){
-if(top1 && top2 && top3 === currentPlayer){
-    alert('sproket wrench please');
-}else if(middle1 && middle2 && middle3 === currentPlayer){
-	alert('sprocket wrench please');
-}else if(bottom1 && bottom3 && bottom3){
-	alert('sprocket wrench please');
-}else if(top1 && middle1 && bottom1 === currentPlayer){
-	alert('sprocket wrench please');
-}else if(top2 && middle2 && bottom2 === currentPlayer){
-	alert('sprocket wrench please');
-}else if(top3 && middle3 && bottom3 === currentPlayer){
-	alert('sprocket wrench please');
-}else if(top1 && middle2 && bottom3 === currentPlayer){
-	alert('sprocket wrench please');
-}else if(top3 && middle2 && bottom1 === currentPlayer){
 
-}else{
-	alert('tie');
+	if(top1.innerHTML && top2.innerHTML && top3.innerHTML === currentPlayerx){
+	    alert('sproket wrench please');
+	    console.log('winner')
+	}else if(middle1.innerHTML && middle2.innerHTML && middle3.innerHTML === currentPlayerx){
+		alert('sprocket wrench please');
+	}else if(bottom1.innerHTML && bottom3.innerHTML && bottom3.innerHTML === currentPlayerx){
+		alert('sprocket wrench please');
+	}else if(top1.innerHTML && middle1.innerHTML && bottom1.innerHTML === currentPlayerx){
+		alert('sprocket wrench please');
+	}else if(top2.innerHTML && middle2.innerHTML && bottom2.innerHTML === currentPlayerx){
+		alert('sprocket wrench please');
+	}else if(top3.innerHTML && middle3.innerHTML && bottom3.innerHTML === currentPlayerx){
+		alert('sprocket wrench please');
+	}else if(top1.innerHTML && middle2.innerHTML && bottom3.innerHTML === currentPlayerx){
+		alert('sprocket wrench please');
+	}else if(top3.innerHTML && middle2.innerHTML && bottom1.innerHTML === currentPlayerx){
+
+	}
+};
+
+// checkWinner();
+
+var runCheck = function(elem){
+	if (currentPlayer === 'x') {
+		elem.innerHTML = currentPlayerx;
+		currentPlayer = 'o';
+		checkWinner();
+	} else if (currentPlayer === 'o') {
+		elem.innerHTML = currentPlayero;
+		currentPlayer = 'x';
+		checkWinner();
+	}
 }
 
-
-
-
-checkWinner();
  
 
 top1.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
-
-	if (currentPlayer === 'x') {
-		top1.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		top1.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
-	
+	runCheck(top1);
 });
 
 
 top2.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		top2.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		top2.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(top2);
 	
 });
 
 top3.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		top3.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		top3.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+runCheck(top3);
 	
 });
 
 middle1.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-	    middle1.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		middle1.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(middle1);
 });
 
 middle2.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		middle2.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		middle2.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(middle2);
 });
 
 middle3.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		middle3.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		middle3.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(middle3);
 });
 
 bottom1.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		bottom1.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		bottom1.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(bottom1);
 });
 
 bottom2.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		bottom2.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		bottom2.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(bottom2);
 });
 
 bottom3.addEventListener('click', function() {
 	alert('current player is: ' + currentPlayer);
 
-	if (currentPlayer === 'x') {
-		bottom3.innerHTML = currentPlayerx;
-		currentPlayer = 'o';
-	} else if (currentPlayer === 'o') {
-		bottom3.innerHTML = currentPlayero;
-		currentPlayer = 'x';
-	}
+	runCheck(bottom3);
 });
 
 
@@ -172,6 +127,8 @@ bottom3.addEventListener('click', function() {
 newGame.addEventListener('click', function() {
   alert('I know how to add an event listener');
 	   location.reload();
+
+
 
 });
 
