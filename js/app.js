814c94fc8eee05f8dcce4477de65984bb802fbb7
sarahpappas <pappas.sarah@gmail.com>
2016-09-18 20:27:05 -0700

    works, no game logic

--- js/app.js
+++ js/app.js
@@ -1,52 +1,64 @@
- var player1;
- var player2;
- var playerTurn;
+var player1;
+var player2;
+var playerTurn;
 
- var xButton = document.getElementById("js_x");
- var oButton = document.getElementById("js_o");
- var resetBoardButton = document.getElementById("js_play_again");
- var currentGameBoard;
+var xButton = document.getElementById("js_x");
+var oButton = document.getElementById("js_o");
+var resetBoardButton = document.getElementById("js_play_again");
 
- resetBoardButton.addEventListener("click", resetBoard);
+resetBoardButton.addEventListener("click", resetBoard);
 
- function resetBoard() {
+function resetBoard() {
  	player1 = "";
  	player2 = "";
  	playerTurn = "";
  	xButton.style.backgroundColor= "";
  	oButton.style.backgroundColor= "";
- 	currentGameBoard = new Gameboard;
- };
+ 	xButton.style.color = "black";
+ 	oButton.style.color = "black";
+ 	for(var rowName in gameboard.cells) {
+		var row = gameboard.cells[rowName];
+ 		for(var columnName in row){
+ 			var cell = row[columnName];
+ 			cell.reset();
+ 		}
+ 	}
+ 	gameboard.hideOrShowGameBoard("hide");
+}
 
- xButton.addEventListener("click", function(event, once) {
+xButton.addEventListener("click", function() {
  	if (player1) {
- 		return
+ 		return;
  	} else {
  		assignPlayer(event.currentTarget);
  		changeColor(event.currentTarget);
- 		currentGameBoard = new Gameboard();
+ 		gameboard.hideOrShowGameBoard("show");
  	}
- });
+});
 
- oButton.addEventListener("click", function(event, once) {
+oButton.addEventListener("click", function() {
  	if (player2) {
- 		return
+ 		return;
  	} else {
  		assignPlayer(event.currentTarget);
  		changeColor(event.currentTarget);
- 		currentGameBoard = new Gameboard();
+ 		gameboard.hideOrShowGameBoard("show");
  	}
- });
+});
 
  function changeColor(){
  	if (playerTurn == "X") {
  		xButton.style.backgroundColor = "purple";
+ 		xButton.style.color = "white";
  		oButton.style.backgroundColor = "";
+ 		oButton.style.color = "black";
  	} else if (playerTurn === "O") {
  		oButton.style.backgroundColor = "purple";
+ 		oButton.style.color = "white";
  		xButton.style.backgroundColor = "";
+ 		xButton.style.color = "black";
  	}
- };
+ }
 
 function assignPlayer(input) {
 	if (input == xButton) {
@@ -63,56 +75,70 @@ function assignPlayer(input) {
 
 
 function Gameboard() {
-	this.row1 = {
-		leftCell: new Cell("js_1"),
-		centerCell: new Cell("js_2"),
-		rightCell: new Cell("js_3")
-	},
-	this.row2 = {
-		leftCell: new Cell("js_4"),
-		centerCell: new Cell("js_5"),
-		rightCell: new Cell("js_6")
-	},
-	this.row3 = {
-		leftCell: new Cell("js_7"),
-		centerCell: new Cell("js_8"),
-		rightCell: new Cell("js_9")
-	}
+	this.cells = {
+		top: {
+			left: new Cell("js_1"),
+			center: new Cell("js_2"),
+			right: new Cell("js_3")
+		},
+		middle: {
+			left: new Cell("js_4"),
+			center: new Cell("js_5"),
+			right: new Cell("js_6")
+		},
+		bottom: {
+			left: new Cell("js_7"),
+			center: new Cell("js_8"),
+			right: new Cell("js_9")
+		}
+	};	
+
+	this.el = document.getElementById("js_game_board");
 };
 
 Gameboard.prototype = {
-
+	hideOrShowGameBoard: function(hideOrShow) {
+	 	if (hideOrShow == "hide") {
+	 		this.el.style.visibility= "hidden";
+	 	} else if (hideOrShow == "show") {
+	 		this.el.style.visibility= "visible";
+	 	}
+ 	}
 };
 
 function Cell(elName) {
 	this.el = document.getElementById(elName);
-	this.el.textContent = "";
-	this.el.addEventListener("click", this.setToXorO.bind(this));
-}; 
+	this.setToXorO = this.setToXorO.bind(this);
+	this.el.addEventListener("click", this.setToXorO);
+}
 
 Cell.prototype = {
 	switchPlayer: function() {
 		if (playerTurn == player1) {
-		playerTurn = player2;
+			playerTurn = player2;
 		} else if (playerTurn == player2){
 			playerTurn = player1;
 		}
-		setTimeout(changeColor, 500);
+		setTimeout(changeColor, 50);
 	},
 	setToXorO: function() {
-		if (this.clicked){
+		if (this.whoClicked){
 			return;
 		} else {
 			this.el.textContent = playerTurn;
-			this.clicked = playerTurn;
+			this.whoClicked = playerTurn;
 			this.switchPlayer;
-			setTimeout(this.switchPlayer, 10);
+			setTimeout(this.switchPlayer, 1);
 		}
 	},
+	reset: function() {
+		this.el.textContent = "";
+		this.whoClicked = "";
+	}
 };
 
-
-
+var gameboard = new Gameboard();
+gameboard.hideOrShowGameBoard("hide");
 
 
 
