b0c3d028dab6a33963dfe45b41484553afe02a0a
brianbixby0@gmail.com <brianbixby0@gmail.com>
2016-12-03 15:53:27 -0800

    board w/eventlisteners @each square

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log('Hello frontend');
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM loaded");
+
+var boxOrder = ["tl", "tm", "tr", "ml", "mm", "mr", "bl", "bm", "br"];
+var boxNotPlayed = [];
+
+for(var i = 0; i < 9; i++){
+    boxNotPlayed.push({ boxPosition: boxOrder[i], played: false});
+  }
+
+// //Add an event listener to each box
+  var squares = document.getElementById("board");
+  console.log(squares);
+  for(var j = 0; j < squares.children.length; j++){
+    squares.children[j].addEventListener("click", boxPlayed);
+  }
+  //Create a function to check whether the box has been play
+  function boxPlayed(){
+    for(var i = 0; i < boxPlayed.length; i++){
+      if(boxPlayed[i].played === true){
+        return false;
+      }
+    }
+    return true;
+  }
+});
