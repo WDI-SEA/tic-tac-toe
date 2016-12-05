09d462e9cb0e51cc525d299f939d09f31faa368e
Ryne Schillinger <ryneschillinger@gmail.com>
2016-12-04 19:56:34 -0800

    tic-tac-toe v1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-console.log('Hello frontend');
+document.addEventListener("DOMContentLoaded", function() {
+
+	var xTurn = true;
+	var board = document.getElementById("board");
+	var sizzle = document.getElementById("sizzle");
+	var baconTurn = document.getElementById("bacon-turn");
+	var eggTurn = document.getElementById("egg-turn");
+	var upArrow = document.getElementById("up-arrow");
+	var downArrow = document.getElementById("down-arrow");
+
+	for (var i = 0; i < board.children.length; i++) {
+	  board.children[i].addEventListener("click", clickCell);
+	}
+
+	function clickCell() {
+		if (document.getElementById(this.id).classList.contains("empty")) {
+			if (xTurn == true) {
+				var xImg = document.createElement("img");
+				xImg.src = "img/bacon-x.png";
+				document.getElementById(this.id).append(xImg);
+				xTurn = false;
+			} else {
+				var xImg = document.createElement("img");
+				xImg.src = "img/egg-o.png";
+				document.getElementById(this.id).append(xImg);
+				xTurn = true;
+			}
+			document.getElementById(this.id).setAttribute("class", "chosen");
+			sizzle.play();
+		}
+		if (xTurn == true) {
+			baconTurn.setAttribute("src", "img/bacon_act.svg");
+			eggTurn.setAttribute("src", "img/egg_in.svg");
+			upArrow.setAttribute("class", "fa fa-caret-up");
+			downArrow.setAttribute("class", "fa fa-caret-down inactive");
+		} else {
+			baconTurn.setAttribute("src", "img/bacon_in.svg");
+			eggTurn.setAttribute("src", "img/egg_act.svg");
+			upArrow.setAttribute("class", "fa fa-caret-up inactive");
+			downArrow.setAttribute("class", "fa fa-caret-down");
+		}
+	}
+
+
+});
