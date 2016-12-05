7084b435f48df0994c60745a1ede111e0202c238
Donnie Ellstrom <dsvene@gmail.com>
2016-12-04 20:50:51 -0800

    Develop new game feature

--- js/main.js
+++ js/main.js
@@ -16,59 +16,70 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-     //Setup game board object to log game state
-     function gameBoard() {
-          var gamePieces = document.getElementsByClassName("gamePiece");
-          var board = {};
-          for (var i = 0; i < gamePieces.length; i++) {
-               var position = gamePieces[i].dataset.position;
-               var content = gamePieces[i].dataset.content;
-               board[position] = content;
-          }
-          return board;
-     }
+    //keep track of whose turn it is, if turn = 9 then tie game or winner
+    var turn = 0;
+    //setup game board object
+    var board = {};
 
+    //Setup game board object to log game state
+    function gameBoard() {
+        var gamePieces = document.getElementsByClassName("gamePiece");
+        // var board = {};
+        for (var i = 0; i < gamePieces.length; i++) {
+            var position = gamePieces[i].dataset.position;
+            var content = gamePieces[i].dataset.content;
+            board[position] = content;
+        }
+        return board;
+    }
 
-     // console.log(gameBoard);
+    //Select and assign click listener to board tiles
+    var clickTile = document.getElementById("game");
+    console.log(clickTile);
 
-     //keep track of whose turn it is
-     //if turns = 9, tie game or win
-     var turn = 0;
+    for(var i = 0; i < clickTile.children.length; i++) {
+        clickTile.children[i].addEventListener("click", handleClick);
+    }
 
-     //Select and assign click listener to board tiles
-     var clickTile = document.getElementById("game");
-     console.log(clickTile);
-
-     for(var i = 0; i < clickTile.children.length; i++) {
-          clickTile.children[i].addEventListener("click", handleClick);
-     }
-     // console.log(clickTile.children[1].textContent);
-
-     function addMarker(elem) {
-               if (turn % 2 === 0) {
-                    elem.innerHTML = '<img src="img/xmark.png">';
-                    elem.dataset.content = "X";
-               }
-               else {
-                    elem.innerHTML = '<img src="img/omark.png">';
-
-                    elem.dataset.content = "O";
-               }
-               turn ++;
-               console.log(turn);
-               console.log(gameBoard());
-               elem.removeEventListener('cick', handleClick);
-     }
+    //Add player piece on click
+    function addMarker(elem) {
+        if (turn % 2 === 0) {
+            elem.innerHTML = '<img src="img/xmark.png">';
+            elem.dataset.content = "X";
+        }
+        else {
+            elem.innerHTML = '<img src="img/omark.png">';
+            elem.dataset.content = "O";
+        }
+        turn ++;
+        console.log(turn);
+        console.log(gameBoard());
+        elem.removeEventListener('cick', handleClick);
+    }
 
      //Checks for empty cell and game turns < 9, executes add marker to .this on click
-     function handleClick() {
-          var isEmpty = this.childNodes.length === 0;
-          if (turn < 9 && isEmpty) {
-          addMarker(this);
-          }
-          else if (turn === 9) {
-          console.log("game over bitch");
-          }
-     }
+    function handleClick() {
+        var isEmpty = this.childNodes.length === 0;
+            if (turn < 9 && isEmpty) {
+                addMarker(this);
+            }
+            else if (turn === 9) {
+                console.log("game over sucka");
+            }
+    }
+
+    //Clear board and score and reset turns
+    var button = document.querySelector("button")
+    button.addEventListener("click", clearStuff);
+
+    function clearStuff() {
+        console.log("Im alive!");
+        this.innerHTML = "Working?";
+        // board = {};
+        // turn = 0;
+        // for(var i = 0; i < clickTile.children.length; i++) {
+        //     clickTile.children[i].remove();
+        // }
+    }
 
 });
