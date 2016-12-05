cfa81ea086408c249b8be0ce93afdd74842a7690
Ryne Schillinger <ryneschillinger@gmail.com>
2016-12-05 00:32:23 -0800

    v3 with reset button

--- js/app.js
+++ js/app.js
@@ -7,27 +7,36 @@ document.addEventListener("DOMContentLoaded", function() {
 	var eggTurn = document.getElementById("egg-turn");
 	var upArrow = document.getElementById("up-arrow");
 	var downArrow = document.getElementById("down-arrow");
+	var numChosen = 0;
+
+	// ASSIGNING CLICK ACTIONS TO CELLS
 
 	for (var i = 0; i < board.children.length; i++) {
-	  board.children[i].addEventListener("click", clickCell);
+	 	board.children[i].addEventListener("click", clickCell);
 	}
+
+	// CLICKING A CELL
 
 	function clickCell() {
 		if (document.getElementById(this.id).classList.contains("empty")) {
 			if (xTurn == true) {
-				var xImg = document.createElement("img");
-				xImg.src = "img/bacon-x.png";
-				document.getElementById(this.id).append(xImg);
+				var newImg = document.createElement("img");
+				newImg.src = "img/bacon-x.png";
+				document.getElementById(this.id).append(newImg);
 				xTurn = false;
 			} else {
-				var xImg = document.createElement("img");
-				xImg.src = "img/egg-o.png";
-				document.getElementById(this.id).append(xImg);
+				var newImg = document.createElement("img");
+				newImg.src = "img/egg-o.png";
+				document.getElementById(this.id).append(newImg);
 				xTurn = true;
 			}
 			document.getElementById(this.id).setAttribute("class", "chosen");
 			sizzle.play();
+			numChosen += 1;
+			console.log(numChosen);
 		}
+
+		// Changing turn indicator
 		if (xTurn == true) {
 			baconTurn.setAttribute("src", "img/bacon_act.svg");
 			eggTurn.setAttribute("src", "img/egg_in.svg");
@@ -40,6 +49,19 @@ document.addEventListener("DOMContentLoaded", function() {
 			downArrow.setAttribute("class", "fa fa-caret-down");
 		}
 	}
+
+	// RESET GAME
+
+	document.getElementById("reset").addEventListener("click", function() {
+		for (var i = 0; i < numChosen; i++) {
+			document.getElementsByClassName("chosen")[i].removeChild(document.getElementsByClassName("chosen")[i].firstChild);
+		}
+		for (var i = 0; i < board.children.length; i++) {
+			board.children[i].setAttribute("class", "empty");
+		}
+		xTurn = true;
+		numChosen = 0;
+	});
 
 
 });
