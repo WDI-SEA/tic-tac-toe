99591c47fa81509d39aafb45d2491b0da0aee7b8
jnhustin <jnhustin@gmail.com>
2017-02-04 09:25:13 -0800

    added square generator & its CSS class

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 	console.log('DOM Loaded');
 
+// user select gameboard dimension // possible feature
 // initialize board
+var gameBoardDimension = 3;
+var gameBoardArr = [];
+var HTMLGameBoard = document.getElementById('gameBoard');
 
+for (var i = 0; i < gameBoardDimension; i++) {
+	for (var j = 0; j < gameBoardDimension; j++) {
+		gameBoardArr.push({value: null, filled: false});
+		//add square to game board
+	}
+}
 
+function addSquare() {
+	var aSquare = document.createElement('div');
+	aSquare.className = 'square';
+	HTMLGameBoard.appendChild(aSquare);
+}
 // check player turn
 
 // check win condition
