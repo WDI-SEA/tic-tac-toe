628197b3c13041a32dabf3a0ff3c363225d08b15
jnhustin <jnhustin@gmail.com>
2017-02-04 09:03:48 -0800

    added general logic skeleton

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('Hello frontend');
+document.addEventListener("DOMContentLoaded", function() {
+	console.log('DOM Loaded');
+
+// initialize board
+
+
+// check player turn
+
+// check win condition
+
+// update allowed moves
+
+// handle allowance for multiple clicks
+
+// vertical win condition
+
+// horizontal win condition
+
+// diagonal win condition
+
+
+
+
+
+// 
+
+
+
+});
