a18c4f38aae31d3e18482cab8308563e2e129bfa
jnhustin <jnhustin@gmail.com>
2017-02-04 10:16:10 -0800

    added eventListener for box clicked

--- js/app.js
+++ js/app.js
@@ -32,9 +32,18 @@ function addRow() {
 	for (var i = 0; i < gameBoardDimension; i++) {
 		var aSquare = document.createElement('div');
 		aSquare.className = 'square';
+
+		//add event listener for player/click
+		aSquare.addEventListener('click', function() {
+			console.log('square clicked');
+			// add check here for player clicked
+			// tie the square to the gameboardArr
+			this.style.backgroundColor = 'red';
+		})
 		divRow.appendChild(aSquare);
 	}
 }
+// box is clicked
 
 // check player turn
 
@@ -54,11 +63,19 @@ function addRow() {
 	// 		return
 	// 	}
 	// }
-	function checkVerticalSquares() {
-		for (var i = 0; i < gameBoardDimensions; i++) {
-				// if filled = true for the entire row, return true, else return false
-		}
-	}
+	// function checkVerticalSquares() {
+	// 	for (var i = 0; i < gameBoardArr; i++) {
+	// 		for (var j = 0; j < gameBoardArr; j++) {
+	// 			if (gameBoardArr[i][i].filled) {
+
+	// 			}
+	// 			else{
+	// 				return false
+	// 			}
+	// 			return true;
+	// 		}
+	// 	}
+	// }
 // horizontal win condition
 
 // diagonal win condition
