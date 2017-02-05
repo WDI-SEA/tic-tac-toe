ab8eb161a751dbf2a968d35e61a3e6fbdb85eda0
krichdev <kgrichardson2@gmail.com>
2017-02-04 20:30:41 -0800

    Added arrays to hold player move history, and win combonations

--- js/app.js
+++ js/app.js
@@ -3,8 +3,22 @@ document.addEventListener("DOMContentLoaded", function(){
 	var board = document.getElementById("board");
 	var turn = 0;
 
+	//Empty Arrays used to store player move history to check against for win logic
 	var xMoves = []; 
 	var oMoves = []; 
+
+	//Win Combanations
+	var winCombos = [
+		["a1", "a2", "a3"],
+		["b1", "b2", "b3"],
+		["c1", "c2", "c3"],
+		["a1", "b1", "c1"],
+		["a2", "b2", "c2"],
+		["a3", "b3", "c3"],
+		["a1", "b2", "c3"],
+		["a3", "b2", "c1"],
+
+	];
 
 	oTurn = document.getElementById("oTurn");
 	xTurn = document.getElementById("xTurn");
