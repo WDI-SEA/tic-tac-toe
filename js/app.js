ec77c5a1c1291bde1d1060075b504a2eb42c1766
m-woo <mark.chi.woo@gmail.com>
2017-02-05 20:59:33 -0800

    board style

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log('Hello frontend');
+
+function main(){
+	var square = document.querySelectorAll('.rowBox');
+	var players = ['X','O'];
+	var currentTurn = 0;
+	var isGameOver = false;
+	var display = document.querySelector(.topContainer);
+}
+console.log('display')
