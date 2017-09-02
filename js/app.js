08bcd1360dc852e651872d8edc7b4adbcbcc5eb5
laurenperez <laurenashleymarks@gmail.com>
2017-09-02 15:19:25 -0700

    game logic started

--- js/app.js
+++ js/app.js
@@ -1,1 +1,42 @@
-console.log('Hello frontend');
+
+
+var counter = 0;
+var whosTurn = function(){
+  if (counter % 2 === 0){
+    return "X";
+  } else {
+    return "O";
+  }
+};
+
+var playTurn = function() {
+  if (!this.classList.contains("X") && !this.classList.contains("O")) {
+    this.classList.add(whosTurn());
+    console.log(whosTurn);
+    counter++;
+    //check for winner
+  }
+};
+
+
+var startBoard = function() {
+  var gameSquares = document.getElementsByClassName('squares');
+    for (var i=0; i < gameSquares.length; i++){
+      console.log(gameSquares[i]);
+      gameSquares[i].addEventListener('click', playTurn);
+    }
+  };
+
+
+
+startBoard();
+
+
+
+
+
+
+
+
+
+
