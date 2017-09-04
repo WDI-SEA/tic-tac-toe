7bebad7fa5a6b85743bdd0fdb1430e5d9124c43f
laurenperez <laurenashleymarks@gmail.com>
2017-09-04 13:54:27 -0700

    final version

--- js/app.js
+++ js/app.js
@@ -8,88 +8,81 @@ var botL = document.getElementById('botLeft');
 var botM = document.getElementById('botMid');
 var botR = document.getElementById('botRight');
 
-winnerTextEl = document.getElementById('winnerText');
-
+var winnerTextEl = document.getElementById('winnerText');
+var turnText = document.getElementById('turnText');
 var house;
-
 var counter = 0;
+var keepPlaying = true;
 
 var whosTurn = function(){
   if (counter % 2 === 0){
     house = "Targaryan";
+    turnText.textContent = "Lannister";
     return "X";
   } else {
     house = "Lannister";
+    turnText.textContent = "Targaryen";
     return "O";
   }
 };
 
 var displayWinner = function (){
-  winnerTextEl.textContent = house + " wins!";
-}
+  winnerTextEl.textContent = house;
+  turnText.textContent = " ";
+  keepPlaying = false;
+};
 
 var clearBoard = function () {
   var clearSquares = document.getElementsByClassName('squares');
   for (var i=0; i < clearSquares.length; i++) {
         clearSquares[i].classList.remove('X');
         clearSquares[i].classList.remove('O');
-        winnerTextEl.textContent = "The game is on";
+        winnerTextEl.textContent = " ";
         counter = 0;
+        keepPlaying = true;
       }
 };
 
 var clearButton = document.getElementById('clear');
 clearButton.addEventListener('click', clearBoard);
 
-
-
 var checkForWinner = function (){
-  var winner = whosTurn();
+  var player = whosTurn();
   switch(true) {
-    case topL.classList.contains(winner) && topM.classList.contains(winner) && topR.classList.contains(winner):
+    case topL.classList.contains(player) && topM.classList.contains(player) && topR.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case midL.classList.contains(winner) && midM.classList.contains(winner) && midR.classList.contains(winner):
+    case midL.classList.contains(player) && midM.classList.contains(player) && midR.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case botL.classList.contains(winner) && botM.classList.contains(winner) && botR.classList.contains(winner):
+    case botL.classList.contains(player) && botM.classList.contains(player) && botR.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case topL.classList.contains(winner) && midL.classList.contains(winner) && botL.classList.contains(winner):
+    case topL.classList.contains(player) && midL.classList.contains(player) && botL.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case topM.classList.contains(winner) && midM.classList.contains(winner) && botM.classList.contains(winner):
+    case topM.classList.contains(player) && midM.classList.contains(player) && botM.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case topR.classList.contains(winner) && midR.classList.contains(winner) && botR.classList.contains(winner):
+    case topR.classList.contains(player) && midR.classList.contains(player) && botR.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case topL.classList.contains(winner) && midM.classList.contains(winner) && botR.classList.contains(winner):
+    case topL.classList.contains(player) && midM.classList.contains(player) && botR.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
-    case botL.classList.contains(winner) && midM.classList.contains(winner) && topR.classList.contains(winner):
+    case botL.classList.contains(player) && midM.classList.contains(player) && topR.classList.contains(player):
      displayWinner();
-     setTimeout(clearBoard, 2000);
     break;
   }
 };
 
-
 var playTurn = function() {
-  if (!this.classList.contains("X") && !this.classList.contains("O")) {
+  if (!this.classList.contains("X") && !this.classList.contains("O") && keepPlaying) {
     this.classList.add(whosTurn());
     checkForWinner();
     counter++;
   }
 };
-
 
 var startBoard = function() {
   var gameSquares = document.getElementsByClassName('squares');
@@ -98,14 +91,7 @@ var startBoard = function() {
     }
   };
 
-
 startBoard();
-
-
-
-
-
-
 
 
 
