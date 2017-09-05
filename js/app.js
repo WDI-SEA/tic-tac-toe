7adbbc7fa75c6893c977a9a99ed2e69cc364a7fd
erikr93 <erikr93@gmail.com>
2017-09-05 08:36:18 -0700

    Tried to do what I could strugglig with functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('Hello frontend');
+var players = 2;
+var X = "X";
+var O = "O";
+var turn = 0;
+var xPoints = 0;
+var oPoints = 0;
+
+
+function player(){
+	document.getElementByClassName("square");
+	document.addEventListener("click", gamePiece);
+	if(turn % 2 === 0){
+		document.textContent("X");
+	}else{
+		document.textContent("O");
+	}
+}
+
+function gamePiece(){
+	document.getElementByClassName("square").textContent = "X";
+	document.addEventListener("click",player);
+	
+}
+
+function win(){
+	if(gamePiece === 3){
+		alert("You win!");
+
+	}
+}
+
+function createBoard(){
+	document.getElementById("board");
+}
+
+createBoard();
