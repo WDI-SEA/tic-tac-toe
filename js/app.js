c202dbc7937e735c627ae8eb2de8593b9552b8f6
Ari-M <ariminton@live.com>
2017-09-05 08:46:25 -0700

    First commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('Hello frontend');
+console.log('Javascript running, better catch it!');
+
+//Start declaring variables
+var square = document.getElementsByClassName('tile');
+var mark = document.getElementsByTagName('h1');
+var reset = document.querySelector('#reset');
+var colors = ['purple', 'red', 'white', 'orange', 'yellow'];
+var players = ['player1', 'player2'];
+
+//Declare functions
+var addX = function() {
+	mark.textContent = 'X';
+};
+
+//Add event listeners
+square.addEventListener('click', addX);
