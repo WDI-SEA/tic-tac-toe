f34879250d3c4db947735f9ad36eb46dd53af5a0
bjudas03 <ben_judas@hotmail.com>
2017-09-05 10:09:32 -0700

    unfinished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 console.log('Hello frontend');
+
+
+// gameboard======
+// 1. funciton to determine player turn
+// 2. function for x's and o's
+// 		-insert new class of "x" or "o"
+// 		-add background image of "x" or "o"
+// 3. determine if winner 
+// 	class of "Top/center/bottom/left/middle/right" === 3 "x" || 3 "o"
+// 	how to determine diagonal win??
+// 		pop up alert "player (x) wins"
+// 	how to prevent replacing 'x' with 'o' (and vice versa)
+// 3. function for board reset
+
+var Player1 = 'X';
+var Player2 = 'O';
+
+var board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+function  (getWinner){}
+	switch('isWinner') {
+		case 'X Wins':
+		getElementsByClass(.top) === 3;
+
+}
+
+
+functionn 
+
+
+
