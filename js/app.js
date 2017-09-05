3687056d695f936e3e2535871475503d01019f84
foozie3moons <matthew.bell.290@gmail.com>
2017-09-04 18:11:49 -0700

    added a really unnecessary highlighting of the winning cells

--- js/app.js
+++ js/app.js
@@ -12,15 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var ai = 'o';
   var vsAi = false;
 
-  function addEventListeners() {
-    reset.addEventListener('click', resetBoard);
-    Array.from(boxes).forEach(function(box) {
-      box.addEventListener('click', function() {
-        takePlayerTurn(this);
-      });
-    });
-    playAi.addEventListener('click', playVsAi);
-  }
+  /* turn functionality */
 
   function takePlayerTurn(box) {
     if (player === 'x') {
@@ -43,67 +35,22 @@ document.addEventListener('DOMContentLoaded', function() {
       } else if (player === 'o') {
         player = 'x';
       }
-      gameMessage.textContent = "It is player " + player.toUpperCase() + "'s turn!";
       if (vsAi) {
+        gameMessage.textContent = "It is AI " + player.toUpperCase() + "'s turn!";
         takeAiTurn();
+      } else {
+        gameMessage.textContent = "It is player " + player.toUpperCase() + "'s turn!";
       }
-    }
-  }
-
-  function resetBoard() {
-    Array.from(boxes).forEach(function(box)  {
-      box.classList.remove('x');
-      box.classList.remove('o');
-      box.classList.remove('box-overlay');
-      box.title = "";
-    });
-    reset.classList.remove('active');
-    reset.textContent = "Reset Board";
-    player = 'x';
-    gameMessage.textContent = "It is player " + player.toUpperCase() + "'s turn!";
-    gameboardOverlay.style.display = "none";
-
-  }
-
-  function win() {
-    var grid = getGrid();
-    for (var i = 0; i < grid.length; i++) {
-      if ((grid[i][0] === player && grid[i][1] === player && grid[i][2] === player)
-       || (grid[0][i] === player && grid[1][i] === player && grid[2][i] === player)) {
-        return true;
-      }
-    }
-    if ((grid[0][0] === player && grid[1][1] === player && grid[2][2] === player)
-     || (grid[0][2] === player && grid[1][1] === player && grid[2][0] === player)) {
-      return true;
-    }
-    console.log(grid);
-  }
-
-  function playVsAi() {
-    if (vsAi) {
-      vsAi = false;
-      console.log('did something');
-      playAi.textContent = "Play AI";
-    } else {
-      vsAi = true;
-      console.log('did something else');
-      playAi.textContent = "Play Human";
     }
   }
 
   function takeAiTurn() {
     gameboardOverlay.style.display = 'inherit';
     setTimeout(function() {
-      var grid = [];
+      var grid = getGridDOM();
       var rows = getRows();
       var move = generateValidMove();
-      for (var i = 0; i < rows.length; i++) {
-        col1 = rows[i].children[0];
-        col2 = rows[i].children[1];
-        col3 = rows[i].children[2];
-        grid.push([col1, col2, col3]);
-      }
+
       grid[move[0]][move[1]].title = 'o';
       grid[move[0]][move[1]].classList.add('o');
       grid[move[0]][move[1]].classList.add('box-overlay');
@@ -120,12 +67,12 @@ document.addEventListener('DOMContentLoaded', function() {
         gameMessage.textContent = "It is player " + player.toUpperCase() + "'s turn!";
         gameboardOverlay.style.display = "none";
       }
-    }, 2000);
+    }, 1000);
   }
 
   function generateValidMove() {
     var validMove = false;
-    var grid = getGrid();
+    var grid = getGridData();
     var randomMove;
     while (!validMove) {
       randomMove = [Math.floor(Math.random() * grid.length), Math.floor(Math.random() * grid.length)];
@@ -133,9 +80,10 @@ document.addEventListener('DOMContentLoaded', function() {
         validMove = true;
       }
     }
-    console.log(randomMove);
     return randomMove;
   }
+
+  /* grid data */
 
   function getRows() {
     var row1 = document.getElementsByClassName('row-1');
@@ -145,7 +93,19 @@ document.addEventListener('DOMContentLoaded', function() {
     return rows;
   }
 
-  function getGrid() {
+  function getGridDOM() {
+    var rows = getRows();
+    var grid = [];
+    for (var i = 0; i < rows.length; i++) {
+      col1 = rows[i].children[0];
+      col2 = rows[i].children[1];
+      col3 = rows[i].children[2];
+      grid.push([col1, col2, col3]);
+    }
+    return grid;
+  }
+
+  function getGridData() {
     var rows = getRows();
     var grid = [];
     for (var i = 0; i < rows.length; i++) {
@@ -157,8 +117,50 @@ document.addEventListener('DOMContentLoaded', function() {
     return grid;
   }
 
+  /* game ending conditionals */
+
+  function win() {
+    var grid = getGridData();
+    for (var i = 0; i < grid.length; i++) {
+      if ((grid[i][0] === player && grid[i][1] === player && grid[i][2] === player)
+       || (grid[0][i] === player && grid[1][i] === player && grid[2][i] === player)) {
+        return true;
+      }
+    }
+    if ((grid[0][0] === player && grid[1][1] === player && grid[2][2] === player)
+     || (grid[0][2] === player && grid[1][1] === player && grid[2][0] === player)) {
+      return true;
+    }
+  }
+
+  function highlightWinningCells() {
+    var gridDOM = getGridDOM();
+    var gridData = getGridData();
+    console.log(player);
+    for (var i = 0; i < gridData.length; i++) {
+      if (gridData[i][0] === player && gridData[i][1] === player && gridData[i][2] === player) {
+        console.log("did something");
+        gridDOM[i][0].classList.add('winning-solution');
+        gridDOM[i][1].classList.add('winning-solution');
+        gridDOM[i][2].classList.add('winning-solution');
+      } else if (gridData[0][i] === player && gridData[1][i] === player && gridData[2][i] === player) {
+        gridDOM[0][i].classList.add('winning-solution');
+        gridDOM[1][i].classList.add('winning-solution');
+        gridDOM[2][i].classList.add('winning-solution');
+      } else if (gridData[0][0] === player && gridData[1][1] === player && gridData[2][2] === player) {
+        gridDOM[0][0].classList.add('winning-solution');
+        gridDOM[1][1].classList.add('winning-solution');
+        gridDOM[2][2].classList.add('winning-solution');
+      } else if (gridData[0][2] === player && gridData[1][1] === player && gridData[2][0] === player) {
+        gridDOM[2][0].classList.add('winning-solution');
+        gridDOM[1][1].classList.add('winning-solution');
+        gridDOM[0][2].classList.add('winning-solution');
+      }
+    }
+  }
+
   function tie() {
-    var grid = getGrid();
+    var grid = getGridData();
     for (var i = 0; i < grid.length; i++) {
       for (var j = 0; j < grid[i].length; j++) {
         if (grid[i][j] === "") {
@@ -171,15 +173,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function gameOver(winCondition) {
     if (winCondition === "win") {
-      console.log(gameMessage.textContent);
+      highlightWinningCells();
       gameMessage.textContent = "Player " + player.toUpperCase() + " has won!";
-      console.log(gameMessage.textContent);
     } else if (winCondition === "tie") {
       gameMessage.textContent = "It's a tie!";
     }
     gameboardOverlay.style.display = "inherit";
     reset.textContent = "Play Again?";
     reset.classList.add('active');
+  }
+
+  /* handlers */
+
+  function addEventListeners() {
+    reset.addEventListener('click', resetBoard);
+    Array.from(boxes).forEach(function(box) {
+      box.addEventListener('click', function() {
+        takePlayerTurn(this);
+      });
+    });
+    playAi.addEventListener('click', playVsAi);
+  }
+
+  function playVsAi() {
+    if (vsAi) {
+      vsAi = false;
+      playAi.textContent = "Play AI";
+    } else {
+      vsAi = true;
+      playAi.textContent = "Play Human";
+    }
+  }
+
+  function resetBoard() {
+    Array.from(boxes).forEach(function(box)  {
+      box.classList.remove('x');
+      box.classList.remove('o');
+      box.classList.remove('box-overlay');
+      box.classList.remove('winning-solution');
+      box.title = "";
+    });
+    reset.classList.remove('active');
+    reset.textContent = "Reset Board";
+    player = 'x';
+    gameMessage.textContent = "It is player " + player.toUpperCase() + "'s turn!";
+    gameboardOverlay.style.display = "none";
   }
 
   addEventListeners();
