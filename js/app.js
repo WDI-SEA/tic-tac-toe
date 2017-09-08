35c5d1b35281e5f26890eca51707843cdbed1031
chasedeger <cdeger88@gmail.com>
2017-09-08 13:54:25 -0700

    Actual 2nd verion

--- js/app.js
+++ js/app.js
@@ -1,88 +1,66 @@
-document.addEventListener("DOMContentLoaded", function(){
+var x = "X";
+var o = "O";
+var currentPlayer = x;
+var moves = 0;
+
+console.log("started");
+
+var resetBoard = function () {
+  var clearSquares = document.getElementsByClassName('cell');
+  for (var i=0; i < clearSquares.length; i++) {
+        clearSquares[i].classList.remove('x');
+        clearSquares[i].classList.remove('o');
+        moves = 0;
+        console.log("did something");
+      }
+};
+var clearButton = document.getElementById('reset');
+console.log(clearButton);
+clearButton.addEventListener('click', resetBoard);
+
+
+ var switchUser = function () {
+  if (currentPlayer === x) currentPlayer = o;
+  else currentPlayer = x;
+} 
+
+var divClick = function () {
+  if(this.innerHTML === "") {
+    moves++;
+    this.innerHTML=currentPlayer;
+  }
+  else return;
+  if (checkForWinner()) {
+    announceWinner();
+    gameOver();
+  }
  
- var board = document.getElementById("board");
- var turn = 0;
- var oMoves = []; 
- var xMoves = []; 
- var oMovesStr = oMoves.toString();
- 
- var winMoves = ["1,2,3","4,5,6","7,8,9","1,4,7","2,5,8","3,6,9","1,5,9","3,5,7",];
- 
-   xTurn = document.getElementById("xTurn");
-   oTurn = document.getElementById("oTurn");
-   xWin = document.getElementById("xWin");
-   oWin = document.getElementById("oWin");
- 
-   function clickBox(){
-     for (var i = 0; i < board.children.length; i) {
-       board.children[i].addEventListener("click", isClicked);
-     }
-   }
-   clickBox();
- 
-   function isClicked(){
-     if (turn % 2 === 0){
-       this.style.background = "url('./img/paintbrushes.png') no-repeat center";
-       document.querySelector("img").src = "./img/paint-splatter.png";
-       xMoves.push(this.id);
-     } else {
-       this.style.background = "url('./img/paint-splatter.png') no-repeat center";
-       document.querySelector("img").src = "./img/paintbrushes.png";
-       oMoves.push(this.id);
-     }
- 
-     turn
- 
-     this.removeEventListener("click", isClicked);
- 
-     gameWinX();
-     gameWinO();
-     gameTie();
-   } 
- 
- 
- 
-   document.getElementById("restart").addEventListener("click", restartGame);
- 
-   function restartGame(){
-     turn = 0;
-     xMoves = [];
-     oMoves = [];
-     var clearBox = document.querySelectorAll("#board div");
-     for (var i = 0; i < clearBox.length; i) {
-       clearBox[i].style.backgroundImage = "";
-     }
-     clickBox();
- 
-     document.getElementById("message").innerHTML = "Turn = <br><img src=\'./img/paintbrushes.png\' class='scaled'>"
-     xWin.pause();
-     oWin.pause();
-   }
- 
-   function gameWinX(){
-     for (var i = 0; i<winMoves.length; i){
-       xMoves.sort();
-       if(xMoves.toString() == winMoves[i]){
-         document.getElementById("message").innerHTML = "<img src=\'./img/paintbrushes.png\' class='scaled'> Wins!"
-         xTurn.pause();
-       }
-     }
-   }
- 
-   function gameWinO(){
-     for (var i = 0; i<winMoves.length; i){
-       oMoves.sort();
-       if(oMoves.toString() == winMoves[i]){
-         document.getElementById("message").innerHTML = "<img src=\'./img/paint-splatter.png\' class='scaled'> Wins!"
-         oTurn.pause();
-       }
-     }
-   }
- 
-   function gameTie(){
-     if(turn === 9){
-       document.getElementById("message").innerHTML = "<img src=\'./img/paintbrushes.png\' class='scaled'> TIE <img src=\'./img/paint-splatter.png\' class='scaled'>"
-     }
-   }
- 
- });
+  switchUser();
+} 
+
+var cells = document.getElementsByClassName("cell");
+
+for(var i = 0; i < cells.length; i++){
+  cells[i].addEventListener("click", divClick);
+  
+}
+
+var checkForWinner = function() {
+  var cases = ["row1" , "row2", "row3", "col1", "col2", "col3", "diag1", "diag2"];
+  for(var c = 0; c < cases.length; c++){
+    var set = document.getElementsByClassName(cases[c]);
+    var count = 0;
+    
+    for(var i = 0; i < 3; i++){
+      if (set[i].innerHTML == currentPlayer)
+      count++;
+    }
+    if (count == 3) return true;
+  }
+
+  return false;
+}
+
+var announceWinner = function() {
+  alert(currentPlayer + " has WON!!!");
+}
