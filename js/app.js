0b2e2616d2ac2444d7bfd0b8d916c1da58fb14ec
chasedeger <cdeger88@gmail.com>
2017-09-08 13:46:53 -0700

    second version

--- js/app.js
+++ js/app.js
@@ -1,105 +1,88 @@
-var currentPlayer;
-var nextPlayer;
-var numMarkersPlaced;
-var board;
-
-var winningPatterns = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-
-/*paste2 */
-
-}
-
-function checkIfBoardMatchesPattern(pattern, marker) {
-    var matches = true;
-
-    for (var i = 0; i < 3; i++) {
-        matches = matches && board[pattern[i]] === marker;
-    }
-
-    return matches;
-}
-
-function checkForWin(marker) {
-    var winner = false;
-
-    for (var i = 0; i < winningPatterns.length; i++) {
-        if (checkIfBoardMatchesPattern(winningPatterns[i], marker)) {
-            winner = true;
-        }
-    }
-
-    return YouJustWon;
-}
-
-function resetBoard() {
-    board = createBlankBoard();
-    numMarkersPlaced = 0;
-    currentPlayer = playerMarker1;
-    nextPlayer = playerMarker2;
-
-function createBlankBoard() {
-    return [
-        blank, blank, blank,
-        blank, blank, blank,
-        blank, blank, blank
-    ];
-}
-    resetBoard();
-
-
-
-
-/*Doesnt Work
-    ("#reset").html("Clear Board");
-    ("td").on("click", addMarker);
-    ("td").html("");
-    ("#playerTurn").html(`It is {currentPlayer}'s turn`);
-}
-
-
-function addMarker() {
-    if ((this).html().length === 0) {
-        var cellNumber = parseInt((this).attr("data-num"));
-
-        board[cellNumber] = currentPlayer;
-        (this).html(currentPlayer);
-        (this).attr("data-player", currentPlayer === playerMarker1 ? 1 : 2);
-
-        numMarkersPlaced++;
-
-        if (checkForWin(currentPlayer)) {
-            alert("Game over, " + currentPlayer + " wins!");
-            ("#playerTurn").html("Game over, " + currentPlayer + " wins!");
-            ("#reset").html("Play Again");
-            ("td").unbind("click", addMarker);
-        } else if (numMarkersPlaced == 9) {
-    
-
-            alert("Game Over! It's a draw!");
-            ("#playerTurn").html("Game Over! It's a draw!");
-            ("#reset").html("Play Again");
-        } else {
-            
-            var temp = currentPlayer;
-            currentPlayer = nextPlayer;
-            nextPlayer = temp;
-
-            ("#playerTurn").html(`It is {currentPlayer}'s turn`);
-        }
-    }
-}
-                    const blank = "_";
-                    const playerMarker1 = "X";
-                    const playerMarker2 = "O";
-
-(onPageLoad);
-*/
+document.addEventListener("DOMContentLoaded", function(){
+ 
+ var board = document.getElementById("board");
+ var turn = 0;
+ var oMoves = []; 
+ var xMoves = []; 
+ var oMovesStr = oMoves.toString();
+ 
+ var winMoves = ["1,2,3","4,5,6","7,8,9","1,4,7","2,5,8","3,6,9","1,5,9","3,5,7",];
+ 
+   xTurn = document.getElementById("xTurn");
+   oTurn = document.getElementById("oTurn");
+   xWin = document.getElementById("xWin");
+   oWin = document.getElementById("oWin");
+ 
+   function clickBox(){
+     for (var i = 0; i < board.children.length; i) {
+       board.children[i].addEventListener("click", isClicked);
+     }
+   }
+   clickBox();
+ 
+   function isClicked(){
+     if (turn % 2 === 0){
+       this.style.background = "url('./img/paintbrushes.png') no-repeat center";
+       document.querySelector("img").src = "./img/paint-splatter.png";
+       xMoves.push(this.id);
+     } else {
+       this.style.background = "url('./img/paint-splatter.png') no-repeat center";
+       document.querySelector("img").src = "./img/paintbrushes.png";
+       oMoves.push(this.id);
+     }
+ 
+     turn
+ 
+     this.removeEventListener("click", isClicked);
+ 
+     gameWinX();
+     gameWinO();
+     gameTie();
+   } 
+ 
+ 
+ 
+   document.getElementById("restart").addEventListener("click", restartGame);
+ 
+   function restartGame(){
+     turn = 0;
+     xMoves = [];
+     oMoves = [];
+     var clearBox = document.querySelectorAll("#board div");
+     for (var i = 0; i < clearBox.length; i) {
+       clearBox[i].style.backgroundImage = "";
+     }
+     clickBox();
+ 
+     document.getElementById("message").innerHTML = "Turn = <br><img src=\'./img/paintbrushes.png\' class='scaled'>"
+     xWin.pause();
+     oWin.pause();
+   }
+ 
+   function gameWinX(){
+     for (var i = 0; i<winMoves.length; i){
+       xMoves.sort();
+       if(xMoves.toString() == winMoves[i]){
+         document.getElementById("message").innerHTML = "<img src=\'./img/paintbrushes.png\' class='scaled'> Wins!"
+         xTurn.pause();
+       }
+     }
+   }
+ 
+   function gameWinO(){
+     for (var i = 0; i<winMoves.length; i){
+       oMoves.sort();
+       if(oMoves.toString() == winMoves[i]){
+         document.getElementById("message").innerHTML = "<img src=\'./img/paint-splatter.png\' class='scaled'> Wins!"
+         oTurn.pause();
+       }
+     }
+   }
+ 
+   function gameTie(){
+     if(turn === 9){
+       document.getElementById("message").innerHTML = "<img src=\'./img/paintbrushes.png\' class='scaled'> TIE <img src=\'./img/paint-splatter.png\' class='scaled'>"
+     }
+   }
+ 
+ });
