1f9612560d2746c0d86c4c00e0c623d633062440
erikr93 <erikr93@gmail.com>
2017-09-11 16:46:40 -0700

    tic tac toe

--- js/app.js
+++ js/app.js
@@ -1,126 +1,103 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-var player = 1;
-var X = "X";
-var O = "O";
-var nextPlayer = "O";
-var turn = 1;
+var nextPlayer = "X";
+var turn = 0;
 var xPoints = 0;
 var oPoints = 0;
 var squares = document.getElementsByClassName("square");
+var resetBtn = document.getElementById("reset");
+// console.log(squares);
+
+resetBtn.addEventListener("click", reset);
 
 // Turn function
 var player = function (){
-	// document.getElementsByClassName("square");
 	if(turn % 2 === 0){
-		// document.textContent= "X";
 		nextPlayer = "O";
 		turn++;
-		// console.log("O is next");
-	}else{
-		// document.textContent = "O";
+	}else if(turn % 2 === 1){
 		nextPlayer = "X";
 		turn++;
-		// console.log("X is next");
 	}
 };
 
-//second turn function
-// function player(){
-// 	if(player === 1){
-// 		document.getElementsByClassName("squares").textContent = "X";
-// 		player = 0;
-// 	}else {
-// 		document.getElementsByClassName("squares").textContent = "O";
-// 		player = 1;
-// 	}
-// }
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener("click", function(){
+	gamePiece(i);	
+});
 
 
-// X, O function
+X, O function
 function gamePiece(index){
-	/// use this.style or whatever
-	if(squares.textContent === "X" || squares.textContent === "O"){
-		console.log("this square has been played");
-		// squares.push('X').xPLayed;
-		// squares.push('O').oPlayed;
-	} else{
+	if(squares[index].textContent ==="X"||squares[index].textContent ==="O"){
+		console.log("filled square");
+	}else{
 		squares[index].textContent = nextPlayer;
 	}
+}
+	checkWin();
 	player();
-	win();
-} 
-	
-var xPlayed = []
-var oPlayed = []
 
 // win function
-function win(){
-	if(gamePiece === 3){
-		alert("You win!");
-	};
-	switch(true){
-		case squares[0], squares[1], squares[2]:
-			break;
-		case squares[0], squares[3], squares[6]:
-			break;
-		case squares[1], squares[4], squares[7]:
-			break;
-		case squares[2], squares[5], squares[8]:
-			break;
-		case squares[0], squares[4], squares[8]:
-			break;
-		case squares[3], squares[4], squares[5]:
-			break;
-		case squares[6], squares[7], squares[8]:
-			break;
-		case squares[2], squares[4], squares[6]:
-			break;
+function checkWin(){
+  var xo = ["X", "O"];
+  for (var i = 0; i < 2; i++) {
+  	if (squares[0].textContent === xo[i] &&
+        squares[1].textContent === xo[i] &&
+        squares[2].textContent === xo[i] ||
+        	squares[0].textContent === xo[i] &&
+        	squares[3].textContent === xo[i] &&
+        	squares[6].textContent === xo[i] ||
+        squares[1].textContent === xo[i] &&
+        squares[4].textContent === xo[i] &&
+        squares[7].textContent === xo[i] ||
+        	squares[2].textContent === xo[i] &&
+        	squares[5].textContent === xo[i] &&
+        	squares[8].textContent === xo[i] ||
+        squares[0].textContent === xo[i] &&
+        squares[4].textContent === xo[i] &&
+        squares[8].textContent === xo[i] ||
+        	squares[3].textContent === xo[i] &&
+        	squares[4].textContent === xo[i] &&
+        	squares[5].textContent === xo[i] ||
+        squares[6].textContent === xo[i] &&
+        squares[7].textContent === xo[i] &&
+        squares[8].textContent === xo[i] ||
+        	squares[2].textContent === xo[i] &&
+        	squares[4].textContent === xo[i] &&
+        	squares[6].textContent === xo[i]
+          ) {
+  		declareWin();
+  	}
 	}
+
 }
 
-// create board function
-function createBoard(){
-	document.getElementById("board");
+function declareWin(){
+	alert(nextPlayer + " won!");
+	switch(nextPlayer){
+		case "O": oPoints++; 
+		break;
+		case "X": xPoints++; 
+		break;
+		default: 
+		break;
+	}
+	document.getElementById("xScore").textContent = xPoints;
+	document.getElementById("oScore").textContent = oPoints;
+	reset();
 }
 
 //reset function
 function reset(){
-	document.getElementById("board").reset();
-	console.log("reset");
+    for (var i = 0; i < 9; i++) {
+      squares[i].textContent = "";
+    }
+}
+
+function draw(){
+	if(gamePiece === 9 && checkWin === false){
+		alert("Draw!")
+	}
 	reset();
 }
 
-
-////find a way to make this function apply to every member of array
-squares[0].addEventListener("click", function(){
-	gamePiece(0);	
-});
-squares[1].addEventListener("click", function(){
-	gamePiece(1);	
-});
-squares[2].addEventListener("click", function(){
-	gamePiece(2);	
-});
-squares[3].addEventListener("click", function(){
-	gamePiece(3);	
-});
-squares[4].addEventListener("click", function(){
-	gamePiece(4);	
-});
-squares[5].addEventListener("click", function(){
-	gamePiece(5);	
-});
-squares[6].addEventListener("click", function(){
-	gamePiece(6);	
-});
-squares[7].addEventListener("click", function(){
-	gamePiece(7);	
-});
-squares[8].addEventListener("click", function(){
-	gamePiece(8);	
-});
-
-// createBoard();
-
-})
+}
