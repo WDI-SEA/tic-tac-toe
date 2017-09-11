67605d97dcdbffc5bb80704d664abd4f44b9c87f
Ari-M <ariminton@live.com>
2017-09-10 18:11:23 -0700

    removed every move is win bug

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 console.log('Javascript running.');
 //Start declaring variables
 var squares = document.getElementsByClassName('tile');
-var value = []
-for (let i = 0; i < squares.length; i++) {
-var values = squares[i].getAttribute('data-value');
-console.log(values);
-value.push(values[i]);
-}
-
+var values = [];
 var backgroundImage = document.querySelectorAll('.tile')
 var reset = document.querySelector('#reset');
 var turnValue = true;
 
 //Declare functions
+var grabValues = function () {
+	for (let i = 0; i < squares.length; i++) {
+		var value = squares[i].getAttribute('data-value');
+		console.log(value);
+		values.push(value);
+	}
+	return values;
+}
+
 var addX = function (box) {
 	console.log('addX run');
 	box.style.backgroundImage = "url('img/x.png')";
@@ -25,6 +28,7 @@ var addO = function (box) {
 	console.log('addO run');
 	box.style.backgroundImage = "url('img/o.jpg')";
 	box.setAttribute('data-value', 'o');
+	console.log(box);
 	winCondition(false);
 };
 
@@ -48,23 +52,23 @@ var checkDraw = function() {
 	}
 };
 var winCondition = function(bool) {
-	//console.log('win condition is a work in progress');
-	//let div = squares;
-		   if (value[0] === value[1] && value[0] === value[2] && value[1] === value[2]) {
+	console.log('win condition is a work in progress');
+	grabValues();
+		   if (values[0] === values[1] && values[0] === values[2] && values[1] === values[2]) {
 		   	declareWinner(bool);
-		   } else if (value[3] === value[4] && value[3] === value[5] && value[4] === value[5]) {
+		   } else if (values[3] === values[4] && values[3] === values[5] && values[4] === values[5]) {
 		   	declareWinner(bool);
-		   } else if (value[6] === value[7] && value[6] === value[8] && value[7] === value[8]) {
+		   } else if (values[6] === values[7] && values[6] === values[8] && values[7] === values[8]) {
 		   	declareWinner(bool);
-		   } else if (value[0] === value[3] && value[0] === value[6] && value[3] === value[6]) { 
+		   } else if (values[0] === values[3] && values[0] === values[6] && values[3] === values[6]) { 
 		   	declareWinner(bool);
-		   } else if (value[1] === value[4] && value[1] === value[7] && value[4] === value[7]) {
+		   } else if (values[1] === values[4] && values[1] === values[7] && values[4] === values[7]) {
 		   	declareWinner(bool);	
-		   } else if (value[2] === value[5] && value[2] === value[8] && value[5] === value[8]) {
+		   } else if (values[2] === values[5] && values[2] === values[8] && values[5] === values[8]) {
 		   	declareWinner(bool);
-		   } else if (value[0] === value[4] && value[0] === value[8] && value[4] === value[8]) {
+		   } else if (values[0] === values[4] && values[0] === values[8] && values[4] === values[8]) {
 		   	declareWinner(bool);
-		   } else if (value[2] === value[4] && value[2] === value[6] && value[4] === value[6]) {
+		   } else if (values[2] === values[4] && values[2] === values[6] && values[4] === values[6]) {
 		   	declareWinner(bool);
 		   } else {
 		   	checkDraw();
@@ -86,7 +90,8 @@ var playGame = function (box) {
 			addX(box);
 		} else {
 			turnValue = true;
-			addO(box);		}
+			addO(box);	
+		}
 	}
 };
 //Add Event Listeners
