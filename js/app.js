7c47909ff83f8f0b60b79081a62d29e02298a5c3
Ari-M <ariminton@live.com>
2017-09-11 20:07:18 -0700

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -2,15 +2,9 @@
 //Start declaring variables
 var squares = document.getElementsByClassName('tile');
 var values = [];
-for (var i = 0; i < squares.length; i++) {
-	var value = squares[i].getAttribute('data-value')
-	values.push(value);
-}
-//console.log(values);
 var backgroundImage = document.querySelectorAll('.tile')
 var reset = document.querySelector('#reset');
 var turnValue = true;
-
 //Declare functions
 var changeArrayValue = function () {
 	values = []
@@ -19,45 +13,38 @@ var changeArrayValue = function () {
 		values.push(value);
 	}
 }
-
 var addX = function (box) {
 	box.style.backgroundImage = "url('img/x.png')";
-	//console.log('addX run');
 	box.setAttribute('data-value', 'x');
 	changeArrayValue();
-	//console.log(box);
 	winCondition(true);
 };
-
 var addO = function (box) {
 	box.style.backgroundImage = "url('img/o.jpg')";
-	//console.log('addO run');
 	box.setAttribute('data-value', 'o');
 	changeArrayValue();
-	//console.log(box);
 	winCondition(false);
 };
-
 var declareWinner = function(bool) {
 	if (bool === true) {
 		document.getElementById("displayWinner").innerHTML = "X Wins!";
 	} else {
 		document.getElementById("displayWinner").innerHTML = "O Wins!";
+	}; for (let i = 0; i < squares.length; i++) {
+		var isClicked = squares[i];
+		isClicked.setAttribute("data-clicked", "true");
 	}
 };
 var checkDraw = function() {
-	//console.log("This means that winCondition works");
 	for (let i = 8; i < squares.length; i++) {
 		var boardCheck = squares[i].getAttribute('data-clicked');
 		if (boardCheck === "true") {
 			document.getElementById("displayWinner").innerHTML = "It's a draw";
 		} else {
-			//console.log("Waiting next move");
 		}
 	}
 };
 var winCondition = function(bool) {
-	//console.log('win condition is a work in progress');
 		   if (values[0] === values[1] && values[0] === values[2] && values[1] === values[2]) {
 		   	declareWinner(bool);
 		   } else if (values[3] === values[4] && values[3] === values[5] && values[4] === values[5]) {
@@ -77,17 +64,11 @@ var winCondition = function(bool) {
 		   } else {
 		   	checkDraw();
 		}
-};
-var scoreboard = function (point) {
-	var scoreX = 0 + point;
-	var scoreO = 0 + point;
-	//console.log("X Score: " + scoreX + " | " + "O Score: " + scoreO)
 }; 
 var playGame = function (box) {
 	//console.log('playGame run');
 	var isClicked = box.getAttribute("data-clicked");
 	if (isClicked === "true") {
-		//console.log("this square is occupado");
 	} else {
 		box.setAttribute('data-clicked', 'true');
 		if(turnValue) {
@@ -115,5 +96,9 @@ reset.addEventListener('click', function() {
 		isClicked.setAttribute('data-clicked', 'false');
 	}
 	document.getElementById("displayWinner").innerHTML = " ";
+	for (var i = 0; i < squares.length; i++) {
+		var newIndex = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+		squares[i].setAttribute("data-value", newIndex[i])
+		changeArrayValue();
+	}
 });
-
