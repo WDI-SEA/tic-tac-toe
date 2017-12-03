fc89c8a35e27eee73d16f1929e57f97a225d76ee
philpatt <phillip.s.patterson1@gmail.com>
2017-12-03 14:54:39 -0800

    winning logic attempt and styling

--- js/app.js
+++ js/app.js
@@ -14,77 +14,58 @@ var cells = document.querySelectorAll('.cell');
 var playerStart = document.getElementById('playerStart');
 var player1 = document.getElementById('player1');
 var player2 = document.getElementById('player2');
-
+var reset = document.getElementById('reset');
 var turn = 0;
-
-
-
-
-
-
-// var startGame = function() {
-   
-//    for(var i = 0; i < cells.length; i++) {
-//    	cells[i].innerHTML = '';
-
-// 	console.log('start game');
-
-// }
-
-
-
+var player2Notice = function(){
+	player2.style.display = 'inline-block';
+	player2.style.color = 'red';
+	player1.style.display = 'none';
+	playerStart.style.display = 'none';
+}
+var player1Notice = function(){
+	player1.style.display = 'inline-block';
+	player1.style.color = 'blue';
+	player2.style.display = 'none';
+	playerStart.style.display = 'none';
+}
 var cellsClicked = function(){
+	console.log('cellsClicked engaged')
 	turn++;
 	if (turn%2 === 0 ){
-	this.textContent= 'X';
-	this.style.color = 'red';
-	this.removeEventListener('click',cellsClicked)
-	var player1Notice = function(){
-		player1.style.display = 'inline-block';
-		player1.style.color = 'blue';
-		player2.style.display = 'none';
-		playerStart.style.display = 'none';
+		this.textContent= 'X';
+		this.style.color = 'red';
+		this.removeEventListener('click',cellsClicked)
+		player1Notice();
 	}
-	player1Notice();
-} else {
+	else{
 		this.textContent = 'O';
 		this.style.color = 'blue';
 		this.removeEventListener('click',cellsClicked)
-		var player2Notice = function(){
-		player2.style.display = 'inline-block';
-		player2.style.color = 'red';
-		player1.style.display = 'none';
-		playerStart.style.display = 'none';
-	}
-	player2Notice();
-};
+		player2Notice();
+	};
 }
-
 var addCellEventListener = function(){
+	console.log('eventlisteners engaged');
 	for(var i = 0; i < cells.length; i++){
 		cells[i].addEventListener('click', cellsClicked);
 	}
-
-}
-
-var addResetEventListener = function(){
-	var reset = document.getElementById('reset');
-	reset.addEventListener('click', function() {
-      console.log('click button');
-  });
-	addResetEventListener();
 }
 
 
+var newGame = function() {
+	for(var i = 0; i < cells.length; i++){
+		cells[i].innerHTML = '';
+	}
+	playerStart.style.display = 'inline-block';
+	player1.style.display = 'none';
+	player2.style.display = 'none';
+}
 
-
-// add start game function with for loop
-// function clear box
-
-document.addEventListener("DOMContentLoaded", function() {
-
-// startGame();
-addCellEventListener();
-
+reset.addEventListener('click', function(){ 
+	console.log('click');
+	newGame();
+	addCellEventListener();
 });
+
+addCellEventListener();
 
