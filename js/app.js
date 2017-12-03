2a59dafe0255fc9d8dcab38deece4cc0dcc25e1a
Nick Nedev <nedevn@gmail.com>
2017-12-02 17:15:54 -0800

    Fixes game logic

--- js/app.js
+++ js/app.js
@@ -28,10 +28,16 @@
     if (checkIfFree(cell)) {
       if (turn % 2 === 0) {
         cell.target.innerText = 'X';
+        if(checkIfWon(turn % 2)){
+            alert('x won');
+        }
       } else {
         cell.target.innerText = 'O';
+        if(checkIfWon(turn % 2)){
+            alert('o won');
+        }
       }
-      if(checkIfWon(board)){alert('you won');};
+      // if(checkIfWon(board)){alert('you won');};
       turn += 1;
     }
 
@@ -46,38 +52,73 @@
 
 
   function checkIfWon(player) {
-    //get row
-
     var rows = document.querySelectorAll('tr');
     var rowResult = [];
     var colResult = [];
     var diagRight = [];
     var diagLeft = [];
-    var cellsLength = null
-    player = 'XXX'
-    for (var i = 0; i < rows.length; i += 1) {
-        var last = rows[i].cells.length - i -1;
+    var winCondition = null
+    var symbol = null;
 
-         for (var j = 0; j < rows[i].cells.length; j += 1) { 
-            colResult.push(rows[j].cells[i].innerText);
-        rowResult.push(rows[i].cells[j].innerText);
-        
-      }  
-        diagLeft.push(rows[i].cells[last].innerText);
-        diagRight.push(rows[i].cells[i].innerText);
-       console.log(colResult);
-
-      if (rowResult.join('') === player || colResult.join('') === player || diagLeft.join('') === player || diagRight.join('') === player ) { 
-        return true; }
+    switch (player) {
+      case 0:
+        symbol = 'X';
+        winCondition = 'XXX'
+        break;
+      case 1:
+        symbol = "O";
+        winCondition = 'OOO'
     }
+
+    for (var i = 0; i < rows.length; i += 1) {
+      var last = rows[i].cells.length - i - 1;
+      for (var j = 0; j < rows[i].cells.length; j += 1) {
+        if (rows[i].cells[j].innerText === symbol) {
+          rowResult.push(rows[i].cells[j].innerText);
+        }
+        if (rows[j].cells[i].innerText === symbol) {
+          colResult.push(rows[j].cells[i].innerText);
+        }
+      }
+      diagLeft.push(rows[i].cells[last].innerText);
+      diagRight.push(rows[i].cells[i].innerText);
+      // console.log('colResult: '+colResult.filter((str)=>{return /\S/.test(str)}));
+      // console.log('Row result: ' + rowResult);
+      // console.log('Col result: ' + colResult);
+      // console.log('diagLeft: ' + diagLeft);
+      // console.log('diagRight: ' + diagRight);
+
+    if (rowResult.join('') === winCondition || colResult.join('') === winCondition) { 
+        return true }
+    rowResult = [];
+    colResult = [];
+    }
+    if (diagRight.join('') === winCondition || diagLeft.join('') === winCondition) {
+      return true
+    }
+    diagRight = [];
+    diagLeft = [];
+
+    // if (rowResult.join('') === 'XXX' || colResult.join('') === 'XXX' || diagLeft.join('') === 'XXX' || diagRight.join('') === 'XXX' ) { 
+    //     return true; }
+
 
 
   }
 
+  function getAllIndexes(arr, val) {
+    var indexes = []
+    var index = -1;
+    while ((iindex = arr.indexOf(val, index + 1)) != -1) {
+      indexes.push(i);
+    }
+    return indexes;
+  }
+
   // var rows = 3;
-  var cols = 3;
-  var board = document.getElementById('board');
-  var rows = document.querySelectorAll('tr');
+  // var cols = 3;
+  // var board = document.getElementById('board');
+  // var rows = document.querySelectorAll('tr');
 
 
 
