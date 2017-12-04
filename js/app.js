76ff2a94349aeb1331182e3d873eb903028b08a5
Orion Weldon <orion@Orions-MacBook-Pro.local>
2017-12-04 08:52:55 -0800

    somewwhat functional

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log('Hello frontend');
+var square = document.getElementsByClassName("square");
+var start = 0;
+var reset = document.getElementById('reset');
+var squareClicked = function(){
+  start++;
+  if(start%2===0){
+    document.getElementById("displayPlayer").textContent = "Player One, Hold Fast";
+    this.textContent = "X";
+    this.style.color = "gold";
+    this.style.border = "5px solid gold";
+    this.removeEventListener('click', squareClicked);
+  } else{
+    document.getElementById("displayPlayer").textContent = "Player Two At The Ready";
+    this.textContent = "O";
+    this.style.color = "silver";
+    this.removeEventListener('click', squareClicked);
+    }
+  }
+var click = function(){
+    for(var i=0; i < square.length; i++){
+      square[i].addEventListener('click', squareClicked);
+  }
+}
+var startGame = function() {
+   for (var i =1; i <= square.length; i++) {
+   clearBox(i)};
+   click();
+   resetGame();
+ }
+var clearBox = function(number) {
+    return document.getElementById("sq" + number).textContent = "";
+  }
+var resetGame = function(){
+  reset.addEventListener('click', startGame);
+}
+
+startGame();
