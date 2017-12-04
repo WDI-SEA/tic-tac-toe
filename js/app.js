b8fc927af28893e9c25daa46ed856632f1380575
peterpickles <lipeter2006@gmail.com>
2017-12-04 03:13:09 -0800

    deliverable complete-ish, without class color assignment

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log('Hello frontend');
+function startGame () {
+	document.currentPlayer = "X";
+}
+
+function nextTurn(col) {
+	if(col.innerText == '') {
+	   col.innerText = document.currentPlayer;
+	   switchPlayer();
+	}else {
+		setMessage("Choose another square! ")
+	}
+	
+}
+
+function setMessage(msg){
+	document.getElementById("message").innerText = msg;	
+}
+
+
+function switchPlayer(){
+	if(document.currentPlayer == "X") {
+		document.currentPlayer = "O";
+	} else {
+		document.currentPlayer = "X";
+	}
+
+}
