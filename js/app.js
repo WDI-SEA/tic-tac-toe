c56ab2a5c5918241a94211318b4a6ac741b8697d
philpatt <phillip.s.patterson1@gmail.com>
2017-12-05 19:54:26 -0800

    working on win conditions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
- var winCombos = [
+
+// this is for TIC TAC TOE
+
+
+
+ var winCombo= [
  [0, 1, 2],
  [3, 4, 5],
  [6, 7, 8],
@@ -7,14 +12,31 @@
  [2, 5, 8],
  [2, 4, 6],
  [0, 4, 8],
- ]
+ ];
 
+// Add minimax algorithm for hard AI 
+// store arrays for playermoves 
+// calculate player moves arrays
+// use switch statements to create win conditions
+
+
+
+// grabbing key elements from the DOM
 var cells = document.querySelectorAll('.cell');
 var playerStart = document.getElementById('playerStart');
 var player1 = document.getElementById('player1');
 var player2 = document.getElementById('player2');
-var reset = document.getElementById('reset');
+var resetBtn = document.getElementById('resetBtn');
+
+
+var player1Selection = []
+var player2Selection = []
+
+
 var turn = 0;
+
+// functions to add and remov displays to show whos turn it is
+
 var player2Notice = function(){
 	player2.style.display = 'inline-block';
 	player2.style.color = 'red';
@@ -27,28 +49,91 @@ var player1Notice = function(){
 	player2.style.display = 'none';
 	playerStart.style.display = 'none';
 }
+
+
+
+
+var everyTurn = function(param){
+	if(turn >=5){
+	    //pass same parameterinto check winner
+	    checkWinner(param);
+	}
+}
+
+
+var checkWinner = function(winner){
+  //grab the cells
+  // console.log('inside the check winner function',winner);
+  var allCells = document.querySelectorAll('.cell');
+  console.log(allCells);
+  //Loop through win combos
+  //if(allCells[0].someAttribute ==== winner && allCells[1].someAttribute === winner && allcells[2].someAttribute === winner){
+  	//x won
+  //}
+
+
+
+  
+
+	console.log(allCells[0].className === 'cell'+winner);
+    if(allCells[0].className === 'cell'+ winner && allCells[1].className === 'cell'+ winner && boxes[2].className === 'cell'+ winner){
+        // document.getElementById("result").innerHTML = winner.toUpperCase() + " Won!";
+        // removeEventListeners();
+	}
+}
+
+  //check if currentplayer is in any of the combos
+  //returns true if win is detected
+// determine alternating turns
 var cellsClicked = function(){
-	console.log('cellsClicked engaged')
+	var winner;
 	turn++;
 	if (turn%2 === 0 ){
 		this.textContent= 'X';
 		this.style.color = 'red';
-		this.removeEventListener('click',cellsClicked)
+		this.removeEventListener('click',cellsClicked);
+		console.log(this.id);
 		player1Notice();
+		player2Selection.push(this);
+		winner = " x";
+
 	}
 	else{
 		this.textContent = 'O';
 		this.style.color = 'blue';
-		this.removeEventListener('click',cellsClicked)
+		this.removeEventListener('click',cellsClicked);
 		player2Notice();
+		player1Selection.push(this);
+		winner = " o";
+
 	};
+
+	everyTurn(winner);
+
+	if(turn > 8){
+    	console.log('draw');
+    	// display none originally
+    	/////#######&#^&#(*#&$(*$#&)) Change DOM to display a draw
+  	}
 }
+
+// sets button to click that engages newGame and addCellEventListener
+resetBtn.addEventListener('click', function(){ 
+	console.log('click');
+	newGame();
+	addCellEventListener();
+});
+
+// 
 var addCellEventListener = function(){
 	console.log('eventlisteners engaged');
 	for(var i = 0; i < cells.length; i++){
 		cells[i].addEventListener('click', cellsClicked);
 	}
 }
+
+
+// newGame function that 
 var newGame = function() {
 	for(var i = 0; i < cells.length; i++){
 		cells[i].innerHTML = '';
@@ -56,13 +141,10 @@ var newGame = function() {
 	playerStart.style.display = 'inline-block';
 	player1.style.display = 'none';
 	player2.style.display = 'none';
+	turn = 0;
 }
 
-reset.addEventListener('click', function(){ 
-	console.log('click');
-	newGame();
-	addCellEventListener();
-});
+
 
 addCellEventListener();
 
