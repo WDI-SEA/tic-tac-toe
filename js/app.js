217889f40926fdd2a63d923015dd87d3650a8a87
rallysport206 <phan.tony125@gmail.com>
2017-12-07 08:39:07 -0800

    working removeEventListener

--- js/app.js
+++ js/app.js
@@ -14,34 +14,53 @@ var win = [
   [6, 4, 2],
 ];
 
-// box clicked
-var board = function () {
-    for (var i=0; i<box.length; i++){
-    box[i].addEventListener('click', function(){
-      turn ++;
-      if(turn % 2 === 1){
-        this.className = 'tileX';
-        console.log('clickx');
-        removeTile();
-        // document.getElementById('playerChange').textContent = "Player O is Up";
-        // this.removeEventListener('click', tileSelect);
-      }else{
-        this.className = 'tileO';
-        console.log('clicko');
-        removeTile();
-        // document.getElementById('playerChange').textContent= "Player X is Up";
-        // this.removeEventListener('click', tileSelect);
-  }});
+var boxListeners = function () {
+    for (var i = 0; i < box.length; i++){
+      box[i].addEventListener('click', boxClick);
+      console.log("listeners added");
+    }
 }
-}
-board();
-var removeTile = function () {
-  var boardBox = document.querySelectorAll('tile');
-  for(var i=0; i<box.length; i++){
-    console.log('remove');
-    this.removeEventListener('click',board);
+boxListeners();
+function boxClick(){
+  turn++
+  if (turn % 2 ===1){
+    this.className = 'tileX';
+    console.log('click X' + 'on turn ' + turn);
+  } else {
+    this.className = 'tileO';
+    console.log('click O ' + "on turn " + turn);
   }
+  this.removeEventListener('click', boxClick);
+  console.log("listener removed");
 }
+// box clicked
+// var board = function () {
+//     for (var i=0; i<box.length; i++){
+//     box[i].addEventListener('click', function(){
+//       turn ++;
+//       if(turn % 2 === 1){
+//         this.className = 'tileX';
+//         console.log('clickx');
+//         removeTile();
+//         // document.getElementById('playerChange').textContent = "Player O is Up";
+//         // this.removeEventListener('click', tileSelect);
+//       }else{
+//         this.className = 'tileO';
+//         console.log('clicko');
+//         removeTile();
+//         // document.getElementById('playerChange').textContent= "Player X is Up";
+//         // this.removeEventListener('click', tileSelect);
+//   }});
+// }
+// }
+// board();
+// var removeTile = function () {
+//   var boardBox = document.querySelectorAll('tile');
+//   for(var i=0; i<box.length; i++){
+//     console.log('remove');
+//     this.removeEventListener('click',board);
+//   }
+// }
 
 restart.addEventListener('click', function(){
   window.location.reload();
