6bc931583a7a7970ae09037fb86cf95065255623
peterpickles <lipeter2006@gmail.com>
2017-12-06 17:49:46 -0800

    added changes and fix shifting div

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
-console.log('Hello frontend');
+
+///add a class instead of replacing class
+// element.className += " xPlayed";
 function startGame () {
 	document.currentPlayer = "X";
 }
+
+///You need to write a check winner function
+//you're going to call checkWinner() at the end of every turn;
+
+var boxes = document.getElementsByClassName("col");
+console.log(boxes);//this is an array of all the divs
 
 function nextTurn(col) {
 	if(col.innerText == '') {
 	   col.innerText = document.currentPlayer;
 	   switchPlayer();
 	}else {
-		setMessage("Choose another square! ")
+		setMessage("Choose another square! ");
 	}
-	
 }
 
 function setMessage(msg){
 	document.getElementById("message").innerText = msg;	
 }
-
 
 function switchPlayer(){
 	if(document.currentPlayer == "X") {
@@ -24,5 +30,4 @@ function switchPlayer(){
 	} else {
 		document.currentPlayer = "X";
 	}
-
 }
