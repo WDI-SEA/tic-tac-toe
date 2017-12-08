06336a49c2ca0bb355c7288f4b676f28e0f42c8a
Desiree Rainey <Drainey21.DR@gmail.com>
2017-12-08 00:09:14 -0800

    win condition done yayyyyyy

--- js/app.js
+++ js/app.js
@@ -1,66 +1,67 @@
+
+  var index=boxes
 var str="";
 var isStrX=true
-var boxes=document.querySelectorAll(".grid-element"); //get all grid element divs
-	boxes=Array.from(boxes); //create array of boxes
+var boxes=document.querySelectorAll(".grid-element");
+boxes=Array.from(boxes);
+// var overlay=document.querySelector(".overlay");
+var notifiar=document.querySelector("h1");
 
-
-//on boxes click use pushText function
 for (var i = 0; i < boxes.length; i++) {
-   boxes[i].addEventListener("click",pushText);
- }
-
-
-//pushes x or o 
-//str left open for "x" and "o"
-function pushText () {
-   var index=boxes.indexOf(this);
-if (isStrX) {
-   str="X";
-   isStrX=false;
-   console.log(index)
-   }
-else{
-   str="O";
-   isStrX=true;
-   } 
-
-
-
-
-//gets info from each line of boxes and changes into string 
-
-	this.innerHTML=str;
-
-
-checking(0,1,2);
-checking(3,4,5);
-checking(6,7,8);
-
-checking(0,3,6);
-checking(1,4,7);
-checking(2,5,8);
-
-checking(0,4,8);
-checking(2,4,6);
-
+    boxes[i].addEventListener("click",pushText);
+  boxes[i].removeEventListener("click", index);
 }
 
 
 
+function pushText () {
+  
+  var index=boxes.indexOf(this);
+
+  if (isStrX) {
+    str="X";
+    isStrX=false;
+    console.log(index)
+  }
+  else{
+    str="O";
+    isStrX=true;
+  }
+  this.innerHTML=str;
+
+
+  checking(0,1,2);
+  checking(3,4,5);
+  checking(6,7,8);
+
+  checking(0,3,6);
+  checking(1,4,7);
+  checking(2,5,8);
+
+  checking(0,4,8);
+  checking(2,4,6);
+
+}
+
+//show overlay when win is calculated....?
 function checking (num1,num2,num3) {
+
   if (
-	boxes[num1]===boxes[num2] && 
-	boxes[num2]===boxes[num3]&& 
-	boxes[num1]===boxes[num3]&& 
-	boxes[num1]!=="" &&
-	boxes[num2]!=="" &&
-	boxes[num3]!=="" 
+      boxes[num1].innerHTML.trim()===boxes[num2].innerHTML.trim() && 
+      boxes[num2].innerHTML.trim()===boxes[num3].innerHTML.trim() &&
+      boxes[num1].innerHTML.trim()===boxes[num3].innerHTML.trim() &&
+      boxes[num1].innerHTML.trim()!=="" &&
+      boxes[num2].innerHTML.trim()!=="" &&
+      boxes[num3].innerHTML.trim()!=="" 
     )
-    console.log(num1,num2,num3)
-};
+    {
+      notifiar.innerHTML=str+" wins";
+      console.log(num1,num2,num3);
+    }
+}
 
-  function reloadGame() {
-	location.reload();
-	}
-        	
 
+function reloadGame() {
+location.reload()
+}
+       
