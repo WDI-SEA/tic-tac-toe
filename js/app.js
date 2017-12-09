a5c66be3f02bd3c37151590c372277cacfa93f01
rallysport206 <phan.tony125@gmail.com>
2017-12-09 10:58:09 -0800

    added rest of win logic, minor css update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 var box = document.getElementsByClassName('tile');
 var turn = 0;
 var restart = document.getElementById('reset');
-var win = document.getElementById('sq')
 
 var boxListeners = function () {
     for (var i = 0; i < box.length; i++){
@@ -21,23 +20,39 @@ function boxClick(){
     console.log('click O ' + 'on turn' + turn);
     checkWin(" tileO");
   }
-  this.removeEventListener('click', boxClick);
-  console.log("listener removed");
+    this.removeEventListener('click', boxClick);
+    console.log("listener removed");
 }
 var checkWin = function (winner) {
   var boxes = document.querySelectorAll('.tile');
     if(boxes[0].className === 'tile'+ winner && boxes[1].className === 'tile' + winner && boxes[2].className === 'tile' + winner){
-      document.getElementById('headline').textContent = winner + " Won!";
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListener();
+    }else if(boxes[3].className === 'tile'+ winner && boxes[4].className === 'tile' + winner && boxes[5].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListener();
+    }else if(boxes[6].className === 'tile'+ winner && boxes[7].className === 'tile' + winner && boxes[8].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListeners();
+    }else if(boxes[0].className === 'tile'+ winner && boxes[3].className === 'tile' + winner && boxes[6].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListeners();
+    }else if(boxes[1].className === 'tile'+ winner && boxes[4].className === 'tile' + winner && boxes[7].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListeners();
+    }else if(boxes[2].className === 'tile'+ winner && boxes[5].className === 'tile' + winner && boxes[8].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListeners();
+    }else if(boxes[0].className === 'tile'+ winner && boxes[4].className === 'tile' + winner && boxes[8].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListeners();
+    }else if(boxes[2].className === 'tile'+ winner && boxes[4].className === 'tile' + winner && boxes[6].className === 'tile' + winner){
+      document.getElementById('winMessage').textContent = winner + " is your winner!";
+      removeEventListeners();
+    }else if(boxClick === 8){
+      document.getElementById('winMessage').textContent = winner + " This match is a draw!";
     }
 }
-// function checkWin() {
-//   if (tileX)
-// }
-//
-// function boxScan(){
-//   return document.getElementById('sq' + ).className;
-// }
-
 restart.addEventListener('click', function(){
   window.location.reload();
 });
