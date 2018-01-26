99dde01519786ce4e65181b0b07751dbc15fbd19
Sean Cesmat <sean.cesmat@gmail.com>
2018-01-25 22:58:41 -0800

    Added more color | Cleaned up code | save before adding ai

--- js/app.js
+++ js/app.js
@@ -1,67 +1,98 @@
 var currentPlay = 0;
 var resetButton = document.getElementById('reset');
-var element = document.getElementsByClassName("square");
-var squaresArr = [];
+var squaresArr = document.querySelectorAll('.square');
+var currentPlayElement = document.getElementById('current-play');
+
 var displayCurrentPlay = function() {
   if (currentPlay % 2 ===0) {
     whosTurn = "X's turn";
   } else {
     whosTurn = "O's turn"
   }
-  document.getElementById('current-play').innerHTML = currentPlay + 1 + "  " + whosTurn;
+  currentPlayElement.textContent = currentPlay + 1 + "  " + whosTurn;
 
 }
 displayCurrentPlay();
 
-for (var i = 0; i <= 8; i++) {
-  squaresArr.push(element[i]);
-  element[i].addEventListener('click', function() {
-    if (currentPlay % 2 === 0 && !this.hasChildNodes()) {
-      this.innerHTML = 'X';
-      this.className += ' color1';
-      currentPlay++;
-      if (currentPlay <= 8){
-        displayCurrentPlay();
-      } else {
-        document.getElementById('current-play').innerHTML = 'Game Over';
-      }
-    } else if (currentPlay % 2 !== 0 && !this.hasChildNodes()){
-      this.innerHTML = 'O';
-      this.className += ' color2';
-      currentPlay++;
-      if (currentPlay <= 8){
-        displayCurrentPlay();
-      } else {
-        document.getElementById('current-play').innerHTML = 'Game Over';
-      }
+// changeSquare function fired when square is clicked
+var changeSquare = function() {
+  if (currentPlay % 2 === 0 && !this.hasChildNodes()) {
+    this.textContent = 'X';
+    this.className += ' color1';
+    currentPlay++;
+    if (currentPlay <= 8){
+      displayCurrentPlay();
+    } else {
+      currentPlayElement.textContent = 'Game Over';
     }
-    // check to see if anyone has won yet
-    if (squaresArr[0].innerHTML !== '' && squaresArr[0].innerHTML === squaresArr[1].innerHTML && squaresArr[1].innerHTML === squaresArr[2].innerHTML ) {
-      alert(squaresArr[0].innerHTML + " Wins!!");
-    } else if (squaresArr[3].innerHTML !== '' && squaresArr[3].innerHTML === squaresArr[4].innerHTML && squaresArr[4].innerHTML === squaresArr[5].innerHTML ) {
-      alert(squaresArr[3].innerHTML + " Wins!!");
-    } else if (squaresArr[6].innerHTML !== '' && squaresArr[6].innerHTML === squaresArr[7].innerHTML && squaresArr[7].innerHTML === squaresArr[8].innerHTML ) {
-      alert(squaresArr[6].innerHTML + " Wins!!");
-    } else if (squaresArr[0].innerHTML !== '' && squaresArr[0].innerHTML === squaresArr[4].innerHTML && squaresArr[4].innerHTML === squaresArr[8].innerHTML ) {
-      alert(squaresArr[0].innerHTML + " Wins!!");
-    } else if (squaresArr[2].innerHTML !== '' && squaresArr[2].innerHTML === squaresArr[4].innerHTML && squaresArr[4].innerHTML === squaresArr[6].innerHTML ) {
-      alert(squaresArr[2].innerHTML + " Wins!!");
-    } else if (squaresArr[0].innerHTML !== '' && squaresArr[0].innerHTML === squaresArr[3].innerHTML && squaresArr[3].innerHTML === squaresArr[6].innerHTML ) {
-      alert(squaresArr[0].innerHTML + " Wins!!");
-    } else if (squaresArr[1].innerHTML !== '' && squaresArr[1].innerHTML === squaresArr[4].innerHTML && squaresArr[4].innerHTML === squaresArr[7].innerHTML ) {
-      alert(squaresArr[1].innerHTML + " Wins!!");
-    } else if (squaresArr[2].innerHTML !== '' && squaresArr[2].innerHTML === squaresArr[5].innerHTML && squaresArr[5].innerHTML === squaresArr[8].innerHTML ) {
-      alert(squaresArr[2].innerHTML + " Wins!!");
+  } else if (currentPlay % 2 !== 0 && !this.hasChildNodes()){
+    this.textContent = 'O';
+    this.className += ' color2';
+    currentPlay++;
+    if (currentPlay <= 8){
+      displayCurrentPlay();
+    } else {
+      currentPlayElement.textContent = 'Game Over';
     }
-  });
+  }
+  // check to see if anyone has won yet
+  if (squaresArr[0].textContent !== '' && squaresArr[0].textContent === squaresArr[1].textContent && squaresArr[1].textContent === squaresArr[2].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[0].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[3].textContent !== '' && squaresArr[3].textContent === squaresArr[4].textContent && squaresArr[4].textContent === squaresArr[5].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[3].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[6].textContent !== '' && squaresArr[6].textContent === squaresArr[7].textContent && squaresArr[7].textContent === squaresArr[8].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[6].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[0].textContent !== '' && squaresArr[0].textContent === squaresArr[4].textContent && squaresArr[4].textContent === squaresArr[8].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[0].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[2].textContent !== '' && squaresArr[2].textContent === squaresArr[4].textContent && squaresArr[4].textContent === squaresArr[6].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[2].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[0].textContent !== '' && squaresArr[0].textContent === squaresArr[3].textContent && squaresArr[3].textContent === squaresArr[6].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[0].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[1].textContent !== '' && squaresArr[1].textContent === squaresArr[4].textContent && squaresArr[4].textContent === squaresArr[7].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[1].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } else if (squaresArr[2].textContent !== '' && squaresArr[2].textContent === squaresArr[5].textContent && squaresArr[5].textContent === squaresArr[8].textContent ) {
+    setTimeout(function() {
+      alert(squaresArr[2].textContent + " Wins!!");
+      resetBoard();
+    }, 400);
+  } // END -- check to see if anyone has won yet
 }
+// END changeSquare function
 
-resetButton.addEventListener('click', function() {
+// Add Click Event Listener To Each Square
+for (var i = 0; i <= 8; i++) {
+  squaresArr[i].addEventListener('click', changeSquare, false);
+}
+var resetBoard = function() {
   for (var i = 0; i <= 8; i++) {
-    element[i].innerHTML = '';
-    element[i].classList.remove('color1');
-    element[i].classList.remove('color2');
+    squaresArr[i].textContent = '';
+    squaresArr[i].classList.remove('color1');
+    squaresArr[i].classList.remove('color2');
     displayCurrentPlay();
     currentPlay = 0;
   }
-});
+}
+resetButton.addEventListener('click', resetBoard, false);
+
+document.getElementById('game-name').style.transform = "rotate(-90deg)";
