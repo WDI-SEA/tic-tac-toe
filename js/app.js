97c2b8e49ed5c0ea9d329c056443bf2fb9454d48
emily <emilyhannan@D.local>
2018-01-26 13:18:49 -0800

    new changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hello frontend');
+console.log('TIC TAC TOE IS WORKING');
+
+
+
+// set event listener for click
+// on click change class = color
+// on click change text content = x or o
+
+// check combination of clicked objects for winning combination
+//if (combo (sq & sq & sq = true) that's a win
+
+//alert "win"
+
+// if all squares clicked = 9 no wins, display nobody wins
+
+
+sq 1 === sq 2 === sq 3 = wins
+sq 1 === sq 5 === sq 9 = wins
