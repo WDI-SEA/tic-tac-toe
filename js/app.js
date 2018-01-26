03a278950b00c5562460b008c647132fd13f36ff
Sean Cesmat <sean.cesmat@gmail.com>
2018-01-25 22:00:10 -0800

    Changed colors & save before starting trying to clean up code

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@ var resetButton = document.getElementById('reset');
 var element = document.getElementsByClassName("square");
 var squaresArr = [];
 var displayCurrentPlay = function() {
-  document.getElementById('current-play').innerHTML = currentPlay + 1;
+  if (currentPlay % 2 ===0) {
+    whosTurn = "X's turn";
+  } else {
+    whosTurn = "O's turn"
+  }
+  document.getElementById('current-play').innerHTML = currentPlay + 1 + "  " + whosTurn;
+
 }
 displayCurrentPlay();
 
@@ -11,9 +17,8 @@ for (var i = 0; i <= 8; i++) {
   squaresArr.push(element[i]);
   element[i].addEventListener('click', function() {
     if (currentPlay % 2 === 0 && !this.hasChildNodes()) {
-      //console.log(this);
       this.innerHTML = 'X';
-      this.className += ' red';
+      this.className += ' color1';
       currentPlay++;
       if (currentPlay <= 8){
         displayCurrentPlay();
@@ -22,16 +27,14 @@ for (var i = 0; i <= 8; i++) {
       }
     } else if (currentPlay % 2 !== 0 && !this.hasChildNodes()){
       this.innerHTML = 'O';
-      this.className += ' blue';
+      this.className += ' color2';
       currentPlay++;
       if (currentPlay <= 8){
         displayCurrentPlay();
       } else {
         document.getElementById('current-play').innerHTML = 'Game Over';
       }
-
     }
-
     // check to see if anyone has won yet
     if (squaresArr[0].innerHTML !== '' && squaresArr[0].innerHTML === squaresArr[1].innerHTML && squaresArr[1].innerHTML === squaresArr[2].innerHTML ) {
       alert(squaresArr[0].innerHTML + " Wins!!");
@@ -50,17 +53,14 @@ for (var i = 0; i <= 8; i++) {
     } else if (squaresArr[2].innerHTML !== '' && squaresArr[2].innerHTML === squaresArr[5].innerHTML && squaresArr[5].innerHTML === squaresArr[8].innerHTML ) {
       alert(squaresArr[2].innerHTML + " Wins!!");
     }
-
   });
-  //console.log(document.getElementsByClassName("square")[i]);
-  //console.log(squaresArr);
 }
 
 resetButton.addEventListener('click', function() {
   for (var i = 0; i <= 8; i++) {
     element[i].innerHTML = '';
-    element[i].classList.remove('red');
-    element[i].classList.remove('blue');
+    element[i].classList.remove('color1');
+    element[i].classList.remove('color2');
     displayCurrentPlay();
     currentPlay = 0;
   }
