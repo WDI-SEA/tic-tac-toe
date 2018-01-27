9e079000e2b6449f979ac9dece04b635e804e64e
Kyle Van Bergen <kyleavb@gmail.com>
2018-01-26 18:59:52 -0800

    basic layout, added custom images, starting script

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
 var allSquares = document.getElementsByClassName("square");
-var resetBut = document.getElementById("resetButton")
-function dataCheck(){
-  console.log("click")
+var uiTurn = document.getElementById("turn");
+var opp;
+var turn = 1;
+//for Debug only
+function dataCheck(str){
+  console.log(str)
 }
-resetBut.addEventListener("click", dataCheck);
-allSquares[0].addEventListener("click", dataCheck);
+var clearBoard = function (){
+  turn = 1;
+  uiTurn.textContent = 0
+  for(var i=0; i<allSquares.length; i++){
+    allSquares[i].style.background = "blue";
+    allSquares[i].classList.remove("taken");
+  }
+}
+
+var squareSelected = function (){
+
+  if(turn%2 === 1 && !this.classList.contains("taken")){
+      this.style.background = "url('./img/x.png')";
+      this.classList.add("taken");
+      uiTurn.textContent = turn;
+      turn++;
+  }else if(!this.classList.contains("taken")){
+    this.style.background = "url('./img/o.png')";
+    this.classList.add("taken");
+    uiTurn.textContent = turn;
+    turn++;
+  }
+}
+
+//onLoad
+document.addEventListener("DOMContentLoaded", function(){
+
+  var rad = document.getElementsByName("gameType");
+    for(var i=0;i<rad.length;i++){
+      if(rad[i].checked){
+        for(var i=0; i<allSquares.length; i++){
+          allSquares[i].addEventListener("click", squareSelected);
+        }
+        document.getElementById("resetButton").addEventListener("click", clearBoard);
+      }
+  }
+});
