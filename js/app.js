80b6a7ab404e3996c0e91af16408d818d06752ad
Daniel Heyward <danheyward@gmail.com>
2018-01-26 20:52:06 -0800

    built initial game board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log('Hello frontend');
+
+var move = document.querySelectorAll(".square");
+
+move.forEach(function(squareClick) {
+  squareClick.addEventListener("click", function() {
+    if (squareClick.classList.contains("square")) {
+      console.log("clicked!");
+      squareClick.classList.remove("square");
+      squareClick.classList.add("square-clicked");
+    }
+  });
+});
