158f833464d7fe8d8e269ce24a43ab086cc9ca7a
Kyle Van Bergen <kyleavb@gmail.com>
2018-01-26 16:44:05 -0800

    getting started

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log('Hello frontend');
+var allSquares = document.getElementsByClassName("square");
+var resetBut = document.getElementById("resetButton")
+function dataCheck(){
+  console.log("click")
+}
+resetBut.addEventListener("click", dataCheck);
+allSquares[0].addEventListener("click", dataCheck);
