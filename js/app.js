600522ea2b4370969f9cbbe43a0e60610054a22c
Chelsea Zhu <qianwei5668@gmail.com>
2018-01-27 09:58:10 -0800

    placeKey

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-console.log('Hello frontend');
+var turn = 0;
+var gameResult = false;
+
+var reset = function () {
+
+}
+
+var placeKey = function () {
+	var key = null;
+	if (turn % 2 === 0) {
+		key = "treeKey"
+		} else {key = "birdKey"
+	}
+	this.classList.add(key);
+	turn ++;
+}
+
+var checkWin = function () {
+
+}
+
+var displayResult = function () {
+
+}
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	document.getElementById("a1").addEventListener("click",placeKey);
+	document.getElementById("a2").addEventListener("click",placeKey);
+	document.getElementById("a3").addEventListener("click",placeKey);
+	document.getElementById("b1").addEventListener("click",placeKey);
+	document.getElementById("b2").addEventListener("click",placeKey);
+	document.getElementById("b3").addEventListener("click",placeKey);
+	document.getElementById("c1").addEventListener("click",placeKey);
+	document.getElementById("c2").addEventListener("click",placeKey);
+	document.getElementById("c3").addEventListener("click",placeKey);
+	document.getElementById("resetButton").addEventListener("click",reset);
+})
