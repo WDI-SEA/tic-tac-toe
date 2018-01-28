0c9688a884025bda4e18fcb058b7a911ba4a9661
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-01-28 11:45:29 -0800

    new changes to app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,1 @@
-// set event listener for click
-// on click change class = color
-// on click change text content = x or o
-
-// check combination of clicked objects for winning combination
-//if (combo (sq & sq & sq = true) that's a win
-
-//alert "win"
-
-// if all squares clicked = 9 no wins, display nobody wins
-
-var move =
-
-document.getElementsByClass("box").addEventListener("click", function() {
-
-  box.style.background-color: white;
-}
+var turn = 0
---
+++ queue.md
@@ -0,0 +1,12 @@
+// set up grid
+
+//clickable boxes
+  //click counter to say odd click "x" even click "o"
+ //when i click it turns x or o
+
+
+//check win function
+if [box]
+
+//reference
+winstonsummers 
