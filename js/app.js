48731c5eaded2725f3185321ab8d092b30975433
Emily Garrett <emilygarrett@Emilys-MacBook-Pro.local>
2018-01-27 23:51:10 -0800

    added audio, animations, scoreboard

--- js/app.js
+++ js/app.js
@@ -4,7 +4,29 @@ var turnRecord = document.getElementById("player");
 //reset button
 var resBut = document.getElementById("reset");
 
+//deals w scoreboards
+var words = document.querySelector('p');
+var sithScore = document.getElementById('scoreboardSith');
+var rebScore = document.getElementById('scoreboardReb');
+
+//gets all the divs holding the backgrounds for tiles
 var gameBoard = document.querySelectorAll("#gameboard > .unit");
+
+//initializes the animated ships
+var tieFighter = document.getElementById("ship1");
+var xWing = document.getElementById('ship2');
+
+//resets the animated ships to default
+var tieTimeout = setTimeout(function () {
+	tieFighter.className = "invisShip";
+}, 1000);
+var xTimeout = setTimeout(function () {
+	xWing.className = "invisShip";
+}, 1000);
+
+//audio tracks
+var tieFly = document.getElementById('fly');
+var tieShoot = document.getElementById('fire');
 
 //variable initialization for counters
 var turnCount = 0;
@@ -23,7 +45,6 @@ var swapImage = function() {
 		turnRecord.style.color="lightgreen";
 		turnRecord.innerText ="Rebellion's";
 		checkIfWin();
-		//this.parentElement.style.boxShadowColor="lightgreen";
 		turnCount++;
 	} else {
 		this.className = "green";
@@ -40,6 +61,10 @@ for (var i = 0; i < imgs.length; i++) {
 	imgs[i].addEventListener("click", swapImage);
 }
 
+var winner = null;
+var sScore = 0;
+var rScore = 0;
+
 //HECK 
 //these iterators collect whether or not the green/red blocks (parent) classes
 //contain any of the winning class names. how to determine if 3 match??
@@ -49,16 +74,36 @@ var checkIfWin = function() {
 		if (
 	      gameBoard[0].firstElementChild.className == gameBoard[1].firstElementChild.className && gameBoard[1].firstElementChild.className == gameBoard[2].firstElementChild.className  ||
 	      gameBoard[3].firstElementChild.className == gameBoard[4].firstElementChild.className && gameBoard[4].firstElementChild.className == gameBoard[5].firstElementChild.className  ||
-	      gameBoard[6].firstElementChild.className == gameBoard[7].firstElementChild.className && gameBoard[7].firstElementChild.className == gameBoard[8]  ||
+	      gameBoard[6].firstElementChild.className == gameBoard[7].firstElementChild.className && gameBoard[7].firstElementChild.className == gameBoard[8].firstElementChild.className  ||
 	      gameBoard[0].firstElementChild.className == gameBoard[3].firstElementChild.className && gameBoard[3].firstElementChild.className == gameBoard[6].firstElementChild.className  ||
 	      gameBoard[1].firstElementChild.className == gameBoard[4].firstElementChild.className && gameBoard[4].firstElementChild.className == gameBoard[7].firstElementChild.className  ||
-	      gameBoard[2].firstElementChild.className == gameBoard[5].firstElementChild.className && gameBoard[5].firstElementChild.className == gameBoard[8] ||
-	      gameBoard[0].firstElementChild.className == gameBoard[4].firstElementChild.className && gameBoard[4].firstElementChild.className == gameBoard[8]  ||
+	      gameBoard[2].firstElementChild.className == gameBoard[5].firstElementChild.className && gameBoard[5].firstElementChild.className == gameBoard[8].firstElementChild.className ||
+	      gameBoard[0].firstElementChild.className == gameBoard[4].firstElementChild.className && gameBoard[4].firstElementChild.className == gameBoard[8].firstElementChild.className  ||
 	      gameBoard[2].firstElementChild.className == gameBoard[4].firstElementChild.className && gameBoard[4].firstElementChild.className == gameBoard[6].firstElementChild.className 
 	    ) {
 	      console.log("we have a winner!");
-	      document.querySelector('p').innerText = "We have a winner!!!";
+	       if (turnRecord.style.color === "lightgreen") {
+	      	winner = 1;
+	      	sScore++;
+	      	console.log('sith wins');
+	      	sithScore.innerHTML = sScore;
+	      } else {
+	      	winner = 2;
+	      	rScore++;
+	      	console.log('rebels win');
+	      	rebScore.innerHTML = rScore;
+	      }
 
+	      tieFly.play();
+	      var shootTimeout = setTimeout(function (){
+				tieShoot.play();
+			}, 1200);
+
+	      words.innerText = "A winner, we have!!!";
+	      words.style.color = "cyan";
+	      words.style.fontWeight = 600;
+	      xWing.className = "animateX";
+	      tieFighter.className = "animateTie";
 	      for (var i = 0; i < 9; i++) {
 	      	imgs[i].removeEventListener('click', swapImage);
 	      }
@@ -66,7 +111,6 @@ var checkIfWin = function() {
 	      return true;
 
 	    } else {
-
 	      return false;
 	    }
 	}
@@ -82,10 +126,13 @@ reset.onclick = function () {
 		turnRecord.style.color="lightgreen";
 		turnRecord.innerText ="Rebellion's";
 	}
-	document.querySelector('p').innerHTML = "It's the " + "<span id='player'>Rebellion's</span>" + " turn!";
+	words.innerHTML = "The " + "<span id='player'>Rebellion's</span>" + " turn, it is!";
+	words.style.color = "black";
+	words.style.fontWeight = 200;
+	xWing.className = 'invisShip';
+	tieFighter.className = 'invisShip';
 	turnCount=0;
-	results['scoreTrackGreen'] = 0;
-	results['scoreTrackRed'] = 0;
+	winner=0;
 }
 
 
