c35e047aff68b0c2cb356e67156ad624cd016f9c
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-01-29 00:30:06 -0800

    Tim Hass - Tic Tac Toe Game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-console.log('Hello frontend');
 // object that holds playerO
-// https://gomakethings.com/check-if-two-arrays-or-objects-are-equal-with-javascript/
-// https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
 var playerO = {
   id: "playerO",
   currentPlayer: false,
@@ -21,54 +18,118 @@ var playerX = {
 var gameBoard = {
   src: "./img/frame.png",
   moveCount: 0,
-  possibleWins: [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
-  ]
+  gameEnd: false,
+  gameCount: 0,
+  match: []
 };
-var gameCount = 0;
-var gameOver = false;
 
 //reset the game
 var newGame = function(){
-  // console.log("newGame called");
+  setMatch();
   playerO.plays = [];
   playerX.plays = [];
+  gameBoard.gameEnd = false;
   gameBoard.moveCount = 0;
-  gameCount++;
-  var displayCount = document.getElementById("gameCount").textContent = gameCount;
+  gameBoard.gameCount++;
+  var displayCount = document.getElementById("gameCount").textContent = gameBoard.gameCount;
   loadGamePieces();
+  currentPlayer();
 };
+
+// set winning pieces
+var setMatch = function(){
+    var winMatch = document.getElementsByClassName("gamePiece");
+    var matches = Array.from(gameBoard.match);
+    if (gameBoard.gameEnd){
+      for (var i = 0; i < matches.length; i++){
+        winMatch[matches[i]-1].classList.remove("win");
+        gameBoard.match = [];
+      }
+    } else {
+      for (var i = 0; i < matches.length; i++){
+        winMatch[matches[i]-1].classList.add("win");
+        gameBoard.gameEnd = true;
+      }
+    }
+}
+
+// end the game
+var endGame = function(){
+  var el = document.getElementsByTagName("img");
+  for (var i = 0; i < el.length; i++){
+    el[i].removeEventListener("click", flipImage);
+  }
+  gameBoard.gameCount++;
+  var displayCount = document.getElementById("gameCount").textContent = gameBoard.gameCount;
+
+  // update player score
+  if (playerO.currentPlayer){
+    playerO.score++;
+    var gameWon = document.getElementById("playerOScore").textContent = playerO.score;
+  } else if (playerX.currentPlayer) {
+    playerX.score++;
+    var gameWon = document.getElementById("playerXScore").textContent = playerX.score;
+  }
+  setMatch();
+}
+
+// check to see if game is over
+var gameOver = function(array){
+  var arrayToCompare = Array.from(array);
+  arrayToCompare.sort();
+  arrayLength = arrayToCompare.length;
+  // check the winning possibilities
+  switch (arrayLength >= 3){
+    case arrayToCompare.indexOf(1) >-1 && arrayToCompare.indexOf(2) >-1 && arrayToCompare.indexOf(3) >-1:
+      gameBoard.match = [1,2,3];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(4) >-1 && arrayToCompare.indexOf(5) >-1 && arrayToCompare.indexOf(6) >-1:
+      gameBoard.match = [4,5,6];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(7) >-1 && arrayToCompare.indexOf(8) >-1 && arrayToCompare.indexOf(9) >-1:
+      gameBoard.match = [7,8,9];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(1) >-1 && arrayToCompare.indexOf(4) >-1 && arrayToCompare.indexOf(7) >-1:
+      gameBoard.match = [1,4,7];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(2) >-1 && arrayToCompare.indexOf(5) >-1 && arrayToCompare.indexOf(8) >-1:
+      gameBoard.match = [2,5,8];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(3) >-1 && arrayToCompare.indexOf(6) >-1 && arrayToCompare.indexOf(9) >-1:
+      gameBoard.match = [3,6,9];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(1) >-1 && arrayToCompare.indexOf(5) >-1 && arrayToCompare.indexOf(9) >-1:
+      gameBoard.match = [1,5,9];
+      endGame();
+      break;
+    case arrayToCompare.indexOf(3) >-1 && arrayToCompare.indexOf(5) >-1 && arrayToCompare.indexOf(7) >-1:
+      gameBoard.match = [3,5,7];
+      endGame();
+      break;
+  }
+}
 
 //check the game to see if someone won
 var checkGame = function(){
-  console.log("check game called");
-  if (playerO.currentPlayer && playerO.plays.length > 1){
+  if (playerO.currentPlayer && playerO.plays.length >= 3){
     // evaluate playerO plays with possibleWins
-    console.log("check playerO");
-    for (play in playerO.plays){
-      console.log(playerO.plays[play]);
-
-    }
-    for (wins in gameBoard.possibleWins){
-      console.log(gameBoard.possibleWins[wins]);
-
-    }
-  } else if (playerX.currentPlayer && playerX.plays.length > 1){
+    //compare the possible wins with the input
+    gameOver(playerO.plays);
+  } else if (playerX.currentPlayer && playerX.plays.length >= 3){
     // evaluate playerX plays with possibleWins
-    console.log("check playerX");
-
+    //compare the possible wins with the input
+    gameOver(playerX.plays);
   }
-
 }
 
-var currentPlayer = function(){
+// set currentPlayerStyle
+var currentPlayerStyle = function(){
   if (playerO.currentPlayer){
     var notInPlay = document.getElementById("playerO");
     notInPlay.classList.remove("currentPlayer");
@@ -82,28 +143,34 @@ var currentPlayer = function(){
   }
 }
 
+// who is the current player
+var currentPlayer = function(){
+  if (playerO.currentPlayer){
+    playerO.currentPlayer = false;
+    playerX.currentPlayer = true;
+  } else {
+    playerX.currentPlayer = false;
+    playerO.currentPlayer = true;
+  }
+  currentPlayerStyle();
+}
+
 //increase move count, adjust current player, set new image src
 var flipImage = function(){
   gameBoard.moveCount++;
   if (playerO.currentPlayer){
     this.src = playerO.src;
     playerO.plays.push(parseInt(this.id));
-    console.log("playerO");
-    console.log(playerO.plays);
-    playerO.currentPlayer = false;
-    playerX.currentPlayer = true;
+    this.removeEventListener("click", flipImage);
   } else {
     this.src = playerX.src;
     playerX.plays.push(parseInt(this.id));
-    console.log("playerX");
-    console.log(playerX.plays);
-    playerX.currentPlayer = false;
-    playerO.currentPlayer = true;
+    this.removeEventListener("click", flipImage);
   }
   //check the game to see if someone won
   if (gameBoard.moveCount > 9){
     newGame();
-  } else if (gameBoard.moveCount > 4){
+  } else if (gameBoard.moveCount >= 4){
     checkGame();
   }
   currentPlayer();
@@ -113,13 +180,13 @@ var flipImage = function(){
 var loadGamePieces = function(){
   var gamePieces = document.getElementsByClassName("row");
   // if game player game pieces are loaded change the face
-  // console.log(gamePieces[0].children);
   if (gamePieces[0].children.length <= 0){
       // make game pieces
       for (var i = 0; i < gamePieces.length; i++){
         var gamePiece = document.createElement("img");
         gamePiece.setAttribute("src", gameBoard.src);
         gamePiece.setAttribute("id", i+1);
+        gamePiece.setAttribute("class", "gamePiece");
         gamePiece.setAttribute("data-id", "gamePiece"+(i+1));
         gamePiece.addEventListener("click", flipImage);
         gamePieces[i].appendChild(gamePiece);
@@ -129,50 +196,20 @@ var loadGamePieces = function(){
       for (var i = 0; i < gamePieces.length; i++){
         var gamePiece = document.getElementById((i+1));
         gamePiece.setAttribute("src", gameBoard.src);
+        gamePiece.addEventListener("click", flipImage);
       }
-      // console.log(gamePieces[0].children);
     }
   }
 
 // begin the game
 var initGame = function(){
   loadGamePieces();
-  currentPlayer();
   playerX.currentPlayer = true;
-  gameCount = 0;
-
+  currentPlayerStyle();
 }
 
 // wait for DOM to load then initate game
 document.addEventListener("DOMContentLoaded", function(){
   document.getElementById("newGame").addEventListener("click", newGame);
-
   initGame();
 });
-// Before you even start working with JavaScript, construct the gameboard.
-// The gameboard page should include the 3x3 grid (of divs), and at
-// minimum a reset button. Using id and class on clickable elements
-// will help you wire this up in JavaScript afterwards.
-// The JavaScript portion will be next
-// Select elements and attach functions via event listeners
-// You will also need a variable to keep track of moves. This will be
-// used to indicate whether or not to draw an X or an O
-
-// Requirements
-// A user should be able to click on different squares to make a move.
-// Every click will alternate between marking an X and O
-// Upon marking of an individual cell, use JavaScript to add a class to
-// each cell to display separate colors.
-// A cell should not be able to be replayed once marked.
-// Add a reset button that will clear the contents of the board.
-// Display a message to indicate which turn is about to be played.
-// After the necessary moves have been played, stop game and alert the
-// winner if one player ends up winning with three in a row.
-
-// Hint: Determine a set of winning combinations. Check those combinations
-// on the board contents after every move.
-
-// Bonuses
-// Add a simple AI to support one player vs computer mode. Note that randomly
-// selecting a space would count as "simple". Try that and iterate from there.
-// Add an AI that can beat you every time with the mini-max algorithm
