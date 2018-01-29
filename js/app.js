84f83db300ea597550035e837c42b40e05a194be
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2018-01-28 22:05:57 -0800

    new update

--- js/app.js
+++ js/app.js
@@ -1,2 +1,22 @@
 console.log("hello front end");
 
+var turn = 0;
+var gameResult = false;
+
+var reset = function () {
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+ 	document.getElementById("tRight").addEventListener("click", markBox);
+ 	document.getElementById("tMiddle").addEventListener("click", markBox);
+ 	document.getElementById("tLeft").addEventListener("click", markBox);
+ 	document.getElementById("cRight").addEventListener("click", markBox);
+ 	document.getElementById("cMiddle").addEventListener("click", markBox);
+ 	document.getElementById("cLeft").addEventListener("click", markBox);
+ 	document.getElementById("bRight").addEventListener("click", markBox);
+ 	document.getElementById("bMiddle").addEventListener("click", markBox);
+ 	document.getElementById("bLeft").addEventListener("click", markBox);
+ 	// document.getElementById("resetButton").addEventListener("click",reset);
+})
