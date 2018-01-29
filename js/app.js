8df8f2d69c2600612f1749db73afec19c3341ef8
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-01-28 20:54:18 -0800

    win condition update

--- js/app.js
+++ js/app.js
@@ -33,22 +33,45 @@ var boxClick = function() {
 //setting all scenarios in which a win could happen
 var checkWin = function() {
 
+//i've set the gameboard at the top of the doccument. now, i'm pulling out each of the 9 squares, and assigning variable to each, so i can match below for win conditions
 var sq1 = classes(gameBoard[0].firstElementChild.className);
+var sq2 = classes(gameBoard[1].firstElementChild.className);
+var sq3 = classes(gameBoard[2].firstElementChild.className);
+var sq4 = classes(gameBoard[3].firstElementChild.className);
+var sq5 = classes(gameBoard[4].firstElementChild.className);
+var sq6 = classes(gameBoard[5].firstElementChild.className);
+var sq7 = classes(gameBoard[6].firstElementChild.className);
+var sq8 = classes(gameBoard[7].firstElementChild.className);
+var sq9 = classes(gameBoard[8].firstElementChild.className);
 
-
-while (turnCount >= 4) {
+while (turnCount >= 3) {
   if (
+      //row1
       (sq1 === sq2 && sq2 === sq3)  ||
-      (midL === midM && midM === midR)  ||
-      (botL === botM && botM === botR)  ||
-      (topL === midL && midL === botL)  ||
-      (topM === midM && midM === botM)  ||
-      (topR === midR && midR === botR)  ||
-      (topL === midM && midM === botR)  ||
-      (topR === midM && midM === botL)
+      //row 2
+      (sq4 === sq5 && sq5 === sq6)  ||
+      //row 3
+      (sq7 === sq8 && sq8 === sq9)  ||
+      //col 1
+      (sq1 === sq4 && sq4 === sq7)  ||
+      //col 2
+      (sq2 === sq5 && sq5 === sq8)  ||
+      //col 3
+      (sq3 === sq6 && sq6 === sq9)  ||
+      //diag 1
+      (sq1 === sq5 && sq5 === sq9)  ||
+      //diag 2
+      (sq3 === sq5 && sq5=== sq7)
     ) {
       console.log("we have a winner!");
+      if (turn%2 === 0) {
+        winner = 'O'
+      } else {
+        winner = 'X'
+      }
     }
+
+    else {
   }
 };
 
