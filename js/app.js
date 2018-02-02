f7710a02cb550dbc1738383cd8d4bf20e9650d0a
Emily Hannan <emilyhannan@Emilys-MacBook-Air.local>
2018-02-02 14:44:48 -0800

    new changes

--- js/app.js
+++ js/app.js
@@ -5,7 +5,10 @@ var box = document.querySelectorAll('.box');
 //turn counter
 var turn = 0
 var player;
-var winnerText = document.getElementsByClassName('winnerText')
+var winnerText = document.querySelector('.winnerText')
+
+// var boxIndexX = []
+// var boxIndexO = []
 
 var resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', resetBoard);
@@ -16,18 +19,18 @@ var resetBoard = function (){
       box[i].style.backgroundColor = '#4D9BA6';
       box[i].addAttribute("class", "");
       winnerText.textContent = " ";
+      // boxIndexX = [""];
+      // boxIndexO = [""];
       turn = 0;
   }
 };
 
 //X wins function
 var winnerX = function() {
-window.alert("X Wins!");
+alert("X wins");
 }
-
-//O wins function
 var winnerO = function() {
-window.alert("O Wins!");
+alert("O wins");
 }
 
 //function to turn boxes to X or O depending on turn click, also adds textContent and changes backgroundColor
@@ -37,13 +40,13 @@ var boxClick = function() {
     this.textContent = 'O';
     this.style.backgroundColor = 'red';
     this.className += " O";
+    // boxIndexO.push(this);
     console.log(this.className);
     // this.removeEventListener('click', boxClick);
     // console.log('removed event listener');
-    // if (turn >= 4) {
-    // checkWin();
-    checkWinX();
-    // }
+    if (turn >= 4) {
+    checkWin();
+    }
     // if (!this.classList.contains("O") && !this.classList.contains("X")) {
     // this.classList.add('O')};
 
@@ -51,32 +54,32 @@ var boxClick = function() {
     this.textContent= 'X';
     this.style.backgroundColor = 'black';
     this.className += " X";
+    // boxIndexX.push(this);
     console.log(this.className);
-    // if (turn >= 4) {
-    // checkWin();
-    checkWinO();
-    // }
+    if (turn >= 4) {
+    checkWin();
+    }
     // if (!this.classList.contains("X") && !this.classList.contains("O")) {
     // this.classList.add('X')};
     // player = 'X'
   }
 };
 
-Switch statment is formatted wrong - not pulling in variables
-var checkWinX = //'x box array'
+
+var checkWin = function() {
 //matching two conditions: box does not include textContent and it's classList matches X
 //matches 3 rows, 3 columns, 2 diagonals
   switch (true) {
-      case box[0].textContent !== '' && box[0].classList.contains('X') === box[1].textContent !== '' && box[1].classList.contains('X') === box[2].textContent !== '' && box[2].classList.contains('X'):
+      case box[0].textContent.contains('X') === box[1].textContent && box[1].textContent == box[2].textContent:
         winnerX();
         break;
-      case box[3].textContent !== '' && box[3].classList.contains('X') === box[4].textContent !== '' && box[4].classList.contains('X') === box[5].textContent !== '' && box[5].classList.contains('X'):
+      case box[3].textContent === box[4].textContent && box[4].textContent == box[5].textContent:
         winnerX();
         break;
-      case box[6].textContent !== '' && box[6].classList.contains('X') === box[7].textContent !== '' && box[7].classList.contains('X') === box[8].textContent !== '' && box[8].classList.contains('X'):
+      case box[6].textContent === box[7].textContent && box[7].textContent == box[8].textContent:
         winnerX();
         break;
-      case box[0].textContent !== '' && box[0].classList.contains('X') === box[3].textContent !== '' && box[3].classList.contains('X') === box[6].textContent !== '' && box[6].classList.contains('X'):
+      case box[0].textContent === box[3].textContent && box[3].textContent == box[6].textContent:
         winnerX();
         break;
       case box[1].textContent !== '' && box[1].classList.contains('X') === box[4].textContent !== '' && box[4].classList.contains('X') === box[7].textContent !== '' && box[7].classList.contains('X'):
@@ -91,7 +94,6 @@ var checkWinX = //'x box array'
       case box[2].textContent !== '' && box[2].classList.contains('X') === box[4].textContent !== '' && box[4].classList.contains('X') === box[6].textContent !== '' && box[6].classList.contains('X'):
         winnerX();
         break;
-      //matching 'O winners'
       case box[0].textContent !== '' && box[0].classList.contains('O') === box[1].textContent !== '' && box[1].classList.contains('O') === box[2].textContent !== '' && box[2].classList.contains('O'):
         winnerO();
         break;
@@ -116,9 +118,8 @@ var checkWinX = //'x box array'
       case box[2].textContent !== '' && box[2].classList.contains('O') === box[4].textContent !== '' && box[4].classList.contains('O') === box[6].textContent !== '' && box[6].classList.contains('O'):
         winnerO();
         break;
-  }
-};
-
+  };
+}
 // add event listener for all 9 boxes to be clicked. using loop for the class boxes
 var addBoxClickListener = function() {
 	for(var i = 0; i < box.length; i++){
