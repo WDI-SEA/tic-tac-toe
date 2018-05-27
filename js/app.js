9792efb308a66ec672c6ba5153cba646298029a2
Scott Ammon <scott.o.ammon@gmail.com>
2018-05-27 11:49:53 -0700

    commented files for improved readability, and fixed some css style redundancies

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-console.log('javascript running');
-
 // Global Variables
 var gameOver = false;
 var moveCount = 1;
@@ -7,53 +5,61 @@ var gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var currentPlayer = 'X';
 
 var resetGame = function() {
-
 	gameOver = false;
 	moveCount = 1;
 
   var gameSquares = document.getElementById('game-board').children;
   
+  // clear all the squares, data-id's, and winning background color
   for(let i = 0; i < gameSquares.length; i++) {
   	gameSquares[i].textContent = '';
   	gameSquares[i].setAttribute('data-id','');
+  	gameSquares[i].style.backgroundColor = 'transparent';
   }
-
   initGame();
 };
 
 var checkForWin = function(squareInPlay) {
+	// get classes associated with square that was clicked
 	var classesInPlay = squareInPlay.classList;
 
   for (let i = 0; i < classesInPlay.length; i++) {
     var winVar = 0;
-
+    // get all board elements that have the i'th class of the clicked square
     var classesOnBoard = document.getElementsByClassName(classesInPlay[i]);
+
+    // iterate over board elements to see if they have the matching letter in them
     for(let i = 0; i < classesOnBoard.length; i++) {
     	if(classesOnBoard[i].textContent === currentPlayer) {
         winVar++;
     	}
     }
 
-    // if all three boxes of a given class have 'currentPlayer' in them, they win
+    // if all three boxes of a given class have same letter in them, it's a win!
     if(winVar === 3) {
     	gameOver = true;
+    	// highlight the squares that won the game
+    	for(let i = 0; i < classesOnBoard.length; i++) {
+        classesOnBoard[i].style.backgroundColor = 'rgba(255, 255, 255, 0.5)';
+      }
     }
 
     if(gameOver) {
-      // make all squares 'played' so that users cannot continue after a win
+      // make all squares 'played' so that users cannot continue to click after a win
       var allSquares = document.getElementById('game-board').children;
 
       for(let i = 0; i < allSquares.length; i++) {
         allSquares[i].setAttribute('data-id', 'played');
       }
-      
+
       // Display which player is the winner
-      document.getElementById('tag-line').textContent = "Player " + currentPlayer + " wins!";
+      document.getElementById('tag-line').textContent = "GAME OVER. Player " + currentPlayer + " wins!";
     }
   }
 };
 
 var clickSquare = function() {
+	// execute if the square hasn't been played yet
 	if(this.getAttribute('data-id') !== 'played') {
 		// mark the clicked square as played
     this.setAttribute('data-id','played');
@@ -62,25 +68,26 @@ var clickSquare = function() {
     if(moveCount >= 5) {
     	checkForWin(this);
     }
+
     moveCount++;
 
     if (moveCount === 10 && gameOver === false) {
     	gameOver = true;
-    	// display "nobody won" message
+    	document.getElementById('tag-line').textContent = "GAME OVER. Nobody won...";
     } 
-
-    // if game is still going... then run this code?
-    // change to other player
-    if(currentPlayer === 'X') {
-    	currentPlayer = 'O';
-    	document.getElementById('player-status').textContent = "Player O's turn"
-    } else {
-    	currentPlayer = 'X';
-    	document.getElementById('player-status').textContent = "Player X's turn"
-    }
-    document.getElementById('move-count').textContent = "Move: " + moveCount;
     
-  } // closes the if statement from the top of the function
+    if(!gameOver) {
+      // change to other player
+      if(currentPlayer === 'X') {
+      	currentPlayer = 'O';
+      	document.getElementById('player-status').textContent = "Player O's turn"
+      } else {
+      	currentPlayer = 'X';
+      	document.getElementById('player-status').textContent = "Player X's turn"
+      }
+      document.getElementById('move-count').textContent = "Move: " + moveCount;
+    }
+  }
 };
 
 var initGame = function() {
@@ -91,17 +98,12 @@ var initGame = function() {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("DOM loaded");
-  
-
   // add Event Listeners and initialize the game
   for(let square in gameBoard) {
     document.getElementById(gameBoard[square]).addEventListener('click', clickSquare);
   }
 
   document.getElementById('reset').addEventListener('click', resetGame);
-
   initGame();
-
 });
 
