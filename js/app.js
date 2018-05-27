dba23d4eaba1d7040bf00455ca65ec308c75beca
rkvasisht <rkvasisht@gmail.com>
2018-05-27 15:46:14 -0700

    test status

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log('Hello frontend');
+
+
+var r1 = getElementByClassName('rowONe');
+var r2 = getElementByClassName('rowTwo');
+var r3 = getElementByClassName('rowThree');
+var c1 = getElementByClassName('columnOne');
+var c2 = getElementByClassName('columnTwo');
+var c3 = getElementByClassName('columnThree');
+
+
+
+var r1c1 = getElementById('rowOneColumnOne');
+
+
+function(x){
+	r1c1.style.backgroundColor = "red"
+			}
+
+r1c1.addEventListener("click", x);
+
+
+
+
