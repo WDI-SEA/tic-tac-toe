08806dfc48118b51449a849d307d03bfd493c56e
shullmb <shullmb@gmail.com>
2018-05-27 11:14:28 -0700

    added more game logic and helper functions -- still working on checkForWin

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Global Variables
 
 var turn = 1;
-var infoboxHandle = null;
+var infoBox = null;
 
 var squares = {
     'one': 0,
@@ -15,45 +15,87 @@ var squares = {
     'nine': 0
 }
 
+var initGame = function() {
+    infoBox.textContent = "Get ready to rumble... slowly.";
+
+    // show player and open squares to clickability
+    setTimeout(function () {
+        // apply unselected to squares to open up clickability
+        for (let square in squares) {
+            document.getElementById(square).classList.add('unselected');
+        }
+        infoBox.textContent = "Shroom makes the first move!";
+    }, 1500)
+}
+
 var reset = function() {
     for (let square in squares) {
         //remove player tokens
         document.getElementById(square).classList.remove('snail');
         document.getElementById(square).classList.remove('shroom');
-        
-        // add unselected class back to squares if needed
-        if (!document.getElementById(square).classList.contains('unselected')) {
-            document.getElementById(square).classList.add('unselected');
-        } 
     }
-
     //reset turn count
     turn = 1;
+
+    initGame();
 }
 
-var checkWinCondition = function(square) {
+var endGame = function() {
+    // disable clicking
+    document.getElementsByClassName(square).classList.remove('unselected');
+    
+    // highlight class of winning squares
+    document.getElementsByClassName(winCondition).classList.add('winning-combo');
+
+    // update infobox
+    infoBox.textContent = capitalizePlayer(playerToken) + "claims the forest!";
+
+}
+
+
+var checkWinCondition = function(square,playerToken) {
     //check win conditions that correspond to classes included included in square
     var possibleWinConditions = [];
 
+    // filter doesn't work on classList -- strip out classes unrelated to condition
     square.classList.forEach( function(cls){
         if (cls !== 'square' && cls !== 'shroom' && cls !== 'snail') {
             possibleWinConditions.push(cls);
         }
     })
 
+    // check each relevant win condition
     possibleWinConditions.forEach( function(winCondition){
         // win check logic
         var squareValues = [];
-        var squaresToCheck = []
-    
-        // console.log(winCondition);
-    
-        var elements = document.getElementsByClassName(winCondition)
+        // var squaresToCheck = []
 
-        for (let element in elements){
-            console.log(element);
+        // grab html elements associated with the win condition
+        var elements = document.getElementsByClassName(winCondition);
+        
+        // this is driving me crazy -- there has to be a way to just check if player token
+        // class is contained in each of the elements in a given win class... but .every() 
+        // can't be called on my classLists...
+
+        var squaresToCheck = Array.from(elements);
+
+        // for (square of squaresToCheck) {
+        //     square.classList.contains(playerToken) ? console.log('win') : false; 
+        // }
+
+        console.log(squaresToCheck[0].classList);
+
+        let values = []
+
+        for (let square of squaresToCheck) {
+            square.classList
         }
-
+        
+        
+        
+        // for (let i = 0; i < elements.length; i ++){
+        //     console.log(elements[i].classList.contains('snail'));
+        // }
         // squaresToCheck.forEach( function(id) {
         //     squareValues.push(squares[id]);
         // })
@@ -66,6 +108,12 @@ var checkWinCondition = function(square) {
 
 var checkPlayer = function(turn) {
     return turn % 2 ? 'shroom' : 'snail';
+}
+
+var capitalizePlayer = function(token){
+    var splitToken = token.split('');
+    splitToken[0] = splitToken[0].toUpperCase();
+    return splitToken.join('');
 }
 
 
@@ -82,24 +130,25 @@ var playSquare = function() {
         
         // check for win
         if (turn >= 5) {
-            checkWinCondition(this);
+            checkWinCondition(this, playerToken);
         }
         
         // increment turn
         turn++
         
         //update message in infobox
-        infoboxHandle.textContent = checkPlayer(turn) + "'s turn.";
+        infoBox.textContent = capitalizePlayer(checkPlayer(turn)) + "'s turn.";
     }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM loaded')
-    for(let square in squares ){
+    for (let square in squares){
         document.getElementById(square).addEventListener('click', playSquare);
     }
-    
     document.getElementById('reset').addEventListener('click', reset);  
-    infoboxHandle = document.querySelector('#infobox h2');
+    infoBox = document.querySelector('#infobox h2');
 
+    
+    initGame();
 })
