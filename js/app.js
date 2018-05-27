7381a4ae845478a67240cd85a2ee2bf0e569afe4
shullmb <shullmb@gmail.com>
2018-05-26 22:40:14 -0700

    added game logic -- working on checkForWin

--- js/app.js
+++ js/app.js
@@ -1,53 +1,96 @@
 // Global Variables
 
-var turn = 0;
+var turn = 1;
+var infoboxHandle = null;
 
 var squares = {
-    'sqr-0': 0,
-    'sqr-1': 0,
-    'sqr-2': 0,
-    'sqr-3': 0,
-    'sqr-4': 0,
-    'sqr-5': 0,
-    'sqr-6': 0,
-    'sqr-7': 0,
-    'sqr-8': 0
+    'one': 0,
+    'two': 0,
+    'three': 0,
+    'four': 0,
+    'five': 0,
+    'six': 0,
+    'seven': 0,
+    'eight': 0,
+    'nine': 0
 }
 
 var reset = function() {
     for (let square in squares) {
-        console.log(square);
+        //remove player tokens
         document.getElementById(square).classList.remove('snail');
         document.getElementById(square).classList.remove('shroom');
-          if (!document.getElementById(square).classList.contains('unselected')) {
-              document.getElementById(square).classList.add('unselected');
-          } 
+        
+        // add unselected class back to squares if needed
+        if (!document.getElementById(square).classList.contains('unselected')) {
+            document.getElementById(square).classList.add('unselected');
+        } 
     }
 
-    turn = 0;
+    //reset turn count
+    turn = 1;
 }
 
-var checkWinCondition = function() {
+var checkWinCondition = function(square) {
     //check win conditions that correspond to classes included included in square
+    var possibleWinConditions = [];
+
+    square.classList.forEach( function(cls){
+        if (cls !== 'square' && cls !== 'shroom' && cls !== 'snail') {
+            possibleWinConditions.push(cls);
+        }
+    })
+
+    possibleWinConditions.forEach( function(winCondition){
+        // win check logic
+        var squareValues = [];
+        var squaresToCheck = []
+    
+        // console.log(winCondition);
+    
+        var elements = document.getElementsByClassName(winCondition)
+
+        for (let element in elements){
+            console.log(element);
+        }
+
+        // squaresToCheck.forEach( function(id) {
+        //     squareValues.push(squares[id]);
+        // })
+
+        // console.log(squareValues.reduce( function(acc, sum){ acc + sum}));
+
+    })
+    
+}
+
+var checkPlayer = function(turn) {
+    return turn % 2 ? 'shroom' : 'snail';
 }
 
 
 var playSquare = function() {
     // determine who is playing    
-    playerToken = turn % 2 ? 'shroom' : 'snail';
-    // console.log(turn, playerToken)
-
-    // swap empty square for token
-    this.classList.replace('unselected', playerToken);
-
-    // add value to square object for win check
-    playerToken === 'snail' ? squares[this.id]+= 1 : squares[this.id] += 10;
-
-    // check for win
-    if (turn >= 5) {
-        checkWinCondition();
+    var playerToken = checkPlayer(turn);
+    // check to see if square is free
+    if (this.classList.contains('unselected')){
+        // swap empty square for token
+        this.classList.replace('unselected', playerToken);
+   
+        // add value to square object for win check
+        playerToken === 'snail' ? squares[this.id]+= 1 : squares[this.id] += 10;
+        
+        // check for win
+        if (turn >= 5) {
+            checkWinCondition(this);
+        }
+        
+        // increment turn
+        turn++
+        
+        //update message in infobox
+        infoboxHandle.textContent = checkPlayer(turn) + "'s turn.";
     }
-    turn++
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -57,8 +100,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     
     document.getElementById('reset').addEventListener('click', reset);  
-
-
-
+    infoboxHandle = document.querySelector('#infobox h2');
 
 })
