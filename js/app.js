ef53527e1cecf29e9d682ffa1e0d03e82b4cee51
Genevieve Schultz <genschultz@gmail.com>
2018-05-27 16:31:23 -0700

    initial file upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-console.log('Hello frontend');
+console.log('Javascript is a go');
+
+
+// 8 win conditions, earliest win on 5th move
+// Don't have to check all win conditions, depending on where you play
+// Potential class names: col 1/2/3 row 1/2/3 diag 1/2
