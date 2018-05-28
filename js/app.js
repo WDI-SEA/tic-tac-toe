68af83bbd84135e61eedad12f5b06701af26e22a
rkvasisht <rkvasisht@gmail.com>
2018-05-28 15:05:27 -0700

    test 2

--- js/app.js
+++ js/app.js
@@ -1,23 +1,46 @@
-console.log('Hello frontend');
 
 
-var r1 = getElementByClassName('rowONe');
-var r2 = getElementByClassName('rowTwo');
-var r3 = getElementByClassName('rowThree');
-var c1 = getElementByClassName('columnOne');
-var c2 = getElementByClassName('columnTwo');
-var c3 = getElementByClassName('columnThree');
+var boxOne = document.getElementById("one");
+var boxTwo = document.getElementById("two");
+var boxThree = document.getElementById("three");
+var boxFour = document.getElementById("four");
+var boxFive = document.getElementById("five");
+var boxSix = document.getElementById("six");
+var boxSeven = document.getElementById("seven");
+var boxEight = document.getElementById("eight");
+var boxNine = document.getElementById("nine");
+var box = document.getElementsByClassName("box");
+var marker =["X","O"];
+var turn = 0;
 
 
 
-var r1c1 = getElementById('rowOneColumnOne');
+
+function toggle(){
+	if(turn === 0){
+    turn = 1;
+	}
+else{
+    turn = 0;
+	}
+}
+
+function xAndO(){
+	box.innerText = marker[turn];
+}
+	
+
+for(var i =0; i < box.length; i++){
+
+box[i].addEventListener("click", toggle, xAndO);
+
+}
 
 
-function(x){
-	r1c1.style.backgroundColor = "red"
-			}
 
-r1c1.addEventListener("click", x);
+
+
+	
 
 
 
