93af9b60004e553d94b546a6eb33c0061a2ef886
jayhatha <jay@jayhathaway.com>
2018-05-28 16:50:30 -0700

    added reset button and styles

--- js/app.js
+++ js/app.js
@@ -1,17 +1,7 @@
-// definining global variables like player and turn
 var player = null;
 var turn = null;
-
-// initiating new game/resetting game state
-var initGame = function() {
-  player = 1; // setting initial player
-  turn = 1; // setting initial turn
-  document.getElementById('player').textContent = player;
-  document.getElementById('turn').textContent = turn;
-  // making our squares listen for clicks
-};
-initGame();
-// defining our win conditions
+var lastMove = null;
+var gameOver = false;
 var boardState = [
   [],
   [],
@@ -23,23 +13,63 @@ var boardState = [
   [],
   []
 ];
+// initiating new game/resetting game state
+var initGame = function() {
+  player = 1; // setting initial player
+  turn = 1; // setting initial turn
+  gameOver = false;
+  boardState = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ];
+  document.getElementById('player').textContent = player;
+  document.getElementById('turn').textContent = turn;
+  let squares = document.getElementsByClassName('square');
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].firstElementChild.textContent = ' ';
+    squares[i].classList.remove('occupied');
+    squares[i].classList.remove('o');
+    squares[i].classList.remove('x');
+  }
+};
+
+initGame();
+// defining our win conditions
+
 var checkForWin = function() {
   console.log('checking for win');
-  if ((boardState[0] === boardState[1] && boardState[0] === boardState[2]) || (boardState[3] === boardState[4] && boardState[3] === boardState[5]) ||
-   (boardState[6] === boardState[7] && boardState[6] === boardState[8]) || (boardState[0] === boardState[3] && boardState[0] === boardState[6]) ||
-   (boardState[1] === boardState[4] && boardState[1] === boardState[7]) || (boardState[2] === boardState[5] && boardState[2] === boardState[8]) || (boardState[0] === boardState[4] && boardState[0] === boardState[8]) || (boardState[2] === boardState[4] && boardState[2] === boardState[6])) {
-    console.log('player ' + player + ' wins!');
-  }
-
-  var count = null;
-  for (let i = 0; i < boardState.length; i++) {
-    if (boardState[i] === 'x') {
-      count++;
-    } else if (boardState[i] === 'o') {
-      count++;
+  if ((boardState[0] === boardState[1] && boardState[0] === boardState[2]) ||
+    (boardState[3] === boardState[4] && boardState[3] === boardState[5]) ||
+    (boardState[6] === boardState[7] && boardState[6] === boardState[8]) ||
+    (boardState[0] === boardState[3] && boardState[0] === boardState[6]) ||
+    (boardState[1] === boardState[4] && boardState[1] === boardState[7]) ||
+    (boardState[2] === boardState[5] && boardState[2] === boardState[8]) ||
+    (boardState[0] === boardState[4] && boardState[0] === boardState[8]) ||
+    (boardState[2] === boardState[4] && boardState[2] === boardState[6])) {
+    gameOver = true;
+    if (lastMove === 'x') {
+      console.log('player 1 wins!');
+    } else if (lastMove === 'o') {
+      console.log('player 2 wins!');
+    }
+    var count = null;
+    for (let i = 0; i < boardState.length; i++) {
+      if (boardState[i] === 'x') {
+        count++;
+      } else if (boardState[i] === 'o') {
+        count++;
+      }
     }
   }
-  if (count === 9) {
+  if (turn > 9 && !gameOver) {
+    gameOver = true;
     console.log('cat game');
     // other cat game effects
   }
@@ -47,7 +77,7 @@ var checkForWin = function() {
 
 // making a move
 var makeMove = function() {
-  if (!this.classList.contains('occupied')) {
+  if (!this.classList.contains('occupied') && !gameOver) {
     turn++;
     if (player === 1) {
       this.firstElementChild.textContent = 'X';
@@ -55,6 +85,8 @@ var makeMove = function() {
       for (let i = 0; i < 9; i++) {
         if (this.classList.contains(i)) {
           boardState[i] = 'x';
+          this.classList.add('x');
+          lastMove = 'x';
           // console.log(bucket.classList);
         }
       }
@@ -66,7 +98,8 @@ var makeMove = function() {
       for (let i = 0; i < 9; i++) {
         if (this.classList.contains(i)) {
           boardState[i] = 'o';
-
+          this.classList.add('o');
+          lastMove = 'o';
           // console.log(bucket.classList);
         }
       }
@@ -80,11 +113,10 @@ var makeMove = function() {
     this.classList.add('occupied');
     document.getElementById('player').textContent = player;
     document.getElementById('turn').textContent = turn;
-    console.log('click!');
   }
 };
 var squares = document.getElementsByClassName('square');
-console.log(squares);
 for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', makeMove);
 }
+document.getElementsByTagName('button')[0].addEventListener('click', initGame);
