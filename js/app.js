7e3aed43a45bac6890ae039722aa1eef8ba0bc3d
jayhatha <jay@jayhathaway.com>
2018-05-29 13:34:20 -0700

    got the cpu working, just random moves

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-var player = null;
-var turn = null;
-var lastMove = null;
-var gameOver = false;
+var player = null; // player whose turn it is
+var turn = null; // turn counter
+var lastMove = null; // the player who moved last, used to determine winner
+var gameOver = true; // is the game over? don't allow clicks if it is.
+var vsComputer = false; // is cpu mode on?
+// this array represents the 9 squares
 var boardState = [
   [],
   [],
@@ -13,11 +15,14 @@ var boardState = [
   [],
   []
 ];
+// this array represents squares the AI player can pick
+var computerSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 // initiating new game/resetting game state
 var initGame = function() {
   player = 1; // setting initial player
   turn = 1; // setting initial turn
-  gameOver = false;
+  gameOver = false; // the game is on!
+  // resetting initial board state — no x's or o's marked
   boardState = [
     [],
     [],
@@ -29,6 +34,8 @@ var initGame = function() {
     [],
     []
   ];
+  // resetting the squares the AI can pick
+  computerSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   document.getElementsByClassName('banner')[0].style.backgroundColor = 'rgba(0, 0, 0, 0)';
   document.getElementsByClassName('banner')[0].textContent = ' ';
   document.getElementsByClassName('banner')[0].classList.remove('blink');
@@ -44,8 +51,19 @@ var initGame = function() {
     squares[i].classList.remove('x');
   }
 };
-
-initGame();
+var changeOpponent = function() {
+  if (gameOver) {
+    if (vsComputer === false) {
+      vsComputer = true;
+      document.getElementById('cpu').classList.add('x');
+      document.getElementById('human').classList.remove('x');
+    } else if (vsComputer === true) {
+      vsComputer = false;
+      document.getElementById('human').classList.add('x');
+      document.getElementById('cpu').classList.remove('x');
+    }
+  }
+};
 // defining our win conditions
 
 var checkForWin = function() {
@@ -65,10 +83,8 @@ var checkForWin = function() {
     document.getElementsByTagName('button')[0].style.color = '#3a3a32';
     if (lastMove === 'x') {
       document.getElementsByClassName('banner')[0].textContent = 'X Wins!';
-      console.log('player 1 wins!');
     } else if (lastMove === 'o') {
       document.getElementsByClassName('banner')[0].textContent = '0 Wins!';
-      console.log('player 2 wins!');
     }
     var count = null;
     for (let i = 0; i < boardState.length; i++) {
@@ -89,24 +105,59 @@ var checkForWin = function() {
     // other cat game effects
   }
 };
+// the cpu is making a move.
+var cpuMove = function() {
+  turn++;
+  var rand = computerSquares[Math.floor(Math.random() * computerSquares.length)];
+  console.log('cpu moving to ' + rand);
+  boardState[rand] = 'o';
+  squares[rand].classList.add('o');
+  squares[rand].classList.add('occupied');
+  squares[rand].firstElementChild.textContent = 'O';
+  lastMove = 'o';
 
+  var index = computerSquares.indexOf(rand);
+  console.log(index);
+  if (index !== -1) computerSquares.splice(index, 1);
+  console.log('remaining: ' + computerSquares);
+  player = 1;
+  if (turn > 5) {
+    checkForWin();
+  }
+};
 // making a move
 var makeMove = function() {
-  if (!this.classList.contains('occupied') && !gameOver) {
+  if (!this.classList.contains('occupied') && !gameOver) { // before we do anything, check if square is already occupied
     turn++;
-    if (player === 1) {
+    if (player === 1 && vsComputer === false) {
       this.firstElementChild.textContent = 'X';
-      console.log('player one moved');
       for (let i = 0; i < 9; i++) {
         if (this.classList.contains(i)) {
           boardState[i] = 'x';
           this.classList.add('x');
           lastMove = 'x';
+        }
+        player = 2;
+      }
+    } else if (player === 1 && vsComputer === true) {
+      this.firstElementChild.textContent = 'X';
+      for (let i = 0; i < 9; i++) {
+        if (this.classList.contains(i)) {
+          boardState[i] = 'x';
+          var index = computerSquares.indexOf(i);
+          if (index !== -1) computerSquares.splice(index, 1);
+          console.log('p1 removed ' + index);
+          console.log('remaining: ' + computerSquares);
+          this.classList.add('x');
+          lastMove = 'x';
+          player = 2;
+          if (turn > 5) {
+            checkForWin();
+          }
+          if (!gameOver) cpuMove();
           // console.log(bucket.classList);
         }
       }
-      player++;
-      // claimSquare();
     } else if (player === 2) {
       this.firstElementChild.textContent = 'O';
       console.log('player 2 moved');
@@ -115,11 +166,9 @@ var makeMove = function() {
           boardState[i] = 'o';
           this.classList.add('o');
           lastMove = 'o';
-          // console.log(bucket.classList);
         }
       }
-      player--;
-      // claimSquare();
+      player = 1;
     }
     if (turn > 5) {
       checkForWin();
@@ -135,3 +184,5 @@ for (let i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', makeMove);
 }
 document.getElementsByTagName('button')[0].addEventListener('click', initGame);
+document.getElementById('cpu').addEventListener('click', changeOpponent);
+document.getElementById('human').addEventListener('click', changeOpponent);
