3f31ba844e9ac40165edd491cdef7c00477e4319
MizJones <gmjones206@gmail.com>
2018-07-29 16:15:50 -0700

    first commit, many hours

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 console.log('Hello frontend');
+var mavIntro;
+var aryaIntro;
+var gameIntro;
+var clickSound;
+var maverick;
+var arya;
+var mavMoves = [];
+var aryaMoves = [];
+
+document.addEventListener('DOMContentLoaded', function(){
+	console.log('DOM got loaded');
+	document.getElementById('selection').addEventListener('click', start);
+
+	pOne = document.getElementById('selection').addEventListener('click', pOne = this);
+
+});
+
+function start() {
+	console.log('starting game');
+	gameIntro = document.getElementById('introSound');
+	gameIntro.play();
+}
+
+// function endTurn()
+
+// function clickSquare()
