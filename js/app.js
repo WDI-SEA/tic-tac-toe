2da4fb00ff23581583d42d1112970ee93553fce2
yinebeb Girma <yinebebgirma@yinebebs-MBP.domain>
2018-07-29 20:04:23 -0700

    layout complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello frontend');
+document.addEventListener('DOMContentLoaded', function (){
+	console.log('Dom got loaded!');
+
+	document.getElementById('reset').addEventListener('click', start);
+	
+
+});
+
+
+function start(){
+	for (var i = 0; i < 10; i++){
+		var cell = 'cell-' + i
+		document.getElementById(cell).addEventListener('click', cellClick);
+		}
+	}
+}
+
+
+
+function cellClick(type){
+	if (type === x){
+		this.textContent = 'X';
+	} else {
+		this.textContent = 'O';
+	};
+}
+
