c13576ed69fac0b4c8194b2e5fea02d599328edc
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-07-30 07:55:22 -0700

    solution

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ var turns = [];
 
 var gameOver = false;
 
-var score = 0;
 var highscore = localStorage.getItem("highscore");
 
 
@@ -14,6 +13,7 @@ if (highscore === null) {
 } else {
   document.getElementsByClassName('scoreBoard')[0].innerText = "Wins: " + highscore;
 }
+
 
 var objectX = {
   b1: false,
@@ -40,112 +40,52 @@ var objectO = {
 };
 
 
+// if (turns.length === 1 && objectX.b1 === true || objectX.b2 === true || objectX.b3 === true || objectX.b4 === true || objectX.b6 === true || objectX.b7 === true || objectX.b8 === true || objectX.b9 === true) {
+//   num = Math.ceil(Math.random()*9);
+//   document.getElementById('b' + num).src = "img/o.png"
+// }
 //checks the objects for a win of X
 var checkForWinX = function() {
 	if (objectX.b1 && objectX.b2 && objectX.b3) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b4 && objectX.b5 && objectX.b6) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b7 && objectX.b8 && objectX.b9) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b1 && objectX.b4 && objectX.b7) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b2 && objectX.b5 && objectX.b8) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b3 && objectX.b6 && objectX.b9) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b1 && objectX.b5 && objectX.b9) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectX.b3 && objectX.b5 && objectX.b7) {
 		document.getElementById('playerturn').innerText = "Player 1:X WINS!";
     highscore++
     checkWins();
-    // if(highscore !== null){
-    //     highscore = 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //       highscore = score + 1
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (turns.length === 9) {
 		document.getElementById('playerturn').innerText = "DRAW!";
 		gameOver = true;
@@ -157,84 +97,34 @@ var checkForWinX = function() {
 var checkForWinO = function() {
 	if (objectO.b1 && objectO.b2 && objectO.b3) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (objectO.b4 && objectO.b5 && objectO.b6) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
     console.log(highscore);
 	} else if (objectO.b7 && objectO.b8 && objectO.b9) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
     console.log(highscore);
 	} else if (objectO.b1 && objectO.b4 && objectO.b7) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
     console.log(highscore);
 	} else if (objectO.b2 && objectO.b5 && objectO.b8) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
     console.log(highscore);
 	} else if (objectO.b3 && objectO.b6 && objectO.b9) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
     console.log(highscore);
 	} else if (objectO.b1 && objectO.b5 && objectO.b9) {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
     console.log(highscore);
 	} else if (objectO.b3 && objectO.b5 && objectO.b7)  {
 		document.getElementById('playerturn').innerText = "Player 2:O WINS!";
-    // if(highscore !== null){
-    //     highscore = score + 1
-    //         localStorage.setItem("highscore", score);
-    //     } else {
-    //     localStorage.setItem("highscore", score);
-    // }
 		gameOver = true;
-    console.log(highscore);
 	} else if (turns.length === 9) {
 		document.getElementById('playerturn').innerText = "DRAW!";
 		gameOver = true;
@@ -276,25 +166,6 @@ for (let i = 0; i < boxes.length; i++) {
 
 var gameReset = function() {
   location.reload();
-	// document.getElementById('playerturn').innerText = "Turn: Player 1:X";
-  // var elem1 = document.getElementById("b1");
-  // elem1.parentNode.removeChild(elem1);
-  // var elem2 = document.getElementById("b2");
-  // elem2.parentNode.removeChild(elem2);
-  // var elem3 = document.getElementById("b3");
-  // elem3.parentNode.removeChild(elem3);
-  // var elem4 = document.getElementById("b4");
-  // elem4.parentNode.removeChild(elem4);
-  // var elem5 = document.getElementById("b5");
-  // elem5.parentNode.removeChild(elem5);
-  // var elem6 = document.getElementById("b6");
-  // elem6.parentNode.removeChild(elem6);
-  // var elem7 = document.getElementById("b7");
-  // elem7.parentNode.removeChild(elem7);
-  // var elem8 = document.getElementById("b8");
-  // elem8.parentNode.removeChild(elem8);
-  // var elem9 = document.getElementById("b9");
-  // elem9.parentNode.removeChild(elem9);
   gameOver = false;
 };
 
