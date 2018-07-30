73641c01fc6970a8f2f89865ee5bf14c9abf8817
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-07-29 20:47:50 -0700

    General styling and game board set up for tic tac toe. Just startiong game logic.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('Hello frontend');
+var startBoard;
+const human = 'O';
+const computer = 'X';
+const winCombos = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9],
+	[1, 4, 7],
+	[2, 5, 8],
+	[3, 6, 9],
+	[1, 5, 9],
+	[3, 5, 7]
+]
+
+const cells = document.querySelectorAll('.box');
+startGame();
+
+function startGame(){
+	document.querySelector(".endgame").style.display = "none";
+	startBoard = Array.from(Array(9).keys());
+	console.log(startBoard);
+}
