088c67bbc3f033fcb179ba7564482d802a48fa59
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-08-02 07:36:52 -0700

    wrote checkWin and checkForWin functions. should probably rename checkWin for clarity

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ console.log('Hello world');
 
 function startGame() {
 	document.turn = "X";
-
 	setMessage(document.turn + " gets to start.");
 }
 
@@ -27,4 +26,32 @@ function switchTurn() {
 	}
 
 	setMessage("It's " + document.turn + "'s turn!");
+}
+
+function checkForWin(move) {
+	var result = false;
+	if (checkWin(1, 2, 3, move) || 
+		checkWin(4, 5, 6, move) || 
+		checkWin(7, 8, 9, move) ||
+		checkWin(1, 4, 7, move) ||
+		checkWin(2, 5, 8, move) ||
+		checkWin(3, 6, 9, move) ||
+		checkWin(1, 5, 9, move) ||
+		checkWin(3, 5, 7, move)) {
+
+		result = true;
+	}
+	return result;
+}
+
+function checkWin(a, b, c, move) {
+	var result = false;
+	if (getCell(a) == move && getCell(b) == move && getCell(c) == move) {
+		result = true;
+	}
+	return result;
+}
+
+function getCell(number) {
+	document.getElementById("c" + number).innerText;
 }
