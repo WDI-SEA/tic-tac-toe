ba66d0558e456c1c3cf1f6aa6ff533c51f9cdbe3
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-04 17:18:16 -0700

    updated to allow image to stay and mark whose turn it is. Also detects if cell is occupied.

--- js/game.js
+++ js/game.js
@@ -12,17 +12,23 @@
 // 	resets board to start game condition
 
 console.log("game.js is working")
-
+var playsMade = 0
 var gameGrid = document.getElementsByTagName("td");
 var hero = document.getElementsByClassName("spidey")
-var playerTurn = 0;
+var cellStatus = new Array();
+var occupied = new Array ();
+var setPic;
 var image = document.createElement("img")
+var playerTurn = 0;
 // var boxNum = document.getElementbyId(index);
 
 document.addEventListener("DOMContentLoaded", function(){
 	console.log("dom content loaded");
-	var playerTurn = 0;
 	// gameBoard();
+	for(var i = 0; i < 9; i++){
+		cellStatus[i] = false
+	}
+
 	
 });
 
@@ -39,24 +45,44 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function selection(tabNum){
 	console.log(playerTurn)
-	if(playerTurn === 0){
-		console.log("player 1");
-		image.setAttribute('src', './img/spiderman_face.jpg');
-		image.setAttribute('width', '75px');
-		image.setAttribute('height', 'auto');
-		tabNum.appendChild(image);
-		//tabNum.removeEventListener('click', selection());
-				
-		playerTurn = 1;
+	setPic = document.getElementById(tabNum);
 
+	if(cellStatus[tabNum] === false){
+		if(playerTurn % 2 == 0){
+			console.log("player 1");
+			setPic.innerHTML = "<img src='./img/spiderman_face.jpg' width='75' height='auto'>"
+			// image.setAttribute('src', './img/spiderman_face.jpg');
+			// image.setAttribute('width', '75px');
+			// image.setAttribute('height', 'auto');
+			// setPic.appendChild(image);
+			occupied[tabNum] = 'spidey'
+			
+
+		} else {
+			console.log('player 2')
+			setPic.innerHTML = "<img src='./img/venom_face.jpg' width='75' height='auto'>"
+			// image.setAttribute('src', './img/venom_face.jpg');
+			// image.setAttribute('width', '75px');
+			// image.setAttribute('height', 'auto');
+			// setPic.appendChild(image);
+			occupied[tabNum] = 'venom'
+			
+		}
 	} else {
-		console.log('player 2')
-		image.setAttribute('src', './img/venom_face.jpg');
-		image.setAttribute('width', '75px');
-		image.setAttribute('height', 'auto');
-		tabNum.appendChild(image);
-		//tabNum.removeEventListener('click', selection());
-
-		playerTurn -= 1
+		console.log('OCCUPIED!')
 	}
+	playerTurn++;
+	cellStatus[tabNum] = true;
+		
 }
+
+
+
+
+
+
+
+
+
+
+
