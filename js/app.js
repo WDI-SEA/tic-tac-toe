4c5136f16f7ed3dc53402de84a1848e6d7e71f70
Sky <anvandp18@gmail.com>
2018-08-05 21:31:04 -0700

    re-submit Tic Tac Toe

--- js/app.js
+++ js/app.js
@@ -1,71 +1,84 @@
+
+var begin = 0;
+
 document.addEventListener('DOMContentLoaded', function(){
-  console.log('Dom got loaded');
-
-  document.getElementById('start').addEventListener('click', startGame);
-  // document.getElementById('pause').addEventListener('click', startGame);
-  // document.getElementById('reset').addEventListener('click', startGame);
+	console.log('Dom got loaded');
+	document.getElementById('play').addEventListener('click', start);
 });
-function startGame() {
-	document.turn = "X";
-	document.winner = null;
 
-	setMessage(document.turn + "'s turn!");
+function start(){
+	begin = 1;
+	document.turn = 'X';
+	for(var i = 0; i<9; i++){
+		document.getElementById('c' + i).textContent = '';
+		document.getElementById('c' + i).style.backgroundColor = 'yellow';
+	}
+	document.getElementById('play').textContent = 'Reset';
+	console.log('let\'s play');
 }
 
-function setMessage(msg) {
-	document.getElementById("message").innerText= msg;
+
+function nextMove(cell){
+	if (begin === 1){
+		if (cell.textContent == ''){
+			cell.textContent = document.turn;
+			if(document.turn == 'X'){
+				cell.style.backgroundColor = 'red';
+			}else{
+				cell.style.backgroundColor = 'green';
+			}
+
+			if (checkWin(document.turn)){
+				alert(document.turn + ' win! Congratulations!!!');
+				begin = 0;
+			}else if(checkTie()){
+				alert('It\'s a tie game!!!');
+			}else{
+			switchTurn();
+			}
+		}else{
+			alert('This cell is already placed!');
+		}
+	}
 }
 
-function nextMove(square) {
-	if (document.winner != null) {
-		setMessage(document.winner + " already won!!!");
-	} else if (square.innerText == "") {
-	square.innerText = document.turn;
-	switchTurn();
+function switchTurn(){
+	if (document.turn === 'X'){
+		document.turn = 'O';
+	}else{
+		document.turn = 'X';
+	}
+}
+
+function checkWin(turn){
+	if (checkThreeCells(0, 1, 2, turn) || checkThreeCells(3, 4, 5, turn) ||
+		checkThreeCells(6, 7, 8, turn) || checkThreeCells(0, 3, 6, turn) ||
+		checkThreeCells(1, 4, 7, turn) || checkThreeCells(2, 5, 8, turn) ||
+		checkThreeCells(0, 4, 8, turn) || checkThreeCells(2, 4, 6, turn)
+		) {
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function checkTie(){
+	for (var i = 0; i < 9; i++){
+		if (document.getElementById('c' + i).textContent == ''){
+			return false;
+		}
+	}
+	return true;
+}
+
+function checkThreeCells(a,b,c,turn){
+	if (document.getElementById('c' +a).textContent === turn &&
+		document.getElementById('c' +b).textContent === turn &&
+		document.getElementById('c' +c).textContent === turn) {
+		return true;
 	} else {
-		setMessage("That square is already used.");
+		return false;
 	}
-}
 
-function switchTurn() {
-	if (checkForWinner(document.turn)){
-		setMessage("Congratulations, " + document.turn + "! You win!!!");
-		document.winner = document.turn;
-	} else if (document.turn =="X"){
-		document.turn = "O";
-		setMessage(document.turn + "'s turn!");
-	} else {
-		document.turn = "X";
-		setMessage(document.turn + "'s turn!");
-	}
-	
-}
-
-function checkForWinner(move) {
-	var result = false;
-	if (checkRow(1,2,3, move) ||
-		checkRow(4,5,6, move) ||
-		checkRow(7,8,9, move) ||
-		checkRow(1,4,7, move) ||
-		checkRow(2,5,8, move) ||
-		checkRow(3,6,9, move) ||
-		checkRow(1,5,8, move) ||
-		checkRow(3,5,7, move)){
-	result = true;
-	}
-	return result;
-}
-
-function checkRow(a, b, c, move) {
-	var result = false;
-	if (getBox(a) == move && getBox(b) == move && getBox(c) == move){
-		result = true;
-	}
-	return result;
-}
-
-
-function getBox(number){
-	document.getElementById("s" + number).innerText;
 }
 
