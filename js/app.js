b2b684750f1f0ce132d14e9f414dee98933d51af
yuanjli <liyuanjienetwork@gmail.com>
2018-08-06 12:43:20 -0700

    rewrote some functions and prepare for minimax algorithm

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-turn = 'X';
-winner = null;
+// modifing the project with an AI algorithm: 
+// notes from 
+// https://stackoverflow.com/questions/125557/what-algorithm-for-a-tic-tac-toe-game-can-i-use-to-determine-the-best-move-for
+
+
+// Pseudocode for Minimax Algorithms: 
+// function minimax(node, depth, maximizingPlayer) is
+//     if depth = 0 or node is a terminal node then
+//         return the heuristic value of node
+//     if maximizingPlayer then
+//         value := −∞
+//         for each child of node do
+//             value := max(value, minimax(child, depth − 1, FALSE))
+//         return value
+//     else (* minimizing player *)
+//         value := +∞
+//         for each child of node do
+//             value := min(value, minimax(child, depth − 1, TRUE))
+//         return value      
+
+
+var turn = null;
+var winner = null;
 
 document.addEventListener('DOMContentLoaded',function(){
 	// console.log("DOM got loaded");
 	// document.addEventListener('click', setMessage('Please click Start'));
 	document.getElementById('start').addEventListener('click', start);
-	document.getElementById('reset').addEventListener('click', start);
+	document.getElementById('cStart').addEventListener('click', start);
 
 });
 //console.log('loaded! again');
 //this function starts the game;
 function start(){
-	console.log('starting game');
+	console.log('Starting game');
 	var num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 	for (var i = 0; i <= 8; i++){
 		clearBox(num[i]);
@@ -19,79 +40,118 @@ function start(){
 	turn = 'X';
 	setMessage(turn + ' is starting:');
 }
-
 function setMessage(m) {
 	document.getElementById('message').innerText = m;
-}
-
-//This function keeps track of who's turn:
-function switchTurn(){
-	// console.log("TURN");
-	if(checkWin(turn)){
-		setMessage(turn + ", WON!")
-		winner = turn;
-	}else if (checkDraw(turn)){
-		setMessage("The game is Draw! ");   // this should be in the next move function
-	}
-	else if (turn == 'X'){
-		turn = 'O';
-	}else{
-		turn = 'X';
-	}
-	setMessage('It\'s '  + turn + ' turn now!' );
-}
-
-function nextMove(square){
-	if(winner != null){
-		setMessage(turn + " has won the game!")
-	}
-	else if (square.innerText == 'O' || square.innerText == 'X'){
-		setMessage('Please Pick another cell');
-	}else {
-		square.innerText = turn;
-		switchTurn();
-	}
 }
 function clearBox(number) {
 	document.getElementById(number).innerText = "";
 	winner = null;
 }
-function checkWin(move) {
-	var result = false;
-	if (checkRow("one", "two", "three", move) ||
-		checkRow("four", "five", "six", move) ||
-		checkRow('seven', 'eight', 'nine', move) ||
-		checkRow('one', 'four', 'seven', move) ||
-		checkRow('one', 'five', 'nine', move) ||
-		checkRow('two', 'five', 'eight', move) ||
-		checkRow('three', 'five', 'seven', move) ||
-		checkRow('three', "six", 'nine', move) 
-		){ result = true;
-	}return result;
-}
-function checkDraw(move) {
-	var result = false;
-	var num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-	for (var i = 0; i <= 8; i++){
-		if (getBoxValue(num[i]) == 'X' || getBoxValue(num[i]) == 'O') {
-			result = true;
-		}else {
-			result =false;
-		}
-	}
-	return result;
-	console.log(result);
+function getBoxValue(idNumber) {
+	// var abc = document.getElementById(idNumber).innerText;
+	// console.log(abc)
+	return document.getElementById(idNumber).innerText;
 }
 function checkRow(a, b, c, move){
 	var result = false;
 	if (getBoxValue(a) == move && getBoxValue(b) == move && getBoxValue(c) == move){
 		result = true;
+	} return result;
+}
+// if there is three symble in a row with the same turn. then the turn wins. 
+function checkWin(currentTurn) {
+	//console.log('is checking win!...');
+	var result = false;
+	if (checkRow("one", "two", "three", currentTurn) ||
+		checkRow("four", "five", "six", currentTurn) ||
+		checkRow('seven', 'eight', 'nine', currentTurn) ||
+		checkRow('one', 'four', 'seven', currentTurn) ||
+		checkRow('one', 'five', 'nine', currentTurn) ||
+		checkRow('two', 'five', 'eight', currentTurn) ||
+		checkRow('three', 'five', 'seven', currentTurn) ||
+		checkRow('three', "six", 'nine', currentTurn) ) 
+	{
+		result = true;
 	}
 	return result;
 }
-function getBoxValue(number) {
-	return document.getElementById(number).innerText;
+//Checks if the game is Draw.
+function checkDraw() {
+	//console.log('is checking Draw!')
+	var result = 0;
+	var num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	for (var i = 0; i <= 8; i++){
+		if (getBoxValue(num[i]) == 'X' || getBoxValue(num[i]) == 'O') {
+			result += 1;
+		}
+	} if (result == 9) {
+		return true;
+	}else {
+		return false;
+	}
 }
+//This function keeps track of who's turn:
+function switchTurn(){
+	// console.log("TURN");
+	if (turn == 'X'){
+		turn = 'O';
+	}else{
+		turn = 'X';
+	}setMessage('It\'s '  + turn + ' turn now!' );
+}
+
+function nextMove(square){
+	if (square.innerText == 'O' || square.innerText == 'X'){
+		if (winner == null) {
+			setMessage('Please Pick another cell');
+		}else{
+			setMessage('This game has ended, please start another game!');
+		}
+	}else if (winner == null) {
+		square.innerText = turn;
+		if (checkWin(turn)) {
+			winner = turn;
+			console.log(checkWin(turn));
+			console.log('call setMessage and ends the game');
+			setMessage(winner + ", has WON the game!");
+
+		} else if (checkDraw()) {
+			winner = 'no one';
+			setMessage("This Game is a Draw!");
+			console.log('call Drawfunctions and setMessage and ends the game');
+		} else {
+			switchTurn();
+		}	
+		console.log('the move has switched: ', turn);
+	} 
+}
+
+
+
+
+
+// if winner is not null, then the game has ended! 
+
+
+
+// function minimax(game, depth, maximizingPlayer) {
+// 	if (game) {}
+
+
+
+// }
+// if depth = 0 or node is a terminal node then
+// 	return the heuristic value of node
+// if maximizingPlayer then
+// 	value := −∞
+// for each child of node do
+// 	value := max(value, minimax(child, depth − 1, FALSE))
+// return value
+// else (* minimizing player *)
+// 	value := +∞
+// for each child of node do
+// 	value := min(value, minimax(child, depth − 1, TRUE))
+// return value
 
 
 
