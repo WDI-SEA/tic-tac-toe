658df4efe207369564928a7c34b3c8166cd1b875
yuanjli <liyuanjienetwork@gmail.com>
2018-08-07 10:02:56 -0700

    reformatted the js, added comments, and working on the minimax

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // modifing the project with an AI algorithm: 
 // notes from 
 // https://stackoverflow.com/questions/125557/what-algorithm-for-a-tic-tac-toe-game-can-i-use-to-determine-the-best-move-for
-
+//https://www.youtube.com/watch?v=6ELUvkSkCts
 
 // Pseudocode for Minimax Algorithms: 
 // function minimax(node, depth, maximizingPlayer) is
@@ -18,47 +18,70 @@
 //             value := min(value, minimax(child, depth − 1, TRUE))
 //         return value      
 
-
+// Setting the global variables for later use: 
 var turn = null;
 var winner = null;
+var computer = null;
+var boardTiles = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
+
+// Set the document listener when loaded to the two buttons: 
 document.addEventListener('DOMContentLoaded',function(){
 	// console.log("DOM got loaded");
 	// document.addEventListener('click', setMessage('Please click Start'));
 	document.getElementById('start').addEventListener('click', start);
-	document.getElementById('cStart').addEventListener('click', start);
+	document.getElementById('cStart').addEventListener('click', compStart);
 
 });
-//console.log('loaded! again');
-//this function starts the game;
+
+
+// Function starts the game and add event listener to the div boxes: 
 function start(){
-	console.log('Starting game');
+	console.log('Starting game!!!');
+	boardTiles.forEach(function(element){
+		document.getElementById(element).addEventListener('click', nextMove)
+	});
 	var num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 	for (var i = 0; i <= 8; i++){
 		clearBox(num[i]);
-	}
+	};
 	turn = 'X';
 	setMessage(turn + ' is starting:');
 }
+
+// computer start: 
+function compStart(){
+	console.log('Computer is starting the game');
+	var num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	for (var i = 0; i <= 8; i++){
+		clearBox(num[i]);
+	};
+	document.getElementById(num[Math.floor(Math.random() * num.length)]).innerText = 'X';
+
+}
+// Set the messages above the game board: 
 function setMessage(m) {
 	document.getElementById('message').innerText = m;
 }
+// Clear the Game board when the game is restarted: 
 function clearBox(number) {
 	document.getElementById(number).innerText = "";
 	winner = null;
 }
+// Check the tile value when needed to see either X is inside or O is inside: 
 function getBoxValue(idNumber) {
 	// var abc = document.getElementById(idNumber).innerText;
 	// console.log(abc)
 	return document.getElementById(idNumber).innerText;
 }
+// Check if tiles a b c are all the same: 
 function checkRow(a, b, c, move){
 	var result = false;
 	if (getBoxValue(a) == move && getBoxValue(b) == move && getBoxValue(c) == move){
 		result = true;
 	} return result;
 }
-// if there is three symble in a row with the same turn. then the turn wins. 
+// if there is three symble in a row with the same turn. then the turn wins:  
 function checkWin(currentTurn) {
 	//console.log('is checking win!...');
 	var result = false;
@@ -70,12 +93,10 @@ function checkWin(currentTurn) {
 		checkRow('two', 'five', 'eight', currentTurn) ||
 		checkRow('three', 'five', 'seven', currentTurn) ||
 		checkRow('three', "six", 'nine', currentTurn) ) 
-	{
-		result = true;
-	}
+		{result = true;}
 	return result;
 }
-//Checks if the game is Draw.
+//Checks if the game is Draw by checking if all the tiles are filled:
 function checkDraw() {
 	//console.log('is checking Draw!')
 	var result = 0;
@@ -90,7 +111,8 @@ function checkDraw() {
 		return false;
 	}
 }
-//This function keeps track of who's turn:
+
+//This function changes turns when we need to change from X player or O player:
 function switchTurn(){
 	// console.log("TURN");
 	if (turn == 'X'){
@@ -100,15 +122,18 @@ function switchTurn(){
 	}setMessage('It\'s '  + turn + ' turn now!' );
 }
 
-function nextMove(square){
-	if (square.innerText == 'O' || square.innerText == 'X'){
+// when the div box is clicked, the nextMove is called to filled the tile if the tile is empty 
+// and no winner is found or game is draw: 
+function nextMove(){
+	console.log('nextmove is clicked!!!')
+	if (this.innerText == 'O' || this.innerText == 'X'){
 		if (winner == null) {
 			setMessage('Please Pick another cell');
 		}else{
 			setMessage('This game has ended, please start another game!');
 		}
 	}else if (winner == null) {
-		square.innerText = turn;
+		this.innerText = turn;
 		if (checkWin(turn)) {
 			winner = turn;
 			console.log(checkWin(turn));
@@ -129,29 +154,59 @@ function nextMove(square){
 
 
 
+// ============================< minimax > ============================
 
+
+// Heuristic function: 
+function score(computer) {
+	if (winner == computer) {
+		return 1;
+	}else if (winner == 'no one') {
+		return 0;
+	}else{
+		return -1;
+	}
+}
+// This function returns all the possible moves of the current state:
+function actions(state){ // need to use the state pre
+	var emptySquareArray = [];
+	var num = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	for (var i = 0; i <= 8; i++){
+		if (getBoxValue(num[i]) !== 'X' && getBoxValue(num[i]) !== 'O') {
+			emptySquareArray.push(i);
+		}
+	}
+	console.log(emptySquareArray);
+	return emptySquareArray;
+}
 // if winner is not null, then the game has ended! 
+// function minimax(gamePlayer, depth) {
+	
+// 	if (winner!==null) {
+// 		return score(gamePlayer);
+// 	} 
+// 	var	scoreArray = [];
+// 	var	moves = [];
+// 	var emptySquares = actions();
 
+// 	if (turn==gamePlayer) {
+// 		emptySquareArray.forEach(function(element){
+// 			value = Math.max(value, minimax('X', depth−1, ));
+// 			return value;
+// 		})
+// 	}else {
+// 		emptySquareArray.forEach(function(element){
+// 			value = Math.min(value, minimax('O', depth−1, );
+// 				return value
+// 			})
+// 		}
+// 	}
 
+// 	function bestmove(){
+// 		var emptySquares = actions();
+// 		return emptySquares[Math.floor(Math.random() * emptySquares.length)];
+// 	}
 
-// function minimax(game, depth, maximizingPlayer) {
-// 	if (game) {}
-
-
-
-// }
-// if depth = 0 or node is a terminal node then
-// 	return the heuristic value of node
-// if maximizingPlayer then
-// 	value := −∞
-// for each child of node do
-// 	value := max(value, minimax(child, depth − 1, FALSE))
-// return value
-// else (* minimizing player *)
-// 	value := +∞
-// for each child of node do
-// 	value := min(value, minimax(child, depth − 1, TRUE))
-// return value
 
 
 
