2af17fffe7351026d80b66b5d6c0476d2aa9fae9
tam-en <tamis@leftelbow.com>
2018-12-02 13:05:19 -0800

    function matches clicked cell to cell in cell array

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+// global variables------------------------------------------
 
 var startButtonText = 'start game!'
 var currentPlayer = '';
 var ticTacState = 'neutral';
 var toeState = 'neutral';
+
+//function to randomly assign initial player-----------------
 
 var getPlayer = function() {
 	if (Math.random() > .5) {
@@ -12,8 +15,23 @@ var getPlayer = function() {
 	};
 }
 getPlayer();
-console.log('player is ', currentPlayer);
+console.log('the starting player is ', currentPlayer);
 
+//starting values for all of the cells----------------------
+
+var allTheCellsArray = [
+	['A1n', '', 'A', '1', 'n', '' ],
+	['A2', '', 'A', '2', '', ''],
+	['A3p', '', 'A', '3', '', 'p'],
+	['B1', '', 'B', '1', '', ''],
+	['B2pn', '', 'B', '2', 'n', 'p'],
+	['B3', '', 'B', '3', '', ''],
+	['C1p', '', 'C', '1', '', 'p'],
+	['C2', '', 'C', '2', '', ''],
+	['C3n', '', 'C', '3', 'n', '']
+];
+
+//event listeners!-------------------------------------------
 
 document.getElementById("A1n").addEventListener('click', clickedCell);
 document.getElementById("A2").addEventListener('click', clickedCell);
@@ -30,32 +48,31 @@ document.getElementById("ticTacAvatar").addEventListener('click', clickedAvatar)
 
 function updatePlayerScore(cell) {
 	console.log('passed', cell, 'to updatePlayerScore function');
-
 }
-
 
 function clickedCell(event) {
 	//check whether cell is already occupied;
 	// if(this.id.occupier != '') {
-	var cellObject = this.id;
-	console.log('blah' + cellObject);
-	console.log('please' + A1n.occupier);
-	console.log(cellObject.occupier);
-	// } else {
-	  console.log("you clicked on " + this.id);
-	  //updatePlayerScore(this.id);
+	var cellObject = this.id.toString();
+	findTheCell(cellObject);
+
+	//updatePlayerScore(this.id);
 	// }
+}
+
+
+function findTheCell(cellName) {
+	for (i = 0; i < allTheCellsArray.length; i++) {
+		if (allTheCellsArray[i][0] === cellName) {
+			console.log(cellName + ' cell is at index ' + i);
+			return i;
+		}
+	}
 }
 
 function clickedAvatar(event) {
 	console.log("you clicked on " + this.id);
 }
 
-var A1n = {
-	occupier:'ticTac',
-	rowId: 'A',
-	colId: 1,
-	slopeN: 'n',
-	slopeP: ''
-}
+
 
