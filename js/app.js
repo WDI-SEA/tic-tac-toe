d540abd19655a12f32a5e7b45db60b1e8ab47a4c
tam-en <tamis@leftelbow.com>
2018-12-02 10:23:27 -0800

    cell and avatar listeners up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
-console.log('Hello frontend');
+
+var startButtonText = 'start game!'
+var currentPlayer = '';
+var ticTacState = 'neutral';
+var toeState = 'neutral';
+
+var getPlayer = function() {
+	if (Math.random() > .5) {
+		currentPlayer = 'ticTac';
+	} else {
+		currentPlayer = 'toe';
+	};
+}
+getPlayer();
+console.log('player is ', currentPlayer);
+
+
+document.getElementById("A1n").addEventListener('click', clickedCell);
+document.getElementById("A2").addEventListener('click', clickedCell);
+document.getElementById("A3p").addEventListener('click', clickedCell);
+document.getElementById("B1").addEventListener('click', clickedCell);
+document.getElementById("B2pn").addEventListener('click', clickedCell);
+document.getElementById("B3").addEventListener('click', clickedCell);
+document.getElementById("C1p").addEventListener('click', clickedCell);
+document.getElementById("C2").addEventListener('click', clickedCell);
+document.getElementById("C3n").addEventListener('click', clickedCell);
+document.getElementById("toeAvatar").addEventListener('click', clickedAvatar);
+document.getElementById("ticTacAvatar").addEventListener('click', clickedAvatar);
+
+
+function updatePlayerScore(cell) {
+	console.log('passed', cell, 'to updatePlayerScore function');
+
+}
+
+
+function clickedCell(event) {
+	//check whether cell is already occupied;
+	// if(this.id.occupier != '') {
+	var cellObject = this.id;
+	console.log('blah' + cellObject);
+	console.log('please' + A1n.occupier);
+	console.log(cellObject.occupier);
+	// } else {
+	  console.log("you clicked on " + this.id);
+	  //updatePlayerScore(this.id);
+	// }
+}
+
+function clickedAvatar(event) {
+	console.log("you clicked on " + this.id);
+}
+
+var A1n = {
+	occupier:'ticTac',
+	rowId: 'A',
+	colId: 1,
+	slopeN: 'n',
+	slopeP: ''
+}
+
