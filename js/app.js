763ce000b41eb24f331fd8c4b35c8fdc0f7491b7
chidrome <chidrome@gmail.com>
2018-12-03 00:11:37 -0800

    finished

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var winConditions = [["one","two","three"],["one","four","seven"],["one","five",
 ["seven","eight","nine"],["three","five","seven"]]
 var dougs = []
 var dans = []
+var dougsPoints = 0;
+var dansPoints = 0;
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -12,8 +14,8 @@ console.log('Hello frontend');
 
 document.getElementById("start").addEventListener("click", start)
 
-
 function start() {
+	resetGame();
 	addBoxListeners();
 }
 
@@ -25,7 +27,7 @@ function clicked() {
 		dougs.push(this.id);
 		// add a class name so that the box will change the background image
 		this.classList.add("dougie");
-		// turn off the even listener so I can't click the same box again
+		// turn off the event listener so I can't click the same box again
 		this.removeEventListener("click", clicked);
 		definePlayer = false;
 	} else {
@@ -33,12 +35,12 @@ function clicked() {
 		dans.push(this.id);
 		// add a class name so that the box will change the background image
 		this.classList.add("dp");
-		// turn off the even listener so I can't click the same box again
+		// turn off the event listener so I can't click the same box again
 		this.removeEventListener("click", clicked);
 		definePlayer = true;
-	} keepScore();
+	};
+	keepScore();
 	console.log(dougs);
-	console.log(dans);
 }
 
 //add event listeners to the boxes
@@ -48,29 +50,84 @@ function addBoxListeners() {
 	}
 }
 
+function removeBoxListeners(){
+	for(var i = 0; i < allBoxes.length; i++) {
+		allBoxes[i].removeEventListener("click",clicked);
+	}
+}
+
 function keepScore(){
-	for(var i = 0; i <winConditions.length; i++) {
+	for(var i = 0; i < winConditions.length; i++) {
 		let innerArr = winConditions[i];
-		let dougsPoints = 0;
-		let dansPoints = 0;
-		for(var j = 0; j < innerArr.length; j++)
+		for(var j = 0; j < innerArr.length; j++) {
 			if (dougs.includes(innerArr[j])) {
-				dougsPoints + 1;
-			} if (dans.includes(innerArr[j])) {
-				dansPoints + 1;
-			};
+				dougsPoints++;
+				checkWin();
+				checkTie();
+			} else if (dans.includes(innerArr[j])) {
+				dansPoints++;
+				checkWin();
+				checkTie();
+			}
+		}
+		dougsPoints = 0;
+		dansPoints = 0;
 	};
-	checkWin();
+}
+
+function resetGame(){
+	// resets all the boxes back to blank's
+	for(var i = 0; i < allBoxes.length; i++) {
+		allBoxes[i].classList.remove("dougie");
+		allBoxes[i].classList.remove("dp");
+	}
+	stopGame();
+	document.getElementById("results").style.color = "white";
+	document.getElementById("results").innerHTML = "Who will win?";
 }
 
 function checkWin() {
 	if (dougsPoints === 3) {
-		console.log("DOUG WINS!!!");
+		document.getElementById("results").innerHTML = "DOUG WINS!";
+		document.getElementById("results").style.color = "green";
+		// set this winning player to go first next
+		definePlayer = true;
+		stopGame();
 	} else if (dansPoints === 3) {
-		console.log("DAN WINS!!!!");
+		document.getElementById("results").innerHTML = "DAN WINS!";
+		document.getElementById("results").style.color = "blue";
+		// set this winnging player to go first next
+		definePlayer = false;
+		stopGame();
 	}
 }
 
-})
+function checkTie() {
+	if (dougs.length === 5) {
+		checkWin();
+		definePlayer = true;
+		stopGame();
+		document.getElementById("results").innerHTML = "It's a TIE!";
+		document.getElementById("results").style.color = "red";
+		console.log(definePlayer);
+	} else if (dans.length ===5) {
+		checkWin();
+		definePlayer = false;
+		stopGame();
+		document.getElementById("results").innerHTML = "It's a TIE!";
+		document.getElementById("results").style.color = "red";
+		console.log(definePlayer);
+	}
+}
 
+// stop the game if there's been a winner
+function stopGame() {
+	//remove all box listeners when there's a winner
+	removeBoxListeners();
+	//set the arrays back to blank
+	dougs = [];
+	dans = [];
+}
+
+})
 
