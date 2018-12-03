946ca7846d370339db78b45a7d85e2b6eb296941
tam-en <tamis@leftelbow.com>
2018-12-02 17:22:21 -0800

    added scorecard arrays

--- js/app.js
+++ js/app.js
@@ -24,12 +24,34 @@ var allTheCellsArray = [
 	['A2', '', 'A', '2', '', ''],
 	['A3p', '', 'A', '3', '', 'p'],
 	['B1', '', 'B', '1', '', ''],
-	['B2pn', '', 'B', '2', 'n', 'p'],
+	['B2np', '', 'B', '2', 'n', 'p'],
 	['B3', '', 'B', '3', '', ''],
 	['C1p', '', 'C', '1', '', 'p'],
 	['C2', '', 'C', '2', '', ''],
 	['C3n', '', 'C', '3', 'n', '']
 ];
+
+//scoreCard array . . . ticTac=index 0, toe=index 1 ---------
+
+var scoreCard = [[
+0, //index 0 = ticTac's A row score
+0, //index 1 = ticTac's B row score
+0, //index 2 = ticTac's C row score
+0, //index 3 = ticTac's 1 column score
+0, //index 4 = ticTac's 2 column score
+0, //index 5 = ticTac's 3 column score
+0, //index 6 = ticTac's n-slope score
+0, //index 7 = ticTac's p-slope score
+], [
+0, //index 0 = toe's A row score
+0, //index 1 = toe's B row score
+0, //index 2 = toe's C row score
+0, //index 3 = toe's 1 column score
+0, //index 4 = toe's 2 column score
+0, //index 5 = toe's 3 column score
+0, //index 6 = toe's n-slope score
+0, //index 7 = toe's p-slope score
+]]
 
 //event listeners!-------------------------------------------
 
@@ -37,7 +59,7 @@ document.getElementById("A1n").addEventListener('click', clickedCell);
 document.getElementById("A2").addEventListener('click', clickedCell);
 document.getElementById("A3p").addEventListener('click', clickedCell);
 document.getElementById("B1").addEventListener('click', clickedCell);
-document.getElementById("B2pn").addEventListener('click', clickedCell);
+document.getElementById("B2np").addEventListener('click', clickedCell);
 document.getElementById("B3").addEventListener('click', clickedCell);
 document.getElementById("C1p").addEventListener('click', clickedCell);
 document.getElementById("C2").addEventListener('click', clickedCell);
@@ -51,28 +73,18 @@ function updatePlayerScore(cell) {
 }
 
 function clickedCell(event) {
-	//check whether cell is already occupied;
-	// if(this.id.occupier != '') {
 	var cellObject = this.id.toString();
-	findTheCell(cellObject);
-
-	//updatePlayerScore(this.id);
-	// }
-}
-
-
-function findTheCell(cellName) {
 	for (i = 0; i < allTheCellsArray.length; i++) {
-		if (allTheCellsArray[i][0] === cellName) {
-			console.log(cellName + ' cell is at index ' + i);
-			return i;
-		}
-	}
+		if (allTheCellsArray[i][0] === cellObject) {
+			console.log(cellObject + ' cell is at index ' + i);
+			var currentCellArrayLocation = i;
+		};
+	};
+
 }
+
+
 
 function clickedAvatar(event) {
 	console.log("you clicked on " + this.id);
 }
-
-
-
