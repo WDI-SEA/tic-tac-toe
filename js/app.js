e1a8ac2e9385158177b8fe97a6499a09ef871797
hayeun7777 <kellyhypark@gmail.com>
2018-12-02 22:01:28 -0800

    almost done

--- js/app.js
+++ js/app.js
@@ -9,105 +9,88 @@ var xMarks = [];
 var oMarks = [];
 
 const winCombos = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
+	["one", "two", "three"],
+	["four", "five", "six"],
+	["seven", "eight", "nine"],
+	["one", "four", "seven"],
+	["two", "five", "eight"],
+	["three", "six", "nine"],
+	["one", "five", "nine"],
+	["three", "five", "seven"]
 ];
 
 var cells = document.querySelectorAll('.cell');
 
+
 function playGame(){
-	document.querySelector(".endGame").style.display = "none";
+	// document.querySelector(".endGame").style.display = "none";
 	initialBoard = Array.from(Array(9).keys());
 	for(i = 0; i < cells.length; i++){
 	cells[i].innerText = '';
-	// cells[i].style.removeProperty("background-color");
 	cells[i].addEventListener("click", turnClick)
-	}	
+	document.getElementById("reset").addEventListener("click", resetBtn);
+	}
 }
 
 function turnClick(event){
 	if (event.target.innerText.length === 0){
 		if (moves % 2 ===0){
 			xMarks.push(event.target.id);
-			console.log(xMarks);
+			console.log("xMarks", xMarks);
 			document.getElementById(event.target.id).innerText = "X";
-			document.getElementById(event.target.id).style.background = "green";
+			document.getElementById(event.target.id).style.background = "#FF85F3";
 		}
 		else{
 			oMarks.push(event.target.id);
-			console.log(oMarks);
+			console.log("oMarks", oMarks);
 			document.getElementById(event.target.id).innerText = "O";
-			document.getElementById(event.target.id).style.background = "blue";
+			document.getElementById(event.target.id).style.background = "#64CFE8";
 		}
 	}
 	else{
 		alert("You already Clicked!");
 	}
 	moves++;
-	winCheck(xMarks);
-	winCheck(oMarks);
+	winCheck(oMarks, xMarks, winCombos);
 	if (moves === 9){
 		alert("It's a Draw! Reset the board to play again");
 	}
 }
-
-// function turnClick(event){	
-// 	if (player1 == true){
-// 	document.getElementById(event.target.id).innerText = "O"; //target.id refers cells[i]
-// // console.log("this is what the event's grabbing", event.target); //for checking
-// 	player1 = 0; 
-// 	} else {
-// 		document.getElementById(event.target.id).innerText = "X";
-// 		player1 = 1;
-// 	}
-// 		moves++;
-// }
 
 function winCheck(oMarks, xMarks, winCombos){
 		for(var i = 0; i < winCombos.length; i++){
 				var oWins = 0;
 				var xWins = 0;
 					 for(var j = 0; j < winCombos[i].length; j++){
-			 		// var innerVal = winCombos[i][j];
-			 			if(winCombos[i].indexOf(oMarks) != -1){
+			 			if(oMarks.includes(winCombos[i][j])){
 			 				oWins++;
-			 			}
-			 			if(winCombos[i].indexOf(xMarks) != -1){
-			 				xWins++
+			 				console.log(oWins);
 			 			}
 			 			if(oWins === 3){
+			 				playGame();
 			 				alert("O wins!");
 			 			}
+			 			if(xMarks.includes(winCombos[i][j])){
+			 				xWins++;
+			 				console.log(xWins);
+			 			}	 			
 			 			if(xWins === 3){
-			 				alert("X wins!");
-			 			}
-			 			else{
 			 				playGame();
+			 				alert("X wins!");
 			 			}
 			 			}
 			 		}
 	}
 
-	
-function stopGame(messageContent){
-	//different message pops ("win"/"lost")
-	document.getElementById("message").textContent = messageContent;
-}
+function resetBtn(){
+var resetBtn = document.getElementById("reset");
+	for(i = 0; i < cells.length; i++){
+		cells[i].innerText != '';
+		cells[i].style.background = "transparent";
+	}
+		xMarks = new Array();
+		oMarks = new Array();
+		moves = 0;
+		playGame();
+	}
 
-function winTheGame(){
-	stopGame("You won!");
-}
-
-function tieTheGame(){
-	stopGame("You both suck!")
-}
-
-function loseTheGame(){
-	stopGame("Boo, You lost!");
-}
