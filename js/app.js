4cba9dabf261fecc5a4523e018cd1a53d56ebb76
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2019-01-27 15:51:02 -0800

    created general outline for game

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log('Hello frontend');
+console.log('Up and Running!');
+
+// required variables
+
+var currentPlayer = 0;
+var playerOne = [];
+var playerTwo = [];
+var playerOneScore = 0;
+var playerTwoScore = 0;
+var gameOver = false;
+
+var clickSquare = function() {
+    if (currentPlayer === 0) {
+        this.textContent = "X";
+        playerOne.push(this);
+        currentPlayer += 1;
+    }   else {
+        this.textContent = "O";
+        playerTwo.push(this);
+        currentPlayer -= 1;
+    }
+}
+
+var squares = document.querySelectorAll(".square");
+//create array for loop
+squares.forEach(function(square) {
+    square.addEventListener("click", clickSquare);
+});
+
+var btn = document.getElementsByTagName("button");
+button.addEventListener("click", reset);
+}
