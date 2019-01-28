165e36d8b128d39eca147fe2a473871a4cb92740
userpc <userpc@mail.com>
2019-01-28 09:08:36 -0800

    finished game without conditional count

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var playerO = [];
 var reset = function(){
         location.reload();
     };
-var count = 0;
-    
+var count = 0; 
+
 winCombos = [
     ["1","2","3"],
     ["4","5","6"],
@@ -17,15 +17,15 @@ winCombos = [
 ];
     
 var gameboard = {
-    1 : 0,
-    2 : 0,
-    3 : 0,
-    4 : 0,
-    5 : 0,
-    6 : 0,
-    7 : 0,
-    8 : 0,
-    9 : 0
+    1 : true,
+    2 : true,
+    3 : true,
+    4 : true,
+    5 : true,
+    6 : true,
+    7 : true,
+    8 : true,
+    9 : true
 };
 //reset button and click counter
 document.getElementById("button").addEventListener('click', reset);
@@ -38,54 +38,56 @@ document.getElementById("tictactoe").addEventListener('click', function(){
       
 })
 //page loads and waits for input
+var checkWin1 = function(){
+    winCombos.forEach(function(combo) {
+
+            if (
+                playerX.includes(combo[0]) &&
+                playerX.includes(combo[1]) &&
+                playerX.includes(combo[2])
+                ) {
+              document.getElementById("turns").textContent = "Player X Wins!";
+              gameOn = false;
+                }
+            });
+        }
+    var checkWin2 = function(){
+        winCombos.forEach(function(combo) {
+            if (
+                playerO.includes(combo[0]) &&
+                playerO.includes(combo[1]) &&
+                playerO.includes(combo[2])
+                ) {
+                    document.getElementById("turns").textContent = "Player O Wins!";
+                    gameOn = false;
+                }
+            });
+        }
+        //Toggles between players and fills with correct mark
+        var markBox = function(){
+            if ( gameOn === true && count % 2 !== 1 && document.getElementById(this.id).classList.contains("unmarked") ){
+                //player x marks the box
+                document.getElementById(this.id).textContent = "X";
+                document.getElementById("turns").textContent = "Player O's turn";
+                playerX.push(this.id);
+                console.log(playerX);
+                document.getElementById(this.id).classList.remove("unmarked");
+                checkWin1();
+            } 
+            if ( gameOn === true && count % 2 === 1 && document.getElementById(this.id).classList.contains("unmarked")){      
+                //player y marks the box
+                document.getElementById(this.id).textContent = "O";    
+                document.getElementById("turns").textContent = "Player X's turn";
+                playerO.push(this.id);
+                console.log(playerO);
+                document.getElementById(this.id).classList.remove("unmarked");
+                checkWin2();            
+            } 
+};
 document.addEventListener("DOMContentLoaded",function(){
     for ( var space in gameboard){
         document.getElementById(space).addEventListener('click', markBox);
         gameOn = true;
         //win logic
-        var checkWin1 = function(){
-            winCombos.forEach(function(combo) {
-
-                if (
-                    playerX.includes(combo[0]) &&
-                    playerX.includes(combo[1]) &&
-                  playerX.includes(combo[2])
-                  ) {
-                      document.getElementById("turns").textContent = "Player X Wins!";
-                      gameOn = false;
-                    }
-                });
-            }
-            var checkWin2 = function(){
-                winCombos.forEach(function(combo) {
-                    if (
-                        playerO.includes(combo[0]) &&
-                        playerO.includes(combo[1]) &&
-                        playerO.includes(combo[2])
-                        ) {
-                            document.getElementById("turns").textContent = "Player O Wins!";
-                            gameOn = false;
-                        }
-                    });
-                }
-                //Toggles between players and fills with correct mark
-                var markBox = function(){
-                    if ( gameOn === true && count % 2 !== 1 ){
-                        //player x marks the box
-                        document.getElementById(this.id).textContent = "X";
-                        document.getElementById("turns").textContent = "Player O's turn";
-                        playerX.push(this.id);
-                        
-                        checkWin1();
-                    } 
-                    if ( gameOn === true && count % 2 === 1 ){      
-                        //player y marks the box
-                        document.getElementById(this.id).textContent = "O";    
-                        document.getElementById("turns").textContent = "Player X's turn";
-                        playerO.push(this.id);
-                
-                        checkWin2();            
-                    } 
-        };
     }
 })
