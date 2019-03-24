eabe7fb58e25a53b53a4c0623896734a0fd42f55
greypanda13 <greypanda13@gmail.com>
2019-03-23 18:23:19 -0700

    added buttons, styling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,41 @@
+//buffer
+var isXsTurn = true;
+
+var c1 = document.getElementById('c1');
+var c2 = document.getElementById('c2');
+var c3 = document.getElementById('c3');
+var c4 = document.getElementById('c4');
+var c5 = document.getElementById('c5');
+var c6 = document.getElementById('c6');
+var c7 = document.getElementById('c7');
+var c8 = document.getElementById('c8');
+var c9 = document.getElementById('c9');
+
 console.log('Hello frontend');
 
+//DO WHEN "NEW MATCH" BUTTON CLICKED
+function newMatchEvents() {
+  //MAKE EVENT LISTENERS ON ALL CELLS
+  //MAKE IT WHOMEVER'S TURN
+
+
+}
+
+
+var cellClickedEvents = function() {
+  //MARK BOX CLICKED
+
+  //
+}
 
 
 
-var turnedOrNot = document.getElementById('c1');
-console.log(turnedOrNot.getAttribute('data-turn'));
+
+
+
+
+
+
+
+
+console.log(c1.getAttribute('data-turn'));
