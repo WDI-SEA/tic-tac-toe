f3243c67c5cea7c6511ccdeacc92ccb9bfe887d9
Bryan Brinson <bryan@beb.com>
2019-07-29 08:41:17 -0700

    bah! partially functional connect four

--- js/main.js
+++ js/main.js
@@ -9,42 +9,61 @@ var boardColumn6 = [];
 var winner = null;
 var iterations = 0;
 
+var h0Count = 0;
+var h1Count = 0;
+
 // add random start for player 1 or 2
 var playerNum = null; // player 0 = red, player 1 = blue.
 
+let resetGlobals = function() {
+  boardColumn0 = [];
+  boardColumn1 = [];
+  boardColumn2 = [];
+  boardColumn3 = [];
+  boardColumn4 = [];
+  boardColumn5 = [];
+  boardColumn6 = [];
+
+  winner = null;
+  iterations = 0;
+
+  h0Count = 0;
+  h1Count = 0;
+
+  // add random start for player 1 or 2
+  playerNum = null; // player 0 = red, player 1 = blue.
+};
+
 document.addEventListener("DOMContentLoaded", function() {
-  // Other load options?
-  document.getElementById("new-game").addEventListener("click", newGame);
+  document.getElementById("start-game").addEventListener("click", function() {
+    //clear the board
+    //clear the listeners
+    //clear the column arrays
+
+    // Set starting player. Could randomize.
+    resetGlobals();
+
+    playerNum = 0;
+    // startPlay(playerNum);
+    // debugger;
+
+    removeColumnListeners();
+    clearGameBoard();
+
+    if (playerNum === 0) {
+      // Setup listeners on each column. This is where we "drop token"
+      let columns = document.getElementsByClassName("board-column");
+
+      for (let i = 0; i < columns.length; i++) {
+        document
+          .getElementById(columns[i].id)
+          .addEventListener("click", dropTokenInColumn);
+      }
+    }
+  });
 });
 
-function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-  // return Math.floor(Math.random() * Math.floor(max));
-}
-
-let newGame = function() {
-  //clear the board
-  //clear the listeners
-  //clear the column arrays
-
-  // randomly select a starig player.
-  playerNum = 0;
-  nextMove(playerNum);
-};
-
-let nextMove = function(playerNum) {
-  // debugger;
-  if (playerNum === 0) {
-    // Setup listeners on each column. This is where we "drop token"
-    let columns = document.getElementsByClassName("board-column");
-
-    for (let i = 0; i < columns.length; i++) {
-      document
-        .getElementById(columns[i].id)
-        .addEventListener("click", dropTokenInColumn);
-    }
-  }
-};
+// let startPlay = function(playerNum) {};
 
 let dropTokenInColumn = function(c) {
   let colNum;
@@ -57,6 +76,7 @@ let dropTokenInColumn = function(c) {
       // c was random int.
       colNum = c;
     }
+
     switch (colNum) {
       case 1:
         boardColumn0.push(playerNum);
@@ -90,15 +110,17 @@ let dropTokenInColumn = function(c) {
         console.log("dropTokenInColumn fell into the default switch. Eh?!");
         break;
     }
+  }
 
-    if (testWinner(playerNum)) {
-      endGame(playerNum);
+  if (testWinner() === true) {
+    endGame(playerNum);
+  } else {
+    if (playerNum === 0) {
+      playerNum = 1;
+      h0Count = 0;
     } else {
-      if (playerNum === 0) {
-        playerNum = 1;
-      } else {
-        playerNum = 0;
-      }
+      playerNum = 0;
+      h1Count = 0;
     }
   }
 
@@ -140,29 +162,253 @@ let dropTokenInColumn = function(c) {
         console.log("dropTokenInColumn fell into the default switch. Eh?!");
         break;
     }
+  }
 
-    if (testWinner(playerNum)) {
-      endGame(playerNum);
+  if (testWinner() === true) {
+    endGame(playerNum);
+  } else {
+    if (playerNum === 0) {
+      playerNum = 1;
+      h0Count = 0;
     } else {
-      if (playerNum === 0) {
-        playerNum = 1;
+      playerNum = 0;
+      h1Count = 0;
+    }
+  }
+};
+
+let testWinner = function(playerNum) {
+  let isWinningRowTotal = function() {
+    if (h0Count === 4) {
+      // return 'endGame(playerNum)';
+      return true;
+    } else if (h1Count === 4) {
+      // return 'endGame(playerNum)';
+      return true;
+    } else {
+      console.log(`{h0Count: ${h0Count}, h1Count: ${h1Count}}`);
+    }
+  };
+  // Check the rows
+
+  for (let i = 0; i < 6; i++) {
+    if (boardColumn0[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn0[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+
+    if (boardColumn1[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn1[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+
+    if (boardColumn2[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn2[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+
+    if (boardColumn3[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn3[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+
+    if (boardColumn4[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn4[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+
+    if (boardColumn5[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn5[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+
+    if (boardColumn6[i] === 0) {
+      h0Count++;
+      isWinningRowTotal();
+    } else if (boardColumn6[i] === 1) {
+      h1Count++;
+      isWinningRowTotal();
+    }
+  }
+
+  // Check the diags
+
+  // Check the columns. This should be for looh over an
+  // array of column arrays.
+  if (boardColumn0.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn0.length; i++) {
+      if (boardColumn0[i] === 0) {
+        v0Count++;
+        v1Count = 0;
       } else {
-        playerNum = 0;
+        v0Count = 0;
+        v1Count++;
       }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 0, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
+    }
+  }
+  if (boardColumn1.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn1.length; i++) {
+      if (boardColumn1[i] === 0) {
+        v0Count++;
+        v1Count = 0;
+      } else {
+        v0Count = 0;
+        v1Count++;
+      }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 1, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
+    }
+  }
+  if (boardColumn2.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn2.length; i++) {
+      if (boardColumn2[i] === 0) {
+        v0Count++;
+        v1Count = 0;
+      } else {
+        v0Count = 0;
+        v1Count++;
+      }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 2, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
+    }
+  }
+  if (boardColumn3.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn3.length; i++) {
+      if (boardColumn3[i] === 0) {
+        v0Count++;
+        v1Count = 0;
+      } else {
+        v0Count = 0;
+        v1Count++;
+      }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 3, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
+    }
+  }
+  if (boardColumn4.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn4.length; i++) {
+      if (boardColumn4[i] === 0) {
+        v0Count++;
+        v1Count = 0;
+      } else {
+        v0Count = 0;
+        v1Count++;
+      }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 4, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
+    }
+  }
+  if (boardColumn5.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn5.length; i++) {
+      if (boardColumn5[i] === 0) {
+        v0Count++;
+        v1Count = 0;
+      } else {
+        v0Count = 0;
+        v1Count++;
+      }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 5, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
+    }
+  }
+  if (boardColumn6.length > 0) {
+    let v0Count = 0;
+    let v1Count = 0;
+    for (let i = 0; i < boardColumn6.length; i++) {
+      if (boardColumn6[i] === 0) {
+        v0Count++;
+        v1Count = 0;
+      } else {
+        v0Count = 0;
+        v1Count++;
+      }
+    }
+    if (v0Count === 4) {
+      return true;
+    } else if (v1Count === 4) {
+      return true;
+    } else {
+      // console.log(`{Column: 6, v0Count: ${v0Count}, v1Count: ${v1Count}}`);
     }
   }
 };
 
 let endGame = function(p) {
   console.log(`Player ${p} won!`);
+
+  resetGlobals();
+  removeColumnListeners();
+
+  document.getElementById('victory').textContent = `Player ${p.toString()} is victorious!`;
+
 };
 
+///////////////////////////////////////////////////////////////////////////////
+
 let setGridColor = function(colNum, p, colArray) {
-  // console.log(`Dropped token for player number: ${p}`);
-
   if (p === 0) {
-    // console.log(event.id, colArray.length);
-
     if (colNum === 1) {
       let fillPosition = 6 - (colArray.length - 1);
       document.getElementById(
@@ -186,48 +432,29 @@ let setGridColor = function(colNum, p, colArray) {
         `board-postion-${fillPosition}`
       ).style.backgroundColor = "blue";
     }
-    // set the background to "blue"
   }
 };
 
-let testWinner = function(playerNum) {
-  // for (let i = 0; i < 7; i++) {
-  //   get cheld elements from columns
-  // }
+let randInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  // return Math.floor(Math.random() * Math.floor(max));  // 0 to max.
+};
 
-  //  Check the columns
-  
+let removeColumnListeners = function() {
+  let columns = document.getElementsByClassName("board-column");
 
-  // Check the rows
-
-
-  // Check the diags
-
-  if (boardColumn0.length > 0) {
-    console.log(`Column 0: ${boardColumn0}`);
+  for (let i = 0; i < columns.length; i++) {
+    document
+      .getElementById(columns[i].id)
+      .removeEventListener("click", dropTokenInColumn);
   }
-  if (boardColumn1.length > 0) {
-    console.log(`Column 1: ${boardColumn1}`);
-  } 
-  if (boardColumn2.length > 0) {
-    console.log(`Column 2: ${boardColumn2}`);
-  } 
-  if (boardColumn3.length > 0) {
-    console.log(`Column 3: ${boardColumn3}`);
-  } 
-  if (boardColumn4.length > 0) {
-    console.log(`Column 4: ${boardColumn4}`);
-  } 
-  if (boardColumn5.length > 0) {
-    console.log(`Column 5: ${boardColumn5}`);
-  } 
-  if (boardColumn6.length > 0) {
-    console.log(`Column 6: ${boardColumn6}`);
+};
+
+let clearGameBoard = function() {
+  var bPosition = document.getElementsByClassName("board-position");
+  for (let i = 0; i < bPosition.length; i++) {
+    bPosition[i].style.backgroundColor = "white";
   }
 
-  if (1 != 1) {
-    return playerNum;
-  } else {
-    console.log("no winner, next move");
-  }
+  document.getElementById('victory').textContent = '';
 };
