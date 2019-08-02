88f8ebea4de656ec9d07d5488aad2c2a011a0140
larynxmith <larynxmith@gmail.com>
2019-08-02 12:09:58 -0700

    logic fixed, buttons functional; img reload issue

--- js/app.js
+++ js/app.js
@@ -1,169 +1,195 @@
 console.log('Hello frontend');
-//variables
-var timesClicked = 0, playerXScore = 0, PlayerOScore = 0, Ties = 0;
-console.log("timesClicked =", timesClicked);
-// var winCombos = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
-// var xClickedArr = [1,4,7];
+// variables
+var timesClicked = 0;
+var playerXScore = 0;
+var playerOScore = 0;
+var ties = 0;
+var gameArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+var gameArrIndex;
+
+
+// console.log("timesClicked =", timesClicked);
+
 
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("restart-button").addEventListener("click", start);
 });
 
+document.getElementById("reset-scores").addEventListener("click", resetScores);
+
+// cached elements
 var resetScoreButton = document.getElementById("reset-scores");
 var restartGameButton = document.getElementById("restart-button");
 var messageBoard = document.getElementById("message-board");
 var boxes = document.querySelectorAll("div.grid-box");
+var xScore = document.getElementById("x-score");
+var oScore = document.getElementById("o-score");
+var tieScore = document.getElementById("tie-score");
+var kitty = document.createElement("img");
+kitty.src = "./img/kitty.jpeg";
+
 console.log("boxes =", boxes);
-//constants needed
-// x Image
-// o Image
-// get
-// var x = 
 
-//document.getElementById("grid-box-1").textContent = "X";
-
-
-//player clicks on a box, an X is entered
-//the box can no longer be clicked until reset
-//player 2 clicks on a box, an O is entered
-//the box can no longer be clicked until reset
-//after 5th iteration, begin checking for a Win
-//if Win, execute youWon, else continue current game
-//if 9 iterations, execute youTied
-//reset game with click of Play Again button
-//update score for winner
-//reset score counter button
-//alternate which player starts
-//draw line through win?
-//create AI player
 
  function start() {
-    console.log("working!");
+    
+    console.log("working!", gameArr);
     document.getElementById("jeopardy-music").play();
     boxesOn();
-    
-    // console.log(document.getElementById("grid-box-1").value);
-    // console.log(xClickedArr.join(""));
-   
-    //getElementsByClassName("grid-box")[0].classList.remove();
+    timesClicked = 0;
+    gameArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        
+    boxes.forEach((box) => {
+        box.textContent = '';
+        console.log(box.textContent);
+    })
  }
-
-// //timer?
-// time = 0
-// document;
-
-// //reset boxes
-// document.getElementById("grid").childNode.remove();
-
-//Set the initial order of play
 function playerOrder() {
     if (timesClicked % 2 === 0) {
         messageBoard.textContent = "Player X, It's Your Turn";
-    } else
-    { messageBoard.textContent = "Player O, It's Your Turn"
+    } else {
+         messageBoard.textContent = "Player O, It's Your Turn"
     }
-    }
+    
+}
 
 
 function clickBox() {
+
     timesClicked += 1;
-    console.log(timesClicked);
+    //console.log(this.value);
     if (timesClicked % 2 !== 0) {
-        //insert image
+        //assign to X
         this.textContent = "X";
-        console.log("x inserted");
-       } else
-    { this.textContent = "O";
-    console.log("o inserted");
-}
-playerOrder();
+        updateArr("X");
+        //gameArr[this.value] = "X";
+        console.log(gameArr);
+    } else {
+        this.textContent = "O";
+        updateArr("O");
+        //gameArr[this.value] = "O";
+        console.log(gameArr);
+    }
+    playerOrder();
     //remove clicking option
     this.removeEventListener("click", clickBox);
-    
     //check if won
     if (timesClicked >= 5) {
-        winGame();
+        if (winGame()){
+            declareWinner(winGame());
+        } else if (timesClicked >= 9){
+            declareWinner(winGame());
+        }
     }
 }
+
 
 //make boxes clickable
 function boxesOn () {
-    
-var emptyBoxes = document.querySelectorAll("div.grid-box");
-
-for (i=0; i < emptyBoxes.length; i++) {
-    emptyBoxes[i].addEventListener("click", clickBox)
+    var onBoxes = document.querySelectorAll("div.grid-box");
+    for (i=0; i < onBoxes.length; i++) {
+    onBoxes[i].addEventListener("click", clickBox);
+    }
 }
+
+function boxesOff () {
+    var offBoxes = document.querySelectorAll("div.grid-box");
+    for (i=0; i < offBoxes.length; i++) {
+    offBoxes[i].removeEventListener("click", clickBox)
+    }
 }
 
 function winGame() {
-    if (boxes[0].getAttribute("textContent") === boxes[1].getAttribute("textContent") &&
-        boxes[1].getAttribute("textContent") === boxes[2].getAttribute("textContent")) {
+    console.log(gameArr);
+    if (gameArr[0] === gameArr[1] &&
+        gameArr[1] === gameArr[2]) {
+        console.log("true1");
             return true;
         } else
-        if (boxes[3].getAttribute("textContent") === boxes[4].getAttribute("textContent") &&
-        boxes[4].getAttribute("textContent") === boxes[5].getAttribute("textContent")) {
+        if (gameArr[3] === gameArr[4] &&
+        gameArr[4] === gameArr[5]) {
+            console.log("true2");
+        return true;
+        } else
+        if (gameArr[6] === gameArr[7] &&
+        gameArr[7] === gameArr[8]) {
+            console.log("true3");
             return true;
         } else
-        if (boxes[6].getAttribute("textContent") === boxes[7].getAttribute("textContent") &&
-        boxes[7].getAttribute("textContent") === boxes[8].getAttribute("textContent")) {
+        if (gameArr[0] === gameArr[3] &&
+        gameArr[3] === gameArr[6]) {
+            console.log("true4");
             return true;
         } else
-        if (boxes[0].getAttribute("textContent") === boxes[3].getAttribute("textContent") &&
-        boxes[3].getAttribute("textContent") === boxes[6].getAttribute("textContent")) {
+        if (gameArr[1] === gameArr[4] &&
+        gameArr[4] === gameArr[7]) {
+            console.log("true5");
             return true;
         } else
-        if (boxes[1].getAttribute("textContent") === boxes[4].getAttribute("textContent") &&
-        boxes[4].getAttribute("textContent") === boxes[7].getAttribute("textContent")) {
+        if (gameArr[2] === gameArr[5] &&
+        gameArr[5] === gameArr[8]) {
+            console.log("true6");
             return true;
         } else
-        if (boxes[2].getAttribute("textContent") === boxes[5].getAttribute("textContent") &&
-        boxes[5].getAttribute("textContent") === boxes[8].getAttribute("textContent")) {
+        if (gameArr[0] === gameArr[4] &&
+        gameArr[4] === gameArr[8]) {
+            console.log("true7");
             return true;
         } else
-        if (boxes[0].getAttribute("textContent") === boxes[4].getAttribute("textContent") &&
-        boxes[4].getAttribute("textContent") === boxes[8].getAttribute("textContent")) {
-            return true;
-        } else
-        if (boxes[2].getAttribute("textContent") === boxes[4].getAttribute("textContent") &&
-        boxes[4].getAttribute("textContent") === boxes[6].getAttribute("textContent")) {
+        if (gameArr[2] === gameArr[4] &&
+        gameArr[4] === gameArr[6]) {
+            console.log("true8");
             return true;
         } else
         return false;
+    }
+    
+
+    function endGame() {
+        // stop music
+        document.getElementById("jeopardy-music").pause();
+        // turn clicks off
+        boxesOff();
+        // change button text
+		document.getElementById("restart-button").textContent = "Play Again?";
+        //post winner
         
     }
-    console.log(winGame());
-
-    function endGame(resultMessage) {
-    //stop music
-        document.getElementById("jeopardy-music").pause();
-    //turn clicks off
-    var emptyBoxes = document.querySelectorAll("div.grid-box");
-    for (i=0; i < emptyBoxes.length; i++) {
-        emptyBoxes[i].removeEventListener("click", clickBox)
-    }
-    //change button text
-    document.getElementById("restart-button").textContent = "Play Again?";
-
+    function updateArr (str) {
+        for (i = 0; i < gameArr.length; i++ ) {
+            if (boxes[i].textContent === str) {
+                gameArr[i] = str;
+            }
+        }
     }
 
-//         } else
-// }
+    function declareWinner(winGame) {
+        if (winGame) {
+            if (timesClicked % 2 !== 0) {
+                console.log("X wins");
+                messageBoard.textContent = "Player X is the Winner!";
+                playerXScore += 1;
+                xScore.textContent = playerXScore;
+                endGame();
+            } else {
+                messageBoard.textContent = "Player O is the Winner!";
+                playerOScore += 1;
+                oScore.textContent = playerOScore;
+                endGame();
+            }
+        } else {
+            messageBoard.textContent = "Cat's Game...Click Below to Play Again";
+            tieScore.textContent = ++ties;
+            endGame();
+        }
+    }
 
-// function checkWin() {
-// 	if (positions[0].getAttribute("data-piece") == positions[1].getAttribute("data-piece") && positions[0].getAttribute("data-piece") == positions[2].getAttribute("data-piece")) {
-// 		return true;
-		
-//
-// function winGame() {
-//     for (i=0; i < winCombos.length; i++) {
-//         if (xClickedArr === 3 && xClickedArr === winCombos[i]) {
-//             return true;
-//         }
-//         else if (xClickedArr === 4) {
-//             (xClickedArr)
-//         }
-//         }
-//     }
-
-// }
+    function resetScores() {
+        playerXScore = 0;
+        playerOScore = 0;
+        ties = 0;
+        xScore.textContent = 0;
+        oScore.textContent = 0;
+        tieScore.textContent = 0;
+        timesClicked = 0;
+}
