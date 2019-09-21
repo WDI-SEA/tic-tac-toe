0607b2f18235efde53e0bc53e14953d3d260694a
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-09-21 14:29:08 -0700

    positions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,56 @@
-var player1;
-var player2;
 var currentPlayer = 1;
-var isChecked = false;
-container = document.getElementById("container").addEventListener("click",boxClicked);
-// .addEventListener("click",boxClicked);
+var winCombo = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 
-console.log(container);
-function boxClicked(e){
+]
+var xPosition = [false,false,false,false,false,false,false,false];
+var oPosition = [false,false,false,false,false,false,false,false];
 
-    if (e.target.innerHTML === "X"|| e.target.innerHTML === "O"){
-        container.removeEventListener("click", boxClicked);
-    }
-    takeTurn(e);
-        
+let box = document.querySelectorAll('.box');
+for (let i = 0; i < box.length; i++) {
+	box[i].addEventListener('click', boxClicked);
+	console.log(box[i]);
 }
-
-function takeTurn(e){
-        console.log('hello');
-        if(currentPlayer === 1){
-        console.log(currentPlayer);
-        e.target.innerHTML = "X";
-        currentPlayer++;
-        
-        }else if(currentPlayer !== 1){
-        e.target.innerHTML= "O";
-        currentPlayer--;
-        
-    }
+function boxClicked(e) {
+  if (e.target.innerHTML === "X" || e.target.innerHTML === "O") {
+    box.removeEventListener("click", boxClicked);
+  }
+  takeTurn(e);
+}
+function takeTurn(e) {
+  if (currentPlayer === 1) {
+    var x = e.target.innerHTML = "X";
+    // xPosition.push(x);
+    xPosition[e.target.id] = true;
+    checkForWin(e)
+    console.log(xPosition);
+    currentPlayer++;
+  } else if (currentPlayer !== 1) {
+    var o = e.target.innerHTML = "O";
+    // oPosition.push(o);
+    oPosition[e.target.id] = true;
+    checkForWin(e);
+    // console.log(oPosition);
+    console.log(oPosition);
+    currentPlayer--;
+  }
+}
+function checkForWin(e){
+  
+  for(let i = 0; i < winCombo.length; i++) {
     
+
+      for(let j= 0; j< winCombo.length[i]; j++){
+      
+    
+      }
+    }
+
 }
