45ea71a0f1ff2da9847f0d7100617bc39663a564
Brandon205 <brandonblack02@gmail.com>
2019-09-21 19:55:14 -0700

    THE WIN THING IS WORKING

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
 //Global Variables
-var moves;
-//DOM References
-var wholeBoard = document.querySelector(".whole-board");
-//Functions
-    //Check for win 
-    //moveCounter ++ then if % 2
-    //
+//Sorry
+// var sq1 = document.getElementById('0');
+// var sq2 = document.getElementById('1');
+// var sq3 = document.getElementById('2');
+// var sq4 = document.getElementById('3');
+// var sq5 = document.getElementById('4');
+// var sq6 = document.getElementById('5');
+// var sq7 = document.getElementById('6');
+// var sq8 = document.getElementById('7');
+// var sq9 = document.getElementById('8');
 
-
-
-//Event Handlers 
-    //The Clicks
-wholeBoard.addEventListener('click', function(e) {
-    console.log(e.target.id);
-})
-//Array of winning conditions 
+var moves = 0;
+var currentPlayer;
 var winCond = [
     [0, 1, 2],
     [3, 4, 5],
@@ -24,4 +21,55 @@ var winCond = [
     [1, 4, 7],
     [2, 4, 6],
     [2, 5, 8]
-]
+];
+//DOM References
+var wholeBoard = document.querySelector(".whole-board");
+var grid = Array.from(document.getElementsByClassName('gametile'));
+var tileId = grid[1].id;
+console.log(grid[0].textContent);
+var checkSame = function(arr) {
+    if (grid[arr[0]].textContent != " " && grid[arr[0]].textContent === grid[arr[1]].textContent && grid[arr[0]].textContent === grid[arr[2]].textContent) {
+        console.log('WIN');
+    } else {
+        console.log('nothing');
+    }
+}
+
+//Functions
+var pickWinner = function() {}
+
+//Check for win 
+var getWinner = function() {
+    for (let i = 0; i < winCond.length; i++) {
+        checkSame(winCond[i]);
+    }
+}
+
+//moveCounter ++ if % 2 === 0 
+var playerTurn = function() {
+    if (moves % 2 === 0) {
+        currentPlayer = "Player1";
+    } else {
+        currentPlayer = "Player2";
+    }
+    document.getElementById('display').textContent = currentPlayer;
+    moves++;
+    // console.log(moves);
+}
+
+//The Clicks
+wholeBoard.addEventListener('click', function(e) {
+    var currentSpot = e.target;
+    if (currentSpot.textContent == " ") {
+        playerTurn();
+        if (currentPlayer === "Player1") {
+            currentSpot.textContent = 'X';
+        } else {
+            currentSpot.textContent = 'O';
+        }
+    } else {
+        console.log("welp");
+    }
+    getWinner();
+})
+//Reset Function
