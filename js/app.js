fbf8dad48c878ccfee24f41658b9fb1ca3fe4a0f
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-11-18 08:45:06 -0800

    tictacnoo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,45 @@
-console.log('Hello frontend');
+var xoBoard;
+const playerOne = 'O';
+const playerTwo = 'X';
+const winCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [6,4,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+]
+
+const cells = document.querySelectorAll('.cell');
+startGame();
+
+function startGame() {
+    document.querySelector(".endgame").style.display = "none"
+    xoBoard = Array.from(Array(9).keys());
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
+
+function turnClick(square) {
+    turn(square.target.id, playerOne)
+}
+
+function turn(squareId, player) {
+    xoBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+}
+
+
+
+
+
+
+
+
+
+
+console.log('Tic Tac Toe');
