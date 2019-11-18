3b460f276b642431119b0f8aa374f04e51795e65
shannonzylstra <shannonzylstra@gmail.com>
2019-11-18 11:32:28 -0800

    changes to app

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log('Hello frontend');
+let user = "x"
+
+const addSquareListeners = () => {
+    // console.log('Add square listeners')
+    let squares = document.querySelectorAll('.square')
+    // Loop through each square
+    for (let i = 0; i < squares.length; i++) {        
+        // add event listener for click! 
+        squares[i].addEventListener('click',clickSquare)
+    }
+}
+
+const clickSquare = e => {
+    // change to the clicked square class
+    e.target.classList.add(user)
+    if (user === "x") { 
+        user = "o"
+    } else {
+        user = "x"
+    }
+}
+
+const start = () => {
+    console.log('started')
+    user = 'x'
+    //squares = document.querySelectorAll('.square')
+    addSquareListeners()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('content was loaded')
+    document.getElementById('play').addEventListener('click', start)
+})
