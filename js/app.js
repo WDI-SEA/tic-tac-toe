17e06c9e948b7d768e7345e714a041457c868490
shannonzylstra <shannonzylstra@gmail.com>
2019-11-18 07:51:52 -0800

    add js for button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log('Hello frontend');
+
+const buttonElement = document.getElementById('play');
+
+// Add a handler for the 'click' event by providing a callback function.
+// Whenever the button#play is clicked, a pop-up with "New game!" will appear.
+buttonElement.addEventListener('click', function (event) {
+    alert('New game!');
+});
