60f924ef0b4ce90659108810a4e29c2de18314c6
shannonzylstra <shannonzylstra@gmail.com>
2019-11-18 10:04:46 -0800

    js stuff

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
-console.log('Hello frontend');
+console.log('Hello frontend')
 
-const buttonElement = document.getElementById('play');
+const unclickedSquares = document.getElementsByClassName('square')
 
-// Add a handler for the 'click' event by providing a callback function.
-// Whenever the button#play is clicked, a pop-up with "New game!" will appear.
-buttonElement.addEventListener('click', function (event) {
+const clickSquare = e => {
+    // change to the clicked square class
+    e.target.
+}
+
+const clickSquare = (event) => {
+        console.log(event)
+        let targetSquare = event.target
+        targetSquare.classList.remove("unclicked")
+        targetSquare.classList.add("clicked")
+}
+for (let i = 0; i < unclickedSquares.length; i++) {
+    square = unclickedSquares[i]
+    square.classList.add("unclicked")
+    square.addEventListener('click', 
+        event.target.removeEventListener('click')
+        alert('Clicked!');
+    })
+}
+
+console.log(document.getElementsByClassName('unclicked'))
+
+
+/* 
+const playButton = document.getElementById('play')
+ */
+// Add a handler for the 'click' event
+/* 
+playButton.addEventListener('click', (event) => {
     alert('New game!');
-});
+})
+ */
---
+++ js/events.js
@@ -0,0 +1,5 @@
+// events.js
+
+eventHandlers = {
+    '#play': ['click',
+}
