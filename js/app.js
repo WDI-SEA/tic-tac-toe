dd38bca68c71f4b36e500c1fbf16dc82b2dddff9
kennansalisbury <kennansalisbury@gmail.com>
2019-11-18 06:51:39 -0800

    cleaned up code notes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 let startButton = document.getElementById('startButton')
 let playAgainButton = document.getElementById('playAgain')
 let startCompButton = document.getElementById('startCompButton')
+
 let squares = document.getElementsByClassName('square')
+
 let trackPlays = []
 let trackSquaresX = []
 let trackSquaresO = []
+
 let messageBoard = document.getElementById('messageBoard')
 let scoreBoard = document.getElementById('scoreBoard')
 let gamesPlayed = document.getElementById('gamesPlayed')
 let playerScoreX = document.getElementById('playerScoreX')
 let playerScoreO = document.getElementById('playerScoreO')
+
 let xCounter = 0
 let oCounter = 0
 let gameCounter = 0
+
 let allSquares = ["sq1", "sq2", "sq3", "sq4", "sq5", "sq6", "sq7", "sq8", "sq9"]
 
 //PLAY CODE FOR 2PL GAME
@@ -21,25 +26,26 @@ const playXO = e => {
     playAgainButton.style.display = "block"
     playAgainButton.addEventListener('click', nextGame)
 
-    //if first turn or last player played add x image when clicked, otherwise, add o
+    //if first turn or the last player played o when clicked, play X image and tell O it is their turn
     if (trackPlays.length === 0 || trackPlays[trackPlays.length-1] === "o") {
         e.target.classList.add('xImage')
+        
         trackPlays.push('x')
         trackSquaresX.push(e.target.id)
-        console.log(trackSquaresX)
-        messageBoard.textContent = "PIE-FACE, it's your turn"
+
+        messageBoard.textContent = "PIE, it's your turn"
     }
     else {
         e.target.classList.add('oImage')
+        
         trackPlays.push('o')
         trackSquaresO.push(e.target.id)
-        console.log(trackSquaresO)
+
         messageBoard.textContent = "TURKEY, it's your turn"
     }
     
     //remove event listener
     e.target.removeEventListener('click', playXO)
-    console.log(trackPlays)
 
     //check win condition
     if (checkWin(trackSquaresX)) {
@@ -55,8 +61,21 @@ const playXO = e => {
     }
 }
 
+
+//shared 2pl and computer code
 //check to see if 3-in-a-row combination present (2pl and comp)
 const checkWin = (arr) => {
+    //if sq1, sq2, sq3
+    //if sq4, sq5, sq6
+    //if sq7, sq8, sq9
+    //if sq1, sq4, sq7
+    //if sq2, sq5, sq8
+    //if sq3, sq6, sq9
+    //if sq1, sq5, sq9
+    //if sq3, sq5, sq7
+       //Win - return true
+    //else return false
+    
     switch(true) {
         case arr.includes('sq1') && arr.includes('sq2') && arr.includes('sq3'):
             return true
@@ -85,129 +104,140 @@ const checkWin = (arr) => {
         default:   
             return false
         }
-    //if sq1, sq2, sq3
-    //if sq4, sq5, sq6
-    //if sq7, sq8, sq9
-    //if sq1, sq4, sq7
-    //if sq2, sq5, sq8
-    //if sq3, sq6, sq9
-    //if sq1, sq5, sq9
-    //if sq3, sq5, sq7
-    
-    //if all squares filled and above not true
-    //return draw
-
-    //else return false
 }
 
-//win game scenario
+//win game scenario - winner message, remove all square event listeners and add 1 to game count
 const winGame = (xo, playXOorComp) => {
     messageBoard.textContent = `Player ${xo} wins!`
+
     removeSquareListeners(playXOorComp)
+
     gamesPlayed.textContent = gameCounter += 1
-    console.log(`${xo} wins!`)
 }
 
-//tie scenario (2pl and comp)
+//tie scenario (2pl and comp) - draw message, remove all square event listeners, and add 1 to game counts
 const drawGame = (playXOorComp) => {
     messageBoard.textContent = "DRAW"
+
     removeSquareListeners(playXOorComp)
+
     gamesPlayed.textContent = gameCounter += 1
-    console.log(`DRAW`)
+
 }
 
-//add and remove listen events on squares (2pl and comp)
+//add and remove event listeners on squares (2pl and comp)
+const addSquareListeners = (playXOorComp) => {
+    
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', playXOorComp)
+    }
+}
+
 const removeSquareListeners = (playXOorComp) => {
     
     for (let i = 0; i < squares.length; i++) {
         squares[i].removeEventListener('click', playXOorComp)
     }
 }
-const addSquareListeners = (playXOorComp) => {
-    
-    for (let i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', playXOorComp)
-        console.log(squares[i])
-    }
-}
 
-//reset to original screen with start over button
+//reset to original screen with start over button and clear all scores
 const startOver = () => {
+    //clear all scores
     trackPlays = []
     trackSquaresX = []
     trackSquaresO = []
     let xCounter = 0
     let oCounter = 0
     let gameCounter = 0
+
+    //show start button for 2Pl game & PL vs. comp again and add event listener
     startButton.style.display = "block"
     startButton.textContent = "2PL Game"
     startButton.addEventListener('click', startGame)
+
     startCompButton.style.display = "block"
     startCompButton.textContent = "PL vs. Comp"
     startCompButton.addEventListener('click', startCompGame)
+
+    //hide play again button and remove event listeners
     playAgainButton.style.display = "none"
     playAgainButton.removeEventListener('click', nextGame)
     playAgainButton.removeEventListener('click', nextGameComp)
+
+    //hide scoreboard
     scoreBoard.style.display = "none"
+
+    //reset squares - remove images and event listeners
     for (let i = 0; i < squares.length; i++) {
         squares[i].setAttribute('class', 'square')
     }
     removeSquareListeners(playXO)
     removeSquareListeners(playXOComp)
+
+    //welcome message
     messageBoard.textContent = "WELCOME"
     }
 
 //start game for 2PL
 const startGame = () => {
+    //reset trackers and counters
     trackPlays = []
     trackSquaresX = []
     trackSquaresO = []
     xCounter = 0
     oCounter = 0
     gameCounter = 0
+
+    //add event listeners to squares for the 2PL play function
     addSquareListeners(playXO)
+
+    //player 1 turn message
     messageBoard.textContent = "TURKEY, it's your turn"
+
+    //change start button to "start over", remove event listener to start game and add event listener to start game over when clicked
     startButton.textContent = "Start Over"
     startButton.removeEventListener('click', startGame)
     startButton.addEventListener('click', startOver)
+
+    //hide start 2pl vs. comp mode button
     startCompButton.style.display = "none"
+
+    //reveal the scoreboard and start at 0 for each player score and games played tracker
     playerScoreX.textContent = 0;
     playerScoreO.textContent = 0;
     gamesPlayed.textContent = 0;
     scoreBoard.style.display = "block"
 }
 
-//reset board but keep track of score for 2PL game
+//reset board but keep track of score for 2PL game - run with "play again" button
 const nextGame = () => {
+    //reset trackers
     trackPlays = []
     trackSquaresX = []
     trackSquaresO = []
     
+    //reset squares and add event listeners back for 2pl game
     for (let i = 0; i < squares.length; i++) {
         squares[i].setAttribute('class', 'square')
     }
     addSquareListeners(playXO)
-    messageBoard.textContent = "TURKEY, it's your turn"
-    startButton.textContent = "Start Over"
-    playAgainButton.style.display = "block"
 }
 
 //reset board but keep track of score for COMP game
 const nextGameComp = () => {
+    //reset trackers    
     trackPlays = []
     trackSquaresX = []
     trackSquaresO = []
-    
+ 
+    //reset squares and add event listeners back for 2pl game
     for (let i = 0; i < squares.length; i++) {
         squares[i].setAttribute('class', 'square')
     }
     addSquareListeners(playXOComp)
-    messageBoard.textContent = "TURKEY, it's your turn"
-    startButton.textContent = "Start Over"
-    playAgainButton.style.display = "block"
 }
 
-//pick random square that hasn't been picked for computer player
+//pick random square that hasn't already been picked for computer player
 const chooseRandomSq = () => {
     let i = Math.floor(Math.random() * 9)
     if (trackSquaresX.includes(allSquares[i]) || trackSquaresO.includes(allSquares[i])) {
@@ -218,7 +248,7 @@ const chooseRandomSq = () => {
     }
 }
 
-//check win for COMP
+//check win for player vs. computer mode
 const didSomeoneWinComp = () =>{
 
     if (checkWin(trackSquaresX)) {
@@ -232,6 +262,7 @@ const didSomeoneWinComp = () =>{
     else if (trackPlays.length === 9 && (checkWin(trackSquaresX) === false && checkWin(trackSquaresO) === false)) {
         drawGame(playXOComp)
     }
+    //added call for computer to play here, because otherwise the other function would be waiting on a click
     else if (trackPlays[trackPlays.length-1] === "x") {
         setTimeout(playCompO, 2000)
     }
@@ -242,52 +273,64 @@ const didSomeoneWinComp = () =>{
 
 //code for computer to play
 const playCompO = () => {
+        //runs random square function, finds that square and adds image to it
         let sqNum = chooseRandomSq()
         document.getElementById(sqNum).classList.add('oImage')
+        
         trackSquaresO.push(sqNum)
         trackPlays.push('o')
-        console.log(trackSquaresO)
+
         document.getElementById(sqNum).removeEventListener('click', playXOComp)
         didSomeoneWinComp()
 }
 
 
 const playXOComp = e => {
+    //reveal play again button and add event listener
     playAgainButton.style.display = "block"
     playAgainButton.addEventListener('click', nextGameComp)
 
-    //if first turn or last player played add x image when clicked, otherwise, add o
+    //if first turn or last player played "o", then it's the player's turn and their click should execute the x (turkey) image
     if (trackPlays.length === 0 || trackPlays[trackPlays.length-1] === "o") {
         e.target.classList.add('xImage')
         trackPlays.push('x')
         trackSquaresX.push(e.target.id)
-        console.log(trackSquaresX)
-        messageBoard.textContent = "Player Pie is thinking..."
+
+        messageBoard.textContent = "Player PIE is thinking..."
     }
     
     //remove event listener
     e.target.removeEventListener('click', playXOComp)
-    console.log(trackPlays)
 
-    
-    //check win condition
+    //check win condition, if no win or draw sets computer play in motion
     didSomeoneWinComp()
 }
 
 // start game for Computer game
 const startCompGame = () => {
+    //reset all trackers and counters
     trackPlays = []
     trackSquaresX = []
     trackSquaresO = []
     xCounter = 0
     oCounter = 0
     gameCounter = 0
+    
+    //add event listeners to trigger the computer mode play function
     addSquareListeners(playXOComp)
+
+    //Player 1 message
     messageBoard.textContent = "TURKEY, it's your turn"
+
+    //change start Pl vs. comp button to "start over", remove event listener to start game and add event listener to start game over when clicked
     startCompButton.textContent = "Start Over"
     startCompButton.removeEventListener('click', startCompGame)
     startCompButton.addEventListener('click', startOver)
+    
+    //hide start 2PL game button
     startButton.style.display = "none"
+
+    //reveal the scoreboard and start at 0 for each player score and games played tracker
     playerScoreX.textContent = 0;
     playerScoreO.textContent = 0;
     gamesPlayed.textContent = 0;
