2acd156ab98bfc6893173413c7651fd658694bb8
shannonzylstra <shannonzylstra@gmail.com>
2019-11-18 21:20:19 -0800

    add xo.js

---
+++ js/xo.js
@@ -0,0 +1,31 @@
+const clickedSquare = document.querySelector('div.square.clicked')
+
+const xo = {
+    x: "https://twemoji.maxcdn.com/2/svg/274c.svg",
+    o: "https://twemoji.maxcdn.com/2/svg/2b55.svg
+}
+
+const clickSquare = (xo) => {
+    switch (xo) {
+        case "x":
+            console.log("x")
+            break
+        case "o":
+            console.log("o")
+            break;
+        default:
+            console.log("unclicked")
+    }
+            
+
+        x" ? xo.x 
+    "
+}
+
+background-image: 
+url("https://twemoji.maxcdn.com/2/svg/274c.svg");
+}
+
+.square.o {
+    background-image: url("https://twemoji.maxcdn.com/2/svg/2b55.svg");
+}
