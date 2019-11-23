d992735055309efa30a58f9d7d3332a144f016af
shannonzylstra <shannonzylstra@gmail.com>
2019-11-22 16:21:31 -0800

    workinggit push origin test-js

--- js/app.js
+++ js/app.js
@@ -46,28 +46,9 @@ const clickSquare = (event) => {
     squares[marker].push(cell)
 
     if (squares[marker].length >= 3) {
+        checkWin(cell, marker)
         // check against win possibilities
-        // sort squares[marker]
-        squares[marker] = squares[marker].sort()
-        for (let i = 0; i < wins.length; i++) {
-            if (wins[i] == squares[marker]) {
-                alert("Game over! ${marker} wins")
-                break
-            }
-        }
     }
-    /*
-    if (cell < 4) {
-        squares[marker][0][cell] = 1
-    } else if (cell <7) {
-        squares[marker][1][cell] = 1
-    } else {
-        squares[marker][2][cell] = 1
-    }
-    if (checkWin(cell, marker)) {
-        alert('Game over!')
-    }
-    */
     
     if (marker === 'x') {
         marker = 'o'
@@ -79,40 +60,18 @@ const clickSquare = (event) => {
 
 addSquareListeners()
 
-const winningSquares = [
-    [0,1,2],
-
-]
 
 const checkWin = (cellNumber, marker) => {
-    
-    console.log(cellNumber)
-    console.log(`${squares[marker]}`)
-    var squaresToCheck = squares[marker]
-    var sum = 0
-    /*
-    // check rows
-    for (let i = 0; i < 3; i++) {
-        // check rows
-        for (let j = 0; j < 3; j++) {
-            sum += squaresToCheck[i][j]
-        }
-        if (sum === 3) {
-            return true
-        } else {
-            // check columns
-            sum = 0
-            for (let j = 0; j < 3; j++) {
-                sum += squaresToCheck[j][i]
-            }
-            if (sum === 3) {
-                return true
-            } else {
-                // check diagonals
-                return false
-            }
-        }
-        */
+    console.log(cellNumber) // 9
+    console.log(`${squares[marker]}`) // 1, 5, 9
+    squares[marker].sort()
+    for (let i = 0; i < wins.length; i++) {
+        if ((wins[i][0] == squares[marker][0]) && (wins[i][1] == squares[marker][1]) && (wins[i][2] == squares[marker][2])) {
+            console.log(`Game over! ${marker} wins`)
+            alert(`Game over! ${marker} wins`)
+            break
+        } 
+    }
 
 }
 
