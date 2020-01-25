a2d39fa6ce179ed7a4b6997a3fbb5f6dfc82fa01
chris-jburke <cjburke04@gmail.com>
2020-01-25 13:54:35 -0800

    board built

--- js/app.js
+++ js/app.js
@@ -1,1 +1,75 @@
-console.log('Hello frontend');
+let row0 = [
+	{
+		marked: false,
+		type: ""
+	},
+	{
+		marked: false,
+		type: ""
+	},
+	{
+		marked: false,
+		type: ""
+	}
+];
+let row1 = [
+	{
+		marked: false,
+		type: ""
+	},
+	{
+		marked: false,
+		type: ""
+	},
+	{
+		marked: false,
+		type: ""
+	}
+];
+let row2 = [
+	{
+		marked: false,
+		type: ""
+	},
+	{
+		marked: false,
+		type: ""
+	},
+	{
+		marked: false,
+		type: ""
+	}
+];
+var numberOfMoves = 0;
+let gameBoard = [row0,row1,row2];
+console.log(gameBoard);
+console.log(gameBoard[0][1].type);
+
+
+let checkBoxHandler = function(e){
+	let idString = e.target.id;
+	let stringSplit = idString.split("-")
+	let currRow = parseInt(stringSplit[0].slice(3), 10);
+	console.log(currRow);
+	let currColumn = parseInt(stringSplit[1].slice(3), 10);
+	console.log(currColumn);
+	console.log(gameBoard[currRow][currColumn].type);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+	document.querySelector(".row-space").addEventListener("click", checkBoxHandler)
+
+})
