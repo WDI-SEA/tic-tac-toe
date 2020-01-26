3331882f315971e3653eeec81c07695bca013c79
CCLancaster <connieclancaster@gmail.com>
2020-01-25 21:39:49 -0800

    created game-board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,60 @@
 console.log('Hello frontend');
+const SQUARES = [
+    {
+        box: "A",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "B",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "C",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "D",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "E",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "F",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "G",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "H",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    {
+        box: "I",
+        xImage: "tic-tac-toe/img/x.png",
+        oImage:"tic-tac-toe/img/o.png"
+    },
+    
+    ];
+
+
+// const createBoard = function() {
+// 	for (let i = 0; i < SQUARES.length; i++) {
+// 		let squareElement = document.createElement('img');
+// 		squareElement.setAttribute('src', 'images/back.png');
+// 		squareElement.setAttribute('data-id', i);
+// 		squareElement.addEventListener('click', flipCard);
+// 		document.getElementById('game-board').appendChild(squareElement);
+// 	}
+// };
