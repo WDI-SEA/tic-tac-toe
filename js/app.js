f36587f00dc1a41a71ab12291918ab7f06fe60a7
dlew253 <dlew253@yahoo.com>
2020-01-26 14:48:08 -0800

    color and styling set

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 console.log('Hello frontend');
+//create x and o. img/letter?
+//lock square when marker is plcaced
+//win message
+//loss message
+//draw
+//reset function---grey out background if possible
+//center reset button
+//winner show image?
+//loser show image?
