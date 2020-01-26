37175d913c48d6d93bb05879c49d1810486eb94a
CCLancaster <connieclancaster@gmail.com>
2020-01-25 22:20:22 -0800

    finalized CSS

--- js/app.js
+++ js/app.js
@@ -3,47 +3,47 @@ const SQUARES = [
     {
         box: "A",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "B",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "C",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "D",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "E",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "F",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "G",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "H",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     {
         box: "I",
         xImage: "tic-tac-toe/img/x.png",
-        oImage:"tic-tac-toe/img/o.png"
+        oImage: "tic-tac-toe/img/o.png"
     },
     
     ];
