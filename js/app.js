34d6e00094114318bfde13fda772a7fc45808b86
Melissa Young <melissay94@yahoo.com>
2020-01-25 21:37:47 -0800

    Logic updates board array to match DOM gameboard

--- js/app.js
+++ js/app.js
@@ -18,6 +18,8 @@ const PLAYER_TWO = {
     token: 'O'
 };
 
+const MAX_MOVES = 9;
+
 let gameboard;
 let gameboardSquares;
 let playerOneLabel;
@@ -25,8 +27,10 @@ let playerOneCount;
 let playerTwoLabel;
 let playerTwoCount;
 let resetButton;
+let endStatus;
 
 let currentPlayer;
+let numberOfMoves = 0;
 
 // Hi honey! I love you! <3
 const setUp = function() {
@@ -49,19 +53,37 @@ const getFirstPlayer = function() {
 }
 
 const resetGameBoard = function() {
-    gameboardSquares = document.querySelectorAll(".square");
+
     for (let i = 0; i < GAME_SQUARES.length; i++) {
         GAME_SQUARES[i] = [0, 0, 0];
     }
 
     for (let i = 0; i < gameboardSquares.length; i++) {
         gameboardSquares[i].innerText = " ";
+        gameboardSquares[i].setAttribute("data-col", i%3);
+        let rowValue;
+        if (i < 3) {
+            rowValue = 0;
+        } else if (i < 6) {
+            rowValue = 1;
+        } else {
+            rowValue = 2;
+        }
+        gameboardSquares[i].setAttribute("data-row", rowValue);
     }
 }
 
 const squareClickHandler = function(e) {
+
     e.target.innerText = currentPlayer.token;
+    let squareRow = e.target.getAttribute("data-row");
+    let squareCol = e.target.getAttribute("data-col");
+    GAME_SQUARES[squareRow][squareCol] = currentPlayer.token;
+
+    numberOfMoves++;
+
     switchActivePlayer();
+    checkBoard();
 
 }
 
@@ -80,17 +102,33 @@ const switchActivePlayer = function() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+const checkBoard = function() {
+    if (numberOfMoves === MAX_MOVES) {
+        endStatus.innerText = "Tie!";
+        console.log(GAME_SQUARES);
+    }
+}
+
+const getDOMElements = function() {
 
     gameboard = document.querySelector("#gameboard");
+    gameboardSquares = document.querySelectorAll(".square");
     playerOneLabel = document.querySelector("#player-one-label");
     playerOneCount = document.querySelector("#player-one-count");
     playerTwoLabel = document.querySelector("#player-two-label");
     playerTwoCount = document.querySelector("#player-two-count");
     resetButton = document.querySelector("#reset");
+    endStatus = document.querySelector(".end-status");
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    getDOMElements();
 
     gameboard.addEventListener("click", squareClickHandler);
     resetButton.addEventListener("click", setUp);
+    
     setUp();
 });
 
