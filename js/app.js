06bfd77778ee2b8428a767d5d80a7faa5bb6203b
chris-jburke <cjburke04@gmail.com>
2020-01-25 18:10:36 -0800

    base requirements finished

--- js/app.js
+++ js/app.js
@@ -40,21 +40,27 @@ let row2 = [
 		type: ""
 	}
 ];
+
 var numberOfMoves = 0;
 let gameBoard = [row0,row1,row2];
+let gameOver = false;
 
 let checkBoxHandler = function(e){
 	let idString = e.target.id;
 	let stringSplit = idString.split("-")
 	let currRow = parseInt(stringSplit[0].slice(3), 10);
 	let currColumn = parseInt(stringSplit[1].slice(3), 10);
-	if(!gameBoard[currRow][currColumn].marked)
+	if(!gameBoard[currRow][currColumn].marked && !gameOver)
 		if(numberOfMoves % 2 == 0){
 			gameBoard[currRow][currColumn].type = "X";
+			e.target.classList.remove("row-box");
+			e.target.classList.add("player-1");
 			e.target.innerText = "X";
 			gameBoard[currRow][currColumn].marked = true;
 			numberOfMoves++;
 		} else {
+			e.target.classList.remove("row-box");
+			e.target.classList.add("player-2");
 			gameBoard[currRow][currColumn].type = "O";
 			e.target.innerText = "O";
 			gameBoard[currRow][currColumn].marked = true;
@@ -62,7 +68,25 @@ let checkBoxHandler = function(e){
 		}
 
 	if(numberOfMoves >= 5){
-		checkWin();
+		let win = checkWin();
+		if(win == true){
+			console.log(numberOfMoves);
+			if(numberOfMoves % 2 == 0 && !gameOver){
+				setTimeout(function(){ alert("player-2 has won the game"); }, 300);
+			} else {
+				if(!gameOver){
+					setTimeout(function(){ alert("player-1 has won the game"); }, 300);
+				}
+			}
+			gameOver = true;
+		} else if(numberOfMoves == 9) {
+			gameOver = true;
+			setTimeout(function(){ alert("the game is a tie"); }, 300);
+
+		}
+	}
+	if (numberOfMoves > 0 && !gameOver){
+		nextTurnHandler();
 	}
 }
 
@@ -71,22 +95,27 @@ let checkWin = function(){
 	let rowWin = checkRowWin();
 	if(rowWin == true){
 		console.log("WINNER");
+		return true;
 	} else {
 		let columnWin = checkColumnWin();
 		if(columnWin == true){
 			console.log("WINNER");
+			return true;
 		} else {
 			let diagWinLeft = checkDiagonalWinLeftToRight();
 			if(diagWinLeft == true){
 				console.log("WINNER");
+				return true;
 			} else {
 				let diaWinRight = checkDiagonalWinRightToLeft();
 				if(diaWinRight == true){
 					console.log("WINNER");
+					return true;
 				}
 			}
 		}
 	}
+	return false;
 }
 
 let checkRowWin = function(){
@@ -183,7 +212,43 @@ let checkDiagonalWinRightToLeft = function (){
 
 }
 
+let resetHandler = function (){
+	console.log("resetButtonHandler");
+	numberOfMoves = 0;
+	gameOver = false;
+	for(let i = 0; i <= 2; i++){
+		for(let j = 0; j <=2; j++){
+			let selector = `#row${i}-box${j}`;
+			//console.log(selector);
+			gameBoard[i][j].type = "";
+			gameBoard[i][j].marked = false;
+			let domBox = document.querySelector(selector);
+			domBox.innerText = "";
+			domBox.classList.remove("player-1");
+			domBox.classList.remove("player-2");
+			domBox.classList.add("row-box");
 
+
+		}
+	}
+	nextTurnHandler();
+}
+let startHandler = function (){
+	if(numberOfMoves > 0){
+		alert("game already started");
+	} else {
+		alert("player-1's turn");
+	}
+}
+let nextTurnHandler = function(){
+	if(numberOfMoves % 2 == 0){
+		setTimeout(function(){ alert("player-1's turn"); }, 300);
+	} else {
+		setTimeout(function(){ alert("player-2's turn"); }, 300);
+		//setTimeout(alert("player-2's turn");
+	}
+
+}
 
 
 
@@ -191,6 +256,8 @@ let checkDiagonalWinRightToLeft = function (){
 
 
 document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector(".row-space").addEventListener("click", checkBoxHandler)
+	document.querySelector(".start-game").addEventListener("click", startHandler);
+	document.querySelector(".row-space").addEventListener("click", checkBoxHandler);
+	document.querySelector(".reset").addEventListener("click", resetHandler);
 
 })
