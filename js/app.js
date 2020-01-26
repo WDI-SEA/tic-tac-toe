a704c22b4dae297b91c3e301a83da59eb50387f7
deanjheikkila <deanjheikkila@gmail.com>
2020-01-25 23:58:48 -0800

    gimped around and started js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log('Hello frontend');
+const emptyImg = "../img/empty.png";
+const xImg = "../img/x.png";
+const oImg = "../img/o.png";
+let squares = [];
+
+let gameInit = function() {
+    //start each square as blank
+    squares = [];
+    for (let i = 0; i < 9; i++) {
+        squares.push({
+            state: "empty",
+            image: emptyImg,
+        });
+    }
+}
+
+gameInit();
+console.log(squares[8]);
