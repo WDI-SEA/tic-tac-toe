278845e7ebd21bdab07806c04c414ff078fd6e93
davidvelichko52 <davidvelichko52@gmail.com>
2020-01-26 22:08:30 -0800

    having a hard time

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 console.log('Hello frontend');
+
+const wcombos = [
+
+    [square1,square2,square3],
+    [square4,square5,square6],
+    [square7,square8,square9],
+    [square1,square5,square9],
+    [square3,square5,square7],
+    [square1,square4,square7],
+    [square2,square5,square8],
+    [square3,square6,square9],
+
+];
+
+let xO = {
+    x: {
+        xImg: 'img/x.png'
+    },
+    o: {
+        oImg: 'img/o.png'
+    }
+};
+
+
+
+
