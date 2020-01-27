53e6ab90db8c9454596325b4ddae1f36f5037aa0
dlew253 <dlew253@yahoo.com>
2020-01-27 11:15:30 -0800

    ugh

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
-console.log('Hello frontend');
-//create x and o. img/letter?
-//lock square when marker is plcaced
-//win message
-//loss message
-//draw
-//reset function---grey out background if possible
-//center reset button
-//winner show image?
-//loser show image?
+gameIcon1 'x'
+gameIcon2 'o'
+var gameState = {
+    gameIcon1: "x",
+    gameIcon2: "o",
+    squares: [
+    "","","","","","","","",""
+    ]
+  };
+
+//winning arrays
+const winCombo = [
+    [0,1,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8]
+    [2,4,6]
+];
