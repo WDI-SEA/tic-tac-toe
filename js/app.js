5c82827e6b20f1dec0bc6ece66dc937dad024423
John William Davis <jwdavisdeveloper@gmail.com>
2020-01-27 00:05:11 -0800

    worked on js, figured out first move, second move

--- js/app.js
+++ js/app.js
@@ -4,24 +4,28 @@
 let player1 = null;
 let player2 = null;
 
+let gameOn = true;
+
 let squaresWithX = [];
 let squaresWithO = [];
 
-let squares = document.querySelectorAll(".squares");
+// let squares = document.querySelectorAll(".squares");
 
 /*------------------------------Choose Player and Reset Variable ---------------------------------------*/
 
-var xButton = document.getElementById("x-button");
-var oButton = document.getElementById("o-button");
+// var xButton = document.getElementById("x-button");
+// var oButton = document.getElementById("o-button");
 var rButton = document.getElementById("r-button");
 
 /* ---------------------------- Variables for Messaging Players ---------------------------------------*/
 
 var message = document.getElementById("message-text");
-var initialMessage = message.innerText = '"Always Play To Win"';
+var initialMessage = message.innerText = '"Always Play To Win"\n"X" Goes First';
 var winMessageX = message.innerText = '"X" Wins!!!';
 var winMessageO = message.innerText = '"O" Wins!!!';
 var tieGameMessage = message.innerText = '"Cat\'s Game" You are tied!';
+var xTurn = message.innerText = 'Make Another Move "X"'; 
+var oTurn = message.innerText = 'Make A Move "O"';
 
 
 var allSquares = document.querySelectorAll(".squares");
@@ -30,8 +34,14 @@ var allSquares = document.querySelectorAll(".squares");
 console.log(allSquares);
 console.log(message);
 
-xButton.addEventListener('click', function(e) {
-    xButton.style.backgroundColor = "#b33939";
+// xButton.addEventListener('click', function(e) {
+//     xButton.style.backgroundColor = "#b33939";
+
+// });
+
+rButton.addEventListener('click', function(e) {
+  // rButton.style.backgroundColor = "#b33939";
+  message.innerText = initialMessage;
 
 });
 
@@ -46,9 +56,18 @@ xButton.addEventListener('click', function(e) {
 document.querySelectorAll('.squares').forEach( square => {
     square.addEventListener('click', function (e) {
         square.style.backgroundColor = "yellow";
+        if (message.innerText === initialMessage || message.innerText === xTurn) {
          square.firstChild.innerText = 'X';
+         message.innerText = oTurn;
+        } else {
+          square.firstChild.innerText = 'O';
+          message.innerText = xTurn;
+        }
+        
     })
   })
 
 console.log(player1);
+console.log(initialMessage)
+console.log(xTurn);
 
