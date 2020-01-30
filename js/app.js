8b592d7993d5954e5c33f700c4313fd106c92095
dannygoodrich <dannyngoodrich@gmail.com>
2020-01-30 12:20:11 -0800

    pull request

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let win;
 
 let mark = "X";
 
-let squares = Array.from(document.querySelectorAll('board'));
+let squares = Array.from(document.querySelectorAll('#board div'));
 
 const winner = [
     [0, 1, 2],
@@ -22,14 +22,14 @@ const winner = [
 ];
 
 
-board = ['X', '', '', '', '', '', '', '', ''];
+board = [];
 document.querySelector('.board').addEventListener('click', currentTurn);
 let gameInit = function() {
             
 
     board.forEach(function(mark, index) {
     console.log('ugh');
-    squares.findIndex.innerText = mark;
+    squares[index].innerText = mark;
     document.querySelector('.square').addEventListener('click', currentTurn);
 // ..........kjfnsdkjbvkjwb
 //for (let i = 0; i < 9; i++) {
@@ -46,13 +46,17 @@ let domResetBtn = document.querySelector('.reset');
 
 function currentTurn(e) {
     let index = squares.findIndex(function(square) {
-    return squares === e.target;
-    document.querySelector('.square').textContent = "X";
+    return square === e.target;
 });
 board[index] = turn;
 turn = turn === 'X' ? 'O' : 'X';
-console.log('hey');
+console.log(board);
+if (turn === 'X') {
+    document.querySelector('.square').src = "https://images.squarespace-cdn.com/content/v1/55ece940e4b048d1ed401c11/1450136257542-4DATU4KRB70MDENGJXJX/ke17ZwdGBToddI8pDm48kAf-OpKpNsh_OjjU8JOdDKBZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpwkCFOLgzJj4yIx-vIIEbyWWRd0QUGL6lY_wBICnBy59Ye9GKQq6_hlXZJyaybXpCc/X%3A++The+Unknown"
+}
+
 };
+document.querySelector('.square').textContent = e.target;
 
 function checkWin(){
     console.log("checking for win");
