1628ba778473cbb7e1dd081b9c5d2d46869d3697
tinnerrss <tinnerrss@hotmail.com>
2020-01-31 08:51:39 -0800

    resubmit tic tac toe

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let counter = 0;
 let messageBoard = document.querySelector(".message-board");
 let squares = document.querySelectorAll("img");
+let gameOver = false;
 const playerOneClicked = [];
 const playerTwoClicked =[];
 const winCombos = [
@@ -10,27 +11,36 @@ const winCombos = [
     [0, 4, 8], [2, 4, 6]
     ];
 
-// /* -------------Functions------------*/
-let theGame = function() {
-    squares.forEach(function(square, index) {
-        square.clicked = false;
-        square.addEventListener("click", function callback() {
-            turnTracker();
-            if (counter % 2 == 0) {
-                square.src = "https://cdn.pixabay.com/photo/2013/07/12/12/39/letter-146064_960_720.png";
-                playerTwoClicked.push(index);
-                console.log("P2 clicked: " + playerTwoClicked)
-                checkWin();
-                
-            } else {
-                square.src = "https://cdn.pixabay.com/photo/2013/07/12/12/39/letter-146055_960_720.png";
-                playerOneClicked.push(index);
-                console.log("P1 clicked: " + playerOneClicked)
-                checkWin();
-                
-            }
-            square.removeEventListener("click", callback);
-        })
+    
+    
+    
+    
+    // /* -------------Functions------------*/
+    //write out how game works
+    let theGame = function() {
+        //iterating through each square and giving it a number index
+        squares.forEach(function(square, index) {
+            square.clicked = false;
+            //on ce the button is clicked then start counting how much times it's been clicked
+                function callback(event) {
+                    turnTracker();
+                    if (counter % 2 == 0) {
+                        event.target.src = "https://cdn.pixabay.com/photo/2013/07/12/12/39/letter-146064_960_720.png";
+                        playerTwoClicked.push(index);
+                        // console.log("P2 clicked: " + playerTwoClicked)
+                        checkWin();
+                        
+                        
+                    } else {
+                        event.target.src = "https://cdn.pixabay.com/photo/2013/07/12/12/39/letter-146055_960_720.png";
+                        playerOneClicked.push(index);
+                        // console.log("P1 clicked: " + playerOneClicked)
+                        checkWin();
+                    
+                    }
+                    event.target.removeEventListener("click", callback);
+                }
+            square.addEventListener("click", callback)
     })
 }
 let turnTracker = function() {
@@ -47,16 +57,29 @@ let turnTracker = function() {
 function reset() {
     location.reload()
 }
+//trying to add removeEventlistener but can't seem to figure it out
+// function removeListener() {
+//     for (var i = 1; i < 10; i ++)
+//     document.getElementById(`s${i}`).removeEventListener("click");
+// }
 function checkWin() {
     winCombos.forEach(function(array) {
         if (array.every(element => playerOneClicked.includes(element))) {
             // lost();
             messageBoard.innerHTML = "Yasssss Player One!"; 
+            gameOver = true;
+            // removeListener();
+            alert("Player One Wins!");
+            reset();
+            
             
         } else if (array.every(element => playerTwoClicked.includes(element))) {
             // lost();
             messageBoard.innerHTML = "Yasssss Player Two!";
-            
+            gameOver = true;
+            alert("Player Two Wins!");
+            reset();
+            // removeListener();
         } else if (
             counter >= 9) {
                 messageBoard.innerHTML = "Ittsssaaa TIE!";
