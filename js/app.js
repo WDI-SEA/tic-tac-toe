76a8452565e8f211763ce41984710307ab5492d1
gjones81 <jones.guy81@gmail.com>
2020-03-22 15:41:59 -0700

    updating

--- js/app.js
+++ js/app.js
@@ -14,19 +14,65 @@ let gameSquare9 = document.getElementById("gameSquare9")
 let squares = [gameSquare1, gameSquare2, gameSquare3, 
 	gameSquare4, gameSquare5, gameSquare6, gameSquare7, gameSquare8, gameSquare9]
 
+// Message window shows whos turn it is
+let messageWindow = document.getElementById('messageWindow')
+
 let startButton = document.getElementById('startButton')
+//changes the clicked square
+
+
+const messageToPlayers = () => {
+	if (turnNumber % 2 === 0) {
+		document.getElementById('messageWindow').textContent = "It's Player 2's turn"
+	}
+	else {
+		document.getElementById('messageWindow').textContent = "It's Player 1's turn"
+	}
+	console.log('messageToPlayers')
+}
+
+const changeSquare = () => {	
+
+	//set color attribute and background color
+	if (turnNumber % 2 === 0) {
+		document.getElementById(id).setAttribute('backgroundColor', '#b32d00')
+		//e.target.style.background-color = red
+	}
+	else {
+		document.getElementById(id).setAttribute('backgroundColor', '#0052cc')
+		//e.target.style.background-color = blue
+	}
+	//remove event listener
+	document.getElementById(id).removeEventListener('click', changeSquare)
+	
+}
 
 //advances the turn count when called
 const advanceTurn = () => {
 	turnNumber +=1
 	console.log(turnNumber)
+	messageToPlayers()
 }
-//Sets eventListeners on each game square
-const setGame = () => {
+//Sets eventlisteners
+const setEventListeners = () => {
 	for (let i = 0; i < squares.length; i++) {
-	squares[i].addEventListener("click", advanceTurn)
+		squares[i].addEventListener("click", (e) => {
+		squares[i].setAttribute('clicked', true)
+		//advanceTurn()
+		//changeSquare()
+		console.log(squares[i])
+		})
+	}
+}
+//Sets the starting condition on each game square
+const setGame = (e) => {
+	for (let j = 0; j < squares.length; j++) {
+	squares[j].setAttribute('clicked', false)
+	squares[j].setAttribute('claimed', false)
+	//console.log(squares[j])
+	setEventListeners()	
 	}	
 }
-	
+
 //Set eventlistener of the start button
 startButton.addEventListener('click', setGame)
