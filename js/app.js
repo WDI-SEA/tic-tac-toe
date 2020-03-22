30f694f7a48b42f69d53329d85719fd5f862e499
ahonore42 <a1honore80@gmail.com>
2020-03-22 05:00:09 -0700

    added structure with HTML and CSS, started JS process

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello frontend');
+console.log('Let\'s play a game!');
+//What are we doing here?
+//Making a tic tac toe game with artificial intelligence
+//Images need to be grabbed on clicks inside the tick tac toe squares
+//They need to be different to represent 2 players
+//The game needs to read that a box has been clicked and record the image there
+//no boxes can be clicked twice during a game
+//win condition is three images in a row
+//it has to be 3 in a row for 3 of the same type
+//can be diagonal, vertical, or horizontal
+//display win after win condition
+//gameboard should have 3 rows and 3 columns
+//A win condition is an entire row, column, or diagonal has been filled
+//make the robot cry and lose his car on win condition
