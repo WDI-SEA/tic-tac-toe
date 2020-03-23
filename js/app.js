d55c2ad81e8ea7cf84ace1b18a6725658a116c58
davidvelichko52 <davidvelichko52@gmail.com>
2020-03-23 01:28:20 -0700

    changed the way to check for winner

--- js/app.js
+++ js/app.js
@@ -1,81 +1,143 @@
+var xTurn = true;
+var turn = 0;
+
+document.getElementById("s_1").addEventListener("click", playerTurn);
+document.getElementById("s_2").addEventListener("click", playerTurn);
+document.getElementById("s_3").addEventListener("click", playerTurn);
+document.getElementById("s_4").addEventListener("click", playerTurn);
+document.getElementById("s_5").addEventListener("click", playerTurn);
+document.getElementById("s_6").addEventListener("click", playerTurn);
+document.getElementById("s_7").addEventListener("click", playerTurn);
+document.getElementById("s_8").addEventListener("click", playerTurn);
+document.getElementById("s_9").addEventListener("click", playerTurn);
+var box1 = document.getElementById("s_1")
+var box2 = document.getElementById("s_2")
+var box3 = document.getElementById("s_3")
+var box4 = document.getElementById("s_4")
+var box5 = document.getElementById("s_5")
+var box6 = document.getElementById("s_6")
+var box7 = document.getElementById("s_7")
+var box8 = document.getElementById("s_8")
+var box9 = document.getElementById("s_9")
+
+var winner = document.getElementById("whoW");
+
+   
 
 
-// ------------------------------------------------------Youtube helped me alot!!!!!!!! 1BestCSharpBlog---------------------------------------------------------
-var box1= document.getElementById("s_1"),
-box2= document.getElementById("s_2"),
-box3= document.getElementById("s_3"),
-box4= document.getElementById("s_4"),
-box5= document.getElementById("s_5"),
-box6= document.getElementById("s_6"),
-box7= document.getElementById("s_7"),
-box8= document.getElementById("s_8"),
-box9= document.getElementById("s_9");
+function playerTurn () {
+  if (xTurn === true){
+      this.innerHTML = "X";
+      xTurn = false;
+    } else {
+      this.innerHTML = "O";
+      xTurn = true;
+    }
+    turn = turn + 1;
+    winCom();
+};
 
 
-
-function getWinner(){
-
-    if (box1.innerHTML != "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML){
-        console.log("win");
-        document.getElementById("whoW").innerHTML = "We Got A Winner!"}
-            
+function winCom (){
+    if (box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    } else if (box4.textContent === "X" && box5.textContent === "X" && box6.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    } else if (box7.textContent === "X" && box8.textContent === "X" && box9.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    }else if (box1.textContent === "X" && box4.textContent === "X" && box7.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    }else if (box2.textContent === "X" && box5.textContent === "X" && box8.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    }else if (box3.textContent === "X" && box6.textContent === "X" && box9.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    }else if (box1.textContent === "X" && box5.textContent === "X" && box9.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    }else if (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X"){
+    winner.textContent = "X Won";
+    winCom = true;
+    }else if (box1.textContent === "O" && box2.textContent === "O" && box3.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box4.textContent === "O" && box5.textContent === "O" && box6.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box7.textContent === "O" && box8.textContent === "O" && box9.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box1.textContent === "O" && box4.textContent === "O" && box7.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box2.textContent === "O" && box5.textContent === "O" && box8.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box3.textContent === "O" && box6.textContent === "O" && box9.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box1.textContent === "O" && box5.textContent === "O" && box9.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    }else if (box3.textContent === "O" && box5.textContent === "O" && box7.textContent === "O"){
+    winner.textContent = "O Won";
+    winCom = true;
+    } 
+    if (turn === 9 && winCom != true) {
+        winner.textContent = "No Winner!!"
+    }
     
-         if (box4.innerHTML != "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner !"}
-            
+    removeClick()
     
-        if (box7.innerHTML != "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner !"}
-            
-         
-        if (box1.innerHTML != "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner!"}
-            
-         
-        if (box2.innerHTML != "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner!"}
-            
-         
-         if (box3.innerHTML != "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner!"}
-            
-         
-         if (box1.innerHTML != "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner!"}
-            
-         
-         if (box3.innerHTML != "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML){
-            console.log("win");
-            document.getElementById("whoW").innerHTML = "We Got A Winner!"}
-            
+};
 
+function removeClick(){
+
+  if (box1 === "X" || "O" ){
+    document.getElementById("s_1").removeEventListener("click", winCom)
+  }
+
+  else if (box2 === "X" || "O" ){
+  document.getElementById("s_2").removeEventListener("click", winCom)
+  }
+
+  else if (box3 === "X" || "O" ){
+  document.getElementById("s_3").removeEventListener("click", winCom)
+  }
+
+  else if (box4 === "X" || "O" ){
+  document.getElementById("s_4").removeEventListener("click", winCom)
+  }
+
+  else if (box5 === "X" || "O" ){
+  document.getElementById("s_5").removeEventListener("click", winCom)
+  }
+
+  else if (box6 === "X" || "O" ){
+  document.getElementById("s_6").removeEventListener("click", winCom)
+  }
+
+  else if (box7 === "X" || "O" ){
+  document.getElementById("s_7").removeEventListener("click", playerTurn)
+  }
+
+  else if (box8 === "X" || "O" ){
+  document.getElementById("s_8").removeEventListener("click", playerTurn)
+  }
+
+  else if (box9 === "X" || "O" ){
+  document.getElementById("s_9").removeEventListener("click", playerTurn)
+  }
 }
 
 
-var boxes = document.querySelectorAll("#main div"), X_or_O = 0;
 
-for(var i = 0; i < boxes.length; i++){
-    boxes[i].onclick = function () {
-
-        if(this.innerHTML !== "X" && this.innerHTML !== "O" ) {
-        if(X_or_O%2 === 0) {
-            console.log(X_or_O);
-            this.innerHTML = "X";
-            getWinner();
-            X_or_O += 1;
-
-        }else{
-            console.log(X_or_O);
-            this.innerHTML = "O";
-            getWinner();
-            X_or_O += 1;
-        }
-      }
-    };
-}
+function resetGame(){
+    location.reload();
+};
+document.getElementById("reset").addEventListener("click", resetGame);
