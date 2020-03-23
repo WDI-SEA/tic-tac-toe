b4fdab666505b8cfb1a0af8a03ee737d78276de4
Chuck Wagon <chuckwagon@Chucks-MacBook-Air.local>
2020-03-23 08:49:58 -0700

    Tic Tac Toe unfinished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('Hello frontend');
+$(document).ready(function() {
+
+const winCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,7],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+
+let player1 = "X";
+let player2 = "O";
+
+let currentTurn = 1;
+let movesMade = 0;
+
+let sqr = $(".square");
+console.log(sqr);
+});
