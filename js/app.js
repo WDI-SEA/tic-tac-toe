0a8e1279da0c617c98f4bc0cf9c9a2c66ae914aa
Chuck Wagon <chuckwagon@Chucks-MacBook-Air.local>
2020-03-23 10:40:22 -0700

    unfinished tic tac

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
-console.log('Hello frontend');
-$(document).ready(function() {
+// console.log('Hello frontend');
 
-const winCombos = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,7],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-]
+// const winCombos = [
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,7],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [6,4,2]
+// ]
 
-let player1 = "X";
-let player2 = "O";
+// let player1 = "X";
+// let player2 = "O";
 
-let currentTurn = 1;
-let movesMade = 0;
+// let currentTurn = 1;
+// let movesMade = 0;
 
-let sqr = $(".square");
-console.log(sqr);
-});
+// let sqr = $(".square");
+// console.log(sqr);
+// });
