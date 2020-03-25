c075ca06a2744ad398017c4035276c6535f8086d
gjones81 <jones.guy81@gmail.com>
2020-03-24 19:46:46 -0700

    updating

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 
 let turnNumber = 0
-//The 9 game squares by Id, numbered in order from top left to bottom right
 let gameSquare1 = document.getElementById("gameSquare1")
 let gameSquare2 = document.getElementById("gameSquare2")
 let gameSquare3 = document.getElementById("gameSquare3")
@@ -10,15 +9,26 @@ let gameSquare6 = document.getElementById("gameSquare6")
 let gameSquare7 = document.getElementById("gameSquare7")
 let gameSquare8 = document.getElementById("gameSquare8")
 let gameSquare9 = document.getElementById("gameSquare9")
-// An array of all game squares
+
 let squares = [gameSquare1, gameSquare2, gameSquare3, 
 	gameSquare4, gameSquare5, gameSquare6, gameSquare7, gameSquare8, gameSquare9]
 
-// Message window shows whos turn it is
+let redSquares = []
+let blueSquares = []
+
+const winningCondition = [
+	{'div#gameSquare1.col', 'div#gameSquare2.col' 'div#gameSquare3.col'}, {'div#gameSquare4.col', 'div#gameSquare5.col', 'div#gameSquare6.col'},
+
+	{'div#gameSquare7.col', 'div#gameSquare8.col', 'div#gameSquare9.col'}, {'div#gameSquare1.col', 'div#gameSquare4.col', 'div#gameSquare7.col'},
+
+	{'div#gameSquare2.col', 'div#gameSquare5.col', 'div#gameSquare8.col'}, {'div#gameSquare3.col', 'div#gameSquare6.col', 'div#gameSquare9.col'},
+
+	{'div#gameSquare1.col', 'div#gameSquare5.col', 'div#gameSquare9.col'}, {'div#gameSquare3.col', 'div#gameSquare5.col', 'div#gameSquare7.col'}
+]
+
 let messageWindow = document.getElementById('messageWindow')
 
 let startButton = document.getElementById('startButton')
-//changes the clicked square
 
 
 const messageToPlayers = () => {
@@ -31,19 +41,21 @@ const messageToPlayers = () => {
 }
 
 const changeSquare = (e) => {
-	e.target.setAttribute('clicked', 'true')
 	e.target.removeEventListener('click', changeSquare)
 	
 	if (turnNumber % 2 === 0) {
 		e.target.style.backgroundImage = "url('https://img.icons8.com/ios-filled/50/000000/o.png')"
 		e.target.style.backgroundColor = 'blue'
-		e.target.setAttribute('claimed', 'blue')
-		
+		let blue = e.target
+		blueSquares.push(blue)
+		console.log(blueSquares)
 	}
 	else {
 		e.target.style.backgroundImage = "url('https://img.icons8.com/ios-filled/50/000000/x.png')"
 		e.target.style.backgroundColor = 'firebrick'
-		e.target.setAttribute('claimed', 'red')
+		let red = e.target
+		redSquares.push(red)
+		console.log(redSquares)
 		
 	}
 advanceTurn()
@@ -51,29 +63,32 @@ checkForWin()
 }
 
 const checkForWin = () => {
-	console.log('checkForWin called')
-		
+	for (let i = 0; i < blueSquares.length; i++) {
+		if (blueSquares == winningCondition[i]){
+			console.log('Blue WINS!!')
+		}
+		else {
+			console.log('Blue Doesnt Win')
+		}
+	}
+	if (let j = 0; j < redSquares.length; j++){
+		if (redSquares == winningCondition[j]){
+			console.log('Red WINS!!')
+		} else {
+			console.log('Red Doesnt Win')
+		}
+	}
 }
 
-//advances the turn count when called
 const advanceTurn = () => {
 	turnNumber +=1
 	messageToPlayers()
 }
-//Sets eventlisteners
+
 const setEventListeners = (e) => {
 	for (let i = 0; i < squares.length; i++) {
 		squares[i].addEventListener('click', changeSquare)
 	}
 }
-//Sets the starting condition on each game square
-const setGame = (e) => {
-	for (let j = 0; j < squares.length; j++) {
-	squares[j].setAttribute('clicked', 'false')
-	squares[j].setAttribute('claimed', 'false')
-	}
-	setEventListeners()
-}	
-
-//Set eventlistener of the start butto
-startButton.addEventListener('click', setGame)
+	
+startButton.addEventListener('click', setEventListeners)
