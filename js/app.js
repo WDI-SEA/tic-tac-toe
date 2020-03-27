ec0bc9b96a041235d510311670ff5d639713b424
smcleod86 <mcleod.scott_86@yahoo.com>
2020-03-27 13:45:16 -0700

    Round 2 of tic-tac

--- js/app.js
+++ js/app.js
@@ -1,117 +1,105 @@
+						//Variables
+let player1 = 'X'
+let player2 = 'O'
 
-//We tried to trouble shoot why I am unable to click on an empty 
-//cell to mark a move. I need to re-address my functions. I tried
-//to google my way out of problems over the weekend, but wasn't able
-//to find much that didn't use JQuery or other materials which we 
-//haven't covered yet in the course. I got some good ideas from classmates
-//as to how better to structure my fuctions and code so I can implement
-//the methods we have learned.
+let board = document.getElementById('gameBoard')
+let turn = 0
+let winner = false
+
+console.log('Started')
+						//Start the Game
+const startGame = () => {
+	document.getElementById('prompt').textContent = 'Go Player 1'
+	player1 = true
+  	player2 = false
+		for (let i = 0; i < board.length; i++) {
+			document.getElementById('box[i]').addEventListener('click', (e) => {
+			move(i)
+			})
+		}
+	document.getElementById('startButton').addEventListener('click', reset)
+console.log('Now Im here')
+}
+
+						//Draw Condition
+const draw = () => {
+	if (turn == 9 && winner == false ) {
+		document.getElementById('prompt').textContent = 'Tie Game'
+	}
+}
+
+						//Player moves
+const move = (cell) => {
+	if (board[cell].classList.contains('marked') = false) {
+		if (player1 == true) {
+      player1 = false
+      player2 = true
+			p1Move(cell)
+			document.getElementById('prompt').textContent = "Player 1 turn"
+		} else {
+			player1 = true
+			player2 = false
+			p2Move(cell)
+			document.getElementById('prompt').textContent= "Player 2 turn"
+		}
+		checkWin()
+		turn = turn + 1
+		draw()
+		let marked = board[cell]
+		marked.classList.add('marked')
+	}
+}
+
+const p1Move = (cell) => {
+	let open = document.getElementById('grid')[cell]
+	open.classList.add("p1mark")
+}
+const p2Move = (cell) => {
+	let open = document.getElementById('grid')[cell]
+	open.classList.add("p2Mark")
+}
+
+						//Win conditions
+const condition = [
+	[0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 
-const player1 = "X"
-const player2 = "O"
+//.....
+//.....
 
-let turn = 1
-let turn = true
-let play_board = ["", "", "", "", "", "", "", "", ""]
 
-const board_container = document.getElementsById("gameBoard")
 
-check_board_complete = () => {
-    let flag = true;
-    play_board.forEach(element => {
-      if (element != player1 && element != player2) {
-        flag = false
-      }
-    })
-    board_full = flag
-  }
-  
-  const checkWin = (a, b, c) => {
-    return (
-      play_board[a] == play_board[b] &&
-      play_board[b] == play_board[c] &&
-      (play_board[a] == player1 || play_board[a] == player2)
-    )
-  }
-  
-  const checkWin = () => {
-    for (i = 0; i < 9; i += 3) {
-      if (check_line(i, i + 1, i + 2)) {
-        return play_board[i]
-      }
-    }
-    for (i = 0; i < 3; i++) {
-      if (checkWin(i, i + 3, i + 6)) {
-        return play_board[i]
-      }
-    }
-    if (checkWin(0, 4, 8)) {
-      return play_board[0]
-    }
-    if (checkWin(2, 4, 6)) {
-      return play_board[2]
-    }
-    return ""
-  }
-  
-  const check_for_winner = () => {
-    let res = check_match()
-    if (res == player1) {
-      winner.innerText = "Winner is Player 1!"
-      winner.classList.add("player1Win")
-      board_full = true
-    } else if (res == player2) {
-      winner.innerText = "Winner is Player 2!"
-      winner.classList.add("player2Win")
-      board_full = true
-    } else if (board_full) {
-      winner.innerText = "Draw!"
-      winner.classList.add("draw")
-    }
-  }
-  
-  const render_board = () => {
-    board_container.innerHTML = ""
-    play_board.forEach((e, i) => {
-      if (e == player1 || e == player2) {
-        document.getElementById(`#col-4`).classList.add("occupied")
-      }
-    })
-  }
-  
-  const game_loop = () => {
-    render_board()
-    check_board_complete()
-    check_for_winner()
-  }
-  
-  const addPlayer1Move = (e) => {
-    if (!board_full && play_board[e] == "") {
-      play_board[e] = player1
-      game_loop()
-      addPlayer2Move()
-    }
-  }
 
- const addPlayer2Move = (e) => {
-    if (!board_full && play_board[e] == "") {
-      play_board[e] = player2
-     e.target.getElementsById("col-4").addEventListener("click", player2)
-        game_loop()
-        addPlayer1Move()
-    }
-  }
-  
+						//Reset the board
+const reset = () => {
+	for (i = 0; i < board.length; i++) {
+		let space =  board[i];
+		if (space.classList.contains('p1mark')) {
+			space.classList.remove('p1mark')
+			}
+		else if (space.classList.contains('p2mark')) {
+			space.classList.remove('p2mark')
+	startGame()
+		}
+	}
+}
 
-  const reset_board = () => {
-    play_board = ["", "", "", "", "", "", "", "", ""]
-    board_full = false
-    winner.classList.remove("player1Win")
-    winner.classList.remove("player2Win")
-    winner.classList.remove("draw")
-    winner.innerText = ""
-    render_board()
-  }
-  
-  render_board() 
+						//Remove marks
+const checkTiles = () => {
+	for (i = 0; i < board.length; i++) {
+		let space =  board[i];
+		if (space.classList.contains('marked') = false) {
+			space.classList.add('marked')
+		}
+	}		
+}	
+
+document.getElementById('startButton').addEventListener('click', startGame)
