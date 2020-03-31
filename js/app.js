5a74002f275fefc6164f90b3975a82d8e1610c2f
smcleod86 <mcleod.scott_86@yahoo.com>
2020-03-31 12:36:27 -0700

    Final: Totaly functional, no AI

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
 						//Variables
+let board = document.querySelectorAll('.col-4')
+let currentPlayer;
 let player1 = true
 let player2 = false
-
-let board = document.querySelectorAll('.col-4')
 let turn = 0
 let winner = false
-
-console.log('Started')
 
 						//Start the Game
 const startGame = () => {
@@ -16,9 +14,7 @@ const startGame = () => {
 		for (let i = 0; i < board.length; i++) {
 			board[i].addEventListener('click', move)
 		}
-		console.log(board.length)
 	document.getElementById('startButton').addEventListener('click', reset)
-console.log('Now Im here')
 }
 
 						//Draw Condition
@@ -30,30 +26,36 @@ const draw = () => {
 
 						//Player moves
 const move = (e) => {
-	console.log('clicked', e.target.id)
-		if (player1 == true) {
-     		player1 = false
-      		player2 = true
-			p1Move(e)
-			document.getElementById('prompt').textContent = 'Player 2 turn'
-		} else {
-			player1 = true
-			player2 = false
-			p2Move(e)
-			document.getElementById('prompt').textContent = 'Player 1 turn'
-		}
-		turn = turn + 1
-		winCon(board. condition)
-		draw()
+	if (player1 == true) {
+		player1 = false
+		player2 = true
+		p1Move(e)
+		document.getElementById('prompt').textContent = 'Player 2 turn'
+	} else {
+		player1 = true
+		player2 = false
+		p2Move(e)
+		document.getElementById('prompt').textContent = 'Player 1 turn'
 	}
+	turn = turn + 1
+	winCon(board, condition)
+	if (winner) {
+		for (let i = 0; i < board.length; i++) {
+			board[i].removeEventListener('click', move)
+		}
+	}
+	draw()
+}
 
 const p1Move = (e) => {
+	e.target.value = "x"
 	e.target.textContent = 'X'
 	e.target.style.textContent = 'center'
 	e.target.removeEventListener('click', move)
 }
 
 const p2Move = (e) => {
+	e.target.value = "o"
 	e.target.textContent = 'O'
 	e.target.style.textContent = 'center'
 	e.target.removeEventListener('click', move)
@@ -71,27 +73,28 @@ const condition = [
     [2, 4, 6]
 ]
 
-
-const winCon = (board, condtion) => {
-	if (turn >= 3) {
+const winCon = (board, condition) => {
+	console.log(winner)
+	 if (turn >= 3) {
 		for (let i = 0; i < condition.length; i++) {
-			const [a, b, c] = condtion[i];
-			if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-				console.log('winner', move)
-				document.getElementById('prompt').textContent = 'Congratulations, You win!'
-				document.removeEventListener('click', board)
-	} else {	
-		draw()
-	}
+			const [x, y, z] = condition[i]
+			if (board[x].value && board[x].value === board[y].value && board[x].value === board[z].value) {
+				document.getElementById('prompt').textContent = `${board[x].value} is the winner!`
+				winner = true
+			}
 		}
 	}
-}
-
+ }
 
 						//Reset the board
 const reset = () => {
+	for (let i = 0; i < board.length; i++) {
+		board[i].textContent = ''
+		board[i].value = i
+	}
+	winner = false
 	startGame()
-	board.textContent = ''
-		}
+}
 
+document.getElementById('restartButton').addEventListener('click', reset)
 document.getElementById('startButton').addEventListener('click', startGame)
