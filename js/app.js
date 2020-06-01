59b42502a28ee8bd0aee273112422c699f376b45
karlmiche <karlymism@gmail.com>
2020-05-31 18:15:41 -0700

    Karly Smith Tic Tac Toe

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
-console.log("This is Randy speaking :-)");
+// console.log("This is Randy speaking :-)");
 
 //Initialize variables; cache the DOM
 var pScore = document.getElementById("player-score");
 var rScore = document.getElementById("computer-score");
 var message = document.getElementById("message");
 var display = document.getElementById("display");
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+
 const resetBtn = document.getElementById("resetBtn");
+
 const square = document.getElementsByClassName("square");
+
+let turn = 0;
 
 //at the beginning, the game isn't over
 //set this value to false
@@ -26,28 +30,38 @@ let gameOver = false;
 //null signifies this value can change
 let player = {
   points: 0,
-  currentChoice: null
+  currentChoice: null,
+  allChoices: []
 }
 
 //object to set Randy's initial points at 0 for beginning of game
 //object to set the first choice to "null", no one has chosen
 let randy = {
     points: 0,
-    currentChoice: null
+    currentChoice: null,
+    allChoices: []
   }
 
 //array for boxes to be filled
-let boxes = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+let boxesToFill = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
 //an array of the arrays of the winning combinations
 //I learned this method from Michela
 const winCombos = [
-    ["one", "five", "nine"], ["one", "two", "three"], ["three", "six", "nine"], ["two", "five", "eight"], ["one", "four", "seven"], ["four", "five", "six"], ["three", "five", "seven"], ["seven", "eight", "nine"]
+    ["one", "five", "nine"], 
+    ["one", "two", "three"], 
+    ["three", "six", "nine"], 
+    ["two", "five", "eight"], 
+    ["one", "four", "seven"], 
+    ["four", "five", "six"], 
+    ["three", "five", "seven"], 
+    ["seven", "eight", "nine"]
 ];
 
 
-//Functions
-//first function to check if it is player turn or computer (Randy) turn
+/*************************Functions**********************/
+
+// function to check if it is player turn or computer (Randy) turn
 //this function relies on Boolean logic to track whose turn it is
 //I learned this function from Lemon 🍋
 function getTurn(){
@@ -60,63 +74,90 @@ function getTurn(){
 
 //function for the computer to "choose" randomly
 //this is why he's named Randy :-)
+//assigning random choice to a random value in boxesToFill
+//once we have this value, we want to take this out of boxesToFill
+//similar logic needs to apply to playerChooses
 function getRandomChoice(){
-  const randomChoice = Math.floor(Math.random() * boxes.length);
-  return choices[randomChoice];
+    //Math.floor rounds it down so it's not a float
+    //numeric value that serves as the index we access boxesToFill from
+  const randomIndex = Math.floor(Math.random() * boxesToFill.length);
+  const randomChoice = boxesToFill[randomIndex];
+    //splice one value from whatever randomIndex turns out to be 
+    snipIt(boxesToFill, randomChoice);
+  return randomChoice;
 }
 
-//isSelected function
-//I learned this from Michela, Emily, and Zachary
-function isSelected(e) {
-    console.dir("You chose " + e)
-    let square = e.target;
-
-    if (playerChooses) {
-       square.classList.add("cat");
-        display.innerText = "Player is on top of their game! Randy is deliberating..."
-    }else if (randyChooses) {
-        square.classList.add("frog");
-    }    
+//remove listeners
+//need to use this in playerChooses and randyChooses to remove 
+//remove elements that he picks
+function removeListener(elementId){
+    const dog = document.getElementById(elementId)
+    dog.removeEventListener("click", playerChooses);
 }
+
+//snipIt - splice logic so that once a id/index has been used, 
+//cannot be used again by either player or Randy
+//boxes will be an array, elementId is a string, "one", "two", ex
+//const index is going to be the index the string lives at
+function snipIt(boxes, elementId){
+    //this is what we need to know what to splice
+const index = boxes.indexOf(elementId)
+if (index > -1) {
+    boxes.splice(index, 1);
+    if (boxes.length < 1) {
+      gameOver = true;
+    //   console.log("game over!! :(");
+      display.innerText = "No one has won! Player and Randy are at a draw."
+    }
+  }
+}
+
 
 //function for player choice
 function playerChooses(event){
-    karly.currentChoice = event.target.value;
+    //need to get id value before passing it in
+    console.log("player", gameOver);
+    player.currentChoice = event.target.id;
+    //push choices to allChoices
+    player.allChoices.push(player.currentChoice)
+    document.getElementById(player.currentChoice).classList.add("cat");
+    snipIt(boxesToFill, player.currentChoice);
+    removeListener(player.currentChoice);
     turn++;
-    //disable the square chosen from being able to be clicked
-    //change background image of div to a cat emoji
+    checkWin();
     updateScoreboard();
-    console.log("turn", turn, "player choice", event.target.value);
+    // console.log("turn", turn, "player choices", player.allChoices);
+    if(boxesToFill.length > 1 && gameOver === false){
     randyChooses();
+    }
 }
 
 //function for computer choice
 function randyChooses(){    
-
+console.log("randy", gameOver);
+   
     //randy's choice is a random choice 
+    //since we do splicelogic in getRandomChoice, don't be redundant
     randy.currentChoice = getRandomChoice();
-    console.log("randy's choice", randy.currentChoice);
-    square.classList.add(".frog");
+    randy.allChoices.push(randy.currentChoice)
+    turn++;
+    // console.log(randy.currentChoice, "randy choices", randy.allChoices);
+    //returns the id of the box he "chose"
+    //we can use document.getElementById and pass in this id 
+    //document.getElementById(randy.currentChoice).classList.add("frog");
+    //removeListener(randy.currentChoice);
+    display.innerText = "Randy is deliberating..."
 
-        //remove randy's choice from the array
-        let boxIndex = boxes.indexOf(".square");
-      // If the box has an index, it needs to be filled!
-      if (boxIndex > -1) {
-        boxesToFill.splice(boxIndex, 1);
-        if (boxesToFill.length < 1) {
-          endGame(true);
-        }
-      } else {
-        endGame(false);
-      }
-
-
-    //make it seem like he is thinking with a timeOut delay
+    checkWin(); 
+    removeCurrentListeners(boxesToFill);
     setTimeout(function(){
-        checkWin();
-        turn++;
-            //remove the ability to click the square "chosen"
-    })
+        document.getElementById(randy.currentChoice).classList.add("frog");
+        currentListeners(boxesToFill);
+        removeListener(randy.currentChoice);
+        display.innerText = "Player, make your move!"
+    }, 2000);
+
+    // console.log("randy's choice", randy.currentChoice, event.target.value);
     updateScoreboard();
 }
 
@@ -128,106 +169,109 @@ function updateScoreboard() {
 
 
 //function to reset the game
-function resetGame(){}
+resetBtn.addEventListener("click", resetGame);
 
-//function to start the game
-function startGame(){}
+function resetGame(){
+    boxesToFill = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    gameOver = false;
+    // console.log("Clicked play game", boxesToFill);
+    boxesToFill.forEach(function(element){
+        //callback function that accepts one parameter
+        const peach = document.getElementById(element);
+        peach.classList.remove("frog");
+        peach.classList.remove("cat");
+    })
+    addAllEventListeners();
+}
 
-/*********Event Listeners***********/
+/********************Event Listeners***********************/
 
 //An event listener to change background image to a cat
 //This is when playerChooses
-one.addEventListener("click", isSelected);
-two.addEventListener("click", isSelected);
-three.addEventListener("click", isSelected);
-four.addEventListener("click", isSelected);
-five.addEventListener("click", isSelected);
-six.addEventListener("click", isSelected);
-seven.addEventListener("click", isSelected);
-eight.addEventListener("click", isSelected);
-nine.addEventListener("click", isSelected);
 
-//Randy's choice cannot be an event listener I don't think
+function addAllEventListeners(){
+one.addEventListener("click", playerChooses);
+two.addEventListener("click", playerChooses);
+three.addEventListener("click", playerChooses);
+four.addEventListener("click", playerChooses);
+five.addEventListener("click", playerChooses);
+six.addEventListener("click", playerChooses);
+seven.addEventListener("click", playerChooses);
+eight.addEventListener("click", playerChooses);
+nine.addEventListener("click", playerChooses);
+}
 
-/*****Game Logic******/
+addAllEventListeners();
 
+function currentListeners(listeners){
+    listeners.forEach(function(element){
+        //callback function that accepts one parameter
+        const peach = document.getElementById(element);
+        peach.addEventListener("click", playerChooses)
+    })
+}
+
+function removeCurrentListeners(listeners){
+    listeners.forEach(function(element){
+        //callback function that accepts one parameter
+        const peach = document.getElementById(element);
+        peach.removeEventListener("click", playerChooses)
+    })
+}
 /****Winning Conditions****/
 
 function checkWin(){
+    // console.log("it is finally fucking working");
 //Conditions if player has won
-if (playerChoices = "one", "five", "nine"){
-    console.log("Player wins!");
-    display.innerText = "Humans are ultimately smarter than computers!"
-    player.points++;
-} if (playerChoices = "one", "two", "three"){
-    console.log("Player wins!");
-    display.innerText = "*Player is taking a victory lap!*"
-    player.points++;
-} if (playerChoices = "three", "six", "nine"){
-    console.log("Player wins!");
-    display.innerText = "Player is impeccable and skillful!"
-    player.points++;
-} if (playerChoices = "two", "five", "eight"){
-    console.log("Player wins!");
-    display.innerText = "5, 6, 7, 8, who do we appreciate?! Player!!"
-    player.points++;
-} if (playerChoices = "one", "four", "seven"){
-    console.log("Player wins!");
-    display.innerText = "*Player takes a bow*"
-    player.points++;
-} if(playerChoices = "four", "five", "six"){
-    console.log("Player wins!");
-    display.innerText = "Player is the future president of the world!"
-    player.points++;
-} if (playerChoices = "three", "five", "seven"){
-    console.log("Player wins!");
-    display.innerText = "Player apparently knows things Randy doesn't!"
-    player.points++;
-} if (playerChoices = "seven", "eight", "nine"){
-    console.log("Player wins!");
-    display.innerText = "Player is victorious!"
-    player.points++;
-}
 
-//Conditions if computer has won (eight total)
-  if (randyChoices = "one", "five", "nine"){
-    console.log("Randy wins!");
-    display.innerText = "Randy is the winner. He will dominate the Earth!"
-    randy.points++;
-}  if (randyChoices = "one", "two", "three"){
-    console.log("Randy wins!");
-    display.innerText = "Randy is the most skillful computer on this planet! Fear him!"
-    randy.points++;
-}  if (randyChoices = "three", "six", "nine"){
-    console.log("Randy wins!");
-    display.innerText = "Randy is fearsome in all his Earthly activities!"
-    randy.points++;
-} if (randyChoices = "two", "five", "eight"){
-    console.log("Randy wins!");
-    display.innerText = "Randy requests your presence when he accepts his award!"
-    randy.points++;
-} if (randyChoices = "one", "four", "seven"){
-    console.log("Randy wins!");
-    display.innerText = "*Randy takes a bow!*"
-    randy.points++;
-} if(randyChoices = "four", "five", "six"){
-    console.log("Randy wins!");
-    display.innerText = "Randy eats human failure for a healthy snack!"
-    randy.points++;
-} if (randyChoices = "three", "five", "seven"){
-    console.log("Randy wins!");
-    display.innerText = "Randy still wants to be friends, although he has defeated you!"
-    randy.points++;
-} if (randyChoices = "seven", "eight", "nine"){
-    console.log("Randy wins!");
-    display.innerText = "Watch out, humanity! Randy wins!"
-    randy.points++;
+
+    for (let i = 0; i < winCombos.length; i++){
+        let playerMatch = 0; 
+        let randyMatch = 0;
+        for (let b = 0; b < winCombos[i].length; b++){
+            if (player.allChoices.includes(winCombos[i][b])){
+                playerMatch++;
+                // console.log(playerMatch);
+            }
+            if (playerMatch === 3){
+                // console.log("WIN MOTHERFUCKER ASKJASKJASJK");
+                display.innerText = "Player wins! Humanity is going to be ok."
+                removeCurrentListeners(boxesToFill);
+                gameOver = true;
+                player = {
+                   currentChoice: null,
+                   allChoices: [],
+                   points: player.points + 1
+                }
+                randy = {
+                    currentChoice: null,
+                    allChoices: [],
+                    points: randy.points
+                }
+                
+            }   if (randy.allChoices.includes(winCombos[i][b])){
+                randyMatch++;
+                // console.log(randyMatch);
+        }   if (randyMatch === 3){
+                // console.log("Randy wins and reigns eternal");
+                display.innerText = "Randy wins! He eats human failure for a healthy snack!"
+                removeCurrentListeners(boxesToFill);
+                gameOver = true;
+                randy = {
+                    currentChoice: null,
+                    allChoices: [],
+                    points: randy.points + 1
+                 }
+
+                player = {
+                    currentChoice: null, 
+                    allChoices: [],
+                    points: player.points 
+                }
+            } 
+        }
+    } 
+}
 
 resetGame();
-
-    //Draw conditions
-} else {
-        display.innerText = "Better luck next time - it was a draw!"
-    }
-    updateScoreboard();
-}
+updateScoreboard();
