b56687eeb35f29be9c4e1976d9409785000cc63e
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-19 13:36:32 -0700

    master: Pseudocode

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+// Get the cells
+
+// Add event listeners to each of them
+
+// Add click event handler that
+//// A. marks the cell
+//// B. checks the score
+//// C. Switch players
+
+
+// Decision Algorithm 
+// (New user's turn) =>  "Circle"
+// (shapeDecider = New user's turn ? o : x) => "shapeDecider = o"
+// add classes
+
+// Winning Algorithm
