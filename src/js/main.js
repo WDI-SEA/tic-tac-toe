38474a26977d7b8fd60c888cf95bc645625e3ac1
PatrickDePuydt <prdepuydt@gmail.com>
2020-12-19 14:07:00 -0700

    master: Develop win function pseudocode

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,18 @@
 // Add event listeners to each of them
 
 // Add click event handler that
-//// A. marks the cell
-//// B. checks the score
-//// C. Switch players
+  //// A. marks the cell
+  //// B. checks the score
+  //// C. Switch players
 
 
 // Decision Algorithm 
-// (New user's turn) =>  "Circle"
-// (shapeDecider = New user's turn ? o : x) => "shapeDecider = o"
-// add classes
+  // (current user's turn) =>  "Circle"
+  // (shapeDecider = current user's turn ? o : x) => "shapeDecider = o"
+  // add classes
 
 // Winning Algorithm
+// Whitelist: Construct all the a winning patterns in an array to check against
+
+// Read current user layout selection
+// Check current users layout selection against the winning profile pattern array
