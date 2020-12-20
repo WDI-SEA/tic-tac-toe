36fa03376c811eeb184fc06ded7e687715b7347e
alitannir <alitannir1@gmail.com>
2020-12-20 15:05:27 -0800

    session 5

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
-document.addEventListener("DOMContentLoaded", (e) => {
 // page loads
 // coordinate elements to variables
-const cells = document.querySelector(".table")
+    const cells = document.querySelectorAll(".cell")
+    const cellArray = Array.from(cells)
 
 // whose turn it is use a if else with === and a for each for the cells clicked
-const playerDisplay = document.querySelector('.player')
+    const playerDisplay = document.querySelector('.player')
 
-let currentPlayer = "PlayerX"
+    let currentPlayer = "PlayerX"
+    let table = document.querySelector(".table")
 
-cells.forEach(cell => {
-    cell.addEventListener("click", clickOutcome)
-})
 
-function clickOutcome(e) {
- const cellArray = Array.from(table)
- const indicator = cellArray.indexOf(e.target)
- playerDisplay.innerHTML = currentPlayer
+    cells.forEach(cell => {
+        cell.addEventListener("click", e=> {clickOutcome(e)})
+    })
+    function clickOutcome(e) {
+        
+    
+        const indicator = cellArray.indexOf(e.target)
+        playerDisplay.innerHTML = currentPlayer
 
- if(currentPlayer === "PlayerX") {
-     cells[indicator].classList.add("PlayerX")
-     currentPlayer = "PlayerO"
- } else {
-     cells[indicator].classList.add("PlayerO")
-     currentPlayer = "PlayerX"
- }
-}
+        if(currentPlayer === "PlayerX") {
+            cells[indicator].classList.add("PlayerX")
+            currentPlayer = "PlayerO"
+        } else {
+            cells[indicator].classList.add("PlayerO")
+            currentPlayer = "PlayerX"
+        }
+    }
