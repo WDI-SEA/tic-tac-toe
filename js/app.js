7b1cbddb0544c60879ee45a8cab54a05f5b431e6
austintip <austin.d.tipograph@gmail.com>
2020-12-20 15:37:46 -0500

    TicTacToe almost

--- js/app.js
+++ js/app.js
@@ -7,30 +7,86 @@ let boxes = document.getElementsByClassName('chart');
 // shows the result
 let result = document.getElementById('winner');
 
+let gameBoard = document.getElementsByClassName('chart');
+
+let playerTurn = "X"
+let winningCombinations = [
+    ["1", "2", "3"], 
+    ["4", "5", "6"],
+    ["7", "8", "9"],
+    ["1", "4", "7"],
+    ["2", "5", "8"],
+    ["3", "6", "9"],
+    ["1", "5", "9"],
+    ["3", "5", "7"],
+];
+
+let playerXTurn = []
+let playerOTurn = []
 // each player
-let X = 'X';
-let O = 'O';
-let player = 0;
+
 
 // if x's turn return X
 // something with player=0; player < 9 ; i++ or something....
 // want it to select X's turn if the number is even and O's turn if it's odd. 
-function playerTurn () {
-    if (player % 2 === 0) {
-        return X;
-        //if O's turn return O 
-    } else {
-        return O;
-    }
-};
+// function playerTurn () {
+//     if (player % 2 === 0) {
+//         return X;
+//         //if O's turn return O 
+//     } else {
+//         return O;
+//     }
+// };
 
-for(let player = 0; player < 10; player++) {
-    playerTurn
-};
+// for(let player = 0; player < 10; player++) {
+//     playerTurn
+// };
 
 /* have an event listener that on click checks
  which turn and adds a number to player */
 
  //this doesn't work...  it says there is a missing ) after the argument list??
-boxes.addEventListener('click', (playerTurn) {
-});
+for (let i=0; i< gameBoard.length; i++) {
+    gameBoard[i].addEventListener('click', (e) => {
+        console.log(e.target); 
+        if (playerTurn == "X") {
+            e.target.innerText = "X";
+            playerXTurn.push(e.target.id);
+            console.log(playerXTurn);
+            playerTurn = "O";
+        } else {
+            e.target.innerText = "O";
+            playerOTurn.push(e.target.id);
+            console.log(playerOTurn);
+            playerTurn = "X";
+        }
+        });
+};
+
+const checkWin = () => {
+    if (playerTurn == "X") {
+        for (let i=0; i < winningCombinations.length; i++) {
+            let match = 0;
+            console.log(winningCombinations.i)
+            for (let j=0; j < winningCombinations[i].length; j++) {
+                if (playerXTurn.includes(winningCombinations[i][j])) {
+                    match++;
+                }
+            }
+        }
+    } else {
+        for (let i=0; i < winningCombinations.length; i++) {
+        let match = 0;
+        console.log(winningCombinations.i)
+        for (let j=0; j < winningCombinations[i].length; j++) {
+            if (playerXTurn.includes(winningCombinations[i][j])) {
+                match++;
+                }
+            }
+        } 
+    }
+}
+
+
+// e.target.id is a string not an int - winningcomb is full of ints. 
+// if match = 3 end game
