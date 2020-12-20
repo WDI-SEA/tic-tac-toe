5e452959027afb272fa9aa661dde518de6c12e79
alitannir <alitannir1@gmail.com>
2020-12-19 22:35:35 -0800

    session 4

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log('Hello frontend');
+document.addEventListener("DOMContentLoaded", (e) => {
+// page loads
+// coordinate elements to variables
+const cells = document.querySelector(".table")
+
+// whose turn it is use a if else with === and a for each for the cells clicked
+const playerDisplay = document.querySelector('.player')
+
+let currentPlayer = "PlayerX"
+
+cells.forEach(cell => {
+    cell.addEventListener("click", clickOutcome)
+})
+
+function clickOutcome(e) {
+ const cellArray = Array.from(table)
+ const indicator = cellArray.indexOf(e.target)
+ playerDisplay.innerHTML = currentPlayer
+
+ if(currentPlayer === "PlayerX") {
+     cells[indicator].classList.add("PlayerX")
+     currentPlayer = "PlayerO"
+ } else {
+     cells[indicator].classList.add("PlayerO")
+     currentPlayer = "PlayerX"
+ }
+}
