bccc33b7b06cb3e6a749e1df263ff08bde64da3b
alitannir <alitannir1@gmail.com>
2020-12-20 17:01:56 -0800

    session7

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@
     const cells = document.querySelectorAll(".cell")
     const cellArray = Array.from(cells)
 
+    const restartButton = document.querySelector(".restartButton");
+
+    restartButton.addEventListener("click",() => {
+        cellArray.forEach((cells, index => {
+            cells[index] = null;
+        }))
+    })
+    
+
 // whose turn it is use a if else with === and a for each for the cells clicked
     const playerDisplay = document.querySelector('.player')
 
@@ -26,4 +35,9 @@
             cells[indicator].classList.add("PlayerO")
             currentPlayer = "PlayerX"
         }
+    
     }
+
+    const statusDisplay = document.querySelector("winningMessage")
+    letgameActive = true;
+    let gameState = ["", "", "", "", "", "", "", "", ""]
