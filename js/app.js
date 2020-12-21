b1d29dbed983d4d72a0e13d343c9f03a433818d7
huiylu <74116599+huiylu@users.noreply.github.com>
2020-12-20 23:14:05 -0800

    Working prototype

--- js/app.js
+++ js/app.js
@@ -1,1 +1,119 @@
 console.log('Hello frontend');
+
+var ticMark="O"
+var winCon=false;
+var winner="";
+//document.getElementById("grid1").innerHTML= "OK";
+//placeO();
+
+
+
+/*
+function placeO(grid){
+    if(ticMark==="X"){
+        if(document.getElementById("grid1").innerText===""){
+            document.getElementById("grid1").innerText = ticMark;
+            ticMark="O";
+        } else if(document.getElementById("grid2").innerText===""){
+            document.getElementById("grid2").innerText = ticMark;
+            ticMark="O";
+        } else if(document.getElementById("grid3").innerText===""){
+            document.getElementById("grid3").innerText = ticMark;
+            ticMark="O";
+        }else if(document.getElementById("grid4").innerText===""){
+            document.getElementById("grid4").innerText = ticMark;
+            ticMark="O";
+        }else if(document.getElementById("grid5").innerText===""){
+            document.getElementById("grid5").innerText = ticMark;
+            ticMark="O";
+        }else if(document.getElementById("grid6").innerText===""){
+            document.getElementById("grid6").innerText = ticMark;
+            ticMark="O";
+        }else if(document.getElementById("grid7").innerText===""){
+            document.getElementById("grid7").innerText = ticMark;
+            ticMark="O";
+        }else if(document.getElementById("grid8").innerText===""){
+            document.getElementById("grid8").innerText = ticMark;
+            ticMark="O";
+        }else if(document.getElementById("grid9").innerText===""){
+            document.getElementById("grid9").innerText = ticMark;
+            ticMark="O";
+        }
+    }else if(ticMark==="O"){
+        if(document.getElementById("grid1").innerText===""){
+            document.getElementById("grid1").innerText = ticMark;
+            ticMark="X";
+        } else if(document.getElementById("grid2").innerText===""){
+            document.getElementById("grid2").innerText = ticMark;
+            ticMark="X";
+        } else if(document.getElementById("grid3").innerText===""){
+            document.getElementById("grid3").innerText = ticMark;
+            ticMark="X";
+        }else if(document.getElementById("grid4").innerText===""){
+            document.getElementById("grid4").innerText = ticMark;
+            ticMark="X";
+        }else if(document.getElementById("grid5").innerText===""){
+            document.getElementById("grid5").innerText = ticMark;
+            ticMark="X";
+        }else if(document.getElementById("grid6").innerText===""){
+            document.getElementById("grid6").innerText = ticMark;
+            ticMark="X";
+        }else if(document.getElementById("grid7").innerText===""){
+            document.getElementById("grid7").innerText = ticMark;
+            ticMark="X";
+        }else if(document.getElementById("grid8").innerText===""){
+            document.getElementById("grid8").innerText = ticMark;
+            ticMark="X";
+        }else if(document.getElementById("grid9").innerText===""){
+            document.getElementById("grid9").innerText = ticMark;
+            ticMark="X";
+        }
+    }
+}
+*/
+var grid;
+function placeO(grid){
+    if(!winCon){
+        if(ticMark==="X"){
+            if(document.getElementById(grid).innerText===""){
+                document.getElementById(grid).innerText = ticMark;
+                ticMark="O";
+            }
+        }else if(ticMark==="O"){
+            if(document.getElementById(grid).innerText===""){
+                document.getElementById(grid).innerText = ticMark;
+                ticMark="X";
+            }
+        }
+        checkWin();
+    }
+    
+}
+
+function checkWin(){
+    if((document.getElementById("grid1").innerText==="O"&&document.getElementById("grid2").innerText==="O"&&document.getElementById("grid3").innerText==="O")
+        ||(document.getElementById("grid4").innerText==="O"&&document.getElementById("grid5").innerText==="O"&&document.getElementById("grid6").innerText==="O")
+        ||(document.getElementById("grid7").innerText==="O"&&document.getElementById("grid8").innerText==="O"&&document.getElementById("grid9").innerText==="O")
+        ||(document.getElementById("grid1").innerText==="O"&&document.getElementById("grid4").innerText==="O"&&document.getElementById("grid7").innerText==="O")
+        ||(document.getElementById("grid3").innerText==="O"&&document.getElementById("grid6").innerText==="O"&&document.getElementById("grid9").innerText==="O")
+        ||(document.getElementById("grid2").innerText==="O"&&document.getElementById("grid5").innerText==="O"&&document.getElementById("grid8").innerText==="O")
+        ||(document.getElementById("grid1").innerText==="O"&&document.getElementById("grid5").innerText==="O"&&document.getElementById("grid9").innerText==="O")
+        ||(document.getElementById("grid3").innerText==="O"&&document.getElementById("grid5").innerText==="O"&&document.getElementById("grid7").innerText==="O")
+        ){
+            winCon=true;
+            winner="O";
+            document.getElementById("winner").innerText=winner +" is the Winner";
+        }else if((document.getElementById("grid1").innerText==="X"&&document.getElementById("grid2").innerText==="X"&&document.getElementById("grid3").innerText==="X")
+        ||(document.getElementById("grid4").innerText==="X"&&document.getElementById("grid5").innerText==="X"&&document.getElementById("grid6").innerText==="X")
+        ||(document.getElementById("grid7").innerText==="X"&&document.getElementById("grid8").innerText==="X"&&document.getElementById("grid9").innerText==="X")
+        ||(document.getElementById("grid1").innerText==="X"&&document.getElementById("grid4").innerText==="X"&&document.getElementById("grid7").innerText==="X")
+        ||(document.getElementById("grid3").innerText==="X"&&document.getElementById("grid6").innerText==="X"&&document.getElementById("grid9").innerText==="X")
+        ||(document.getElementById("grid2").innerText==="X"&&document.getElementById("grid5").innerText==="X"&&document.getElementById("grid8").innerText==="X")
+        ||(document.getElementById("grid1").innerText==="X"&&document.getElementById("grid5").innerText==="X"&&document.getElementById("grid9").innerText==="X")
+        ||(document.getElementById("grid3").innerText==="X"&&document.getElementById("grid5").innerText==="X"&&document.getElementById("grid7").innerText==="X")
+        ){
+            winCon=true;
+            winner="X";
+            document.getElementById("winner").innerText=winner +" is the Winner";
+        }
+}
