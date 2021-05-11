a1ec4e18cc36e4720ea8104f032b7f5edf1dca3c
Tlamatina <Tassi.lamatina@gmail.com>
2021-05-10 21:51:35 -0700

    revision-from-class-plus-style

--- js/app.js
+++ js/app.js
@@ -1,112 +1,148 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-        document.getElementById("demo").addEventListener("click", function);
-    let tiles;
-    let player = "x";
-    let gameOver = false;
-    const index = (i, j) => 3 * i + j
+// DOM Content Loaded event listener
+window.addEventListener("DOMContentLoaded", () => {
+    // Capture DOM elements from page to store as vars
+    let displayResults = document.getElementById("displayResults")
+    let resetBtn = document.getElementById("reset")
+    let gameboard = document.getElementById("gameboard")
+    let displayTurn = document.getElementById("displayTurn")
+    // set intial turn message
+    displayTurn.innerText = "Brazil goes first."
 
-    const Otext = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle fill="none" cx="50" cy="50" stroke-width="6" r="45"/></svg>`
+    // Create or declare all game logic variables and conditions
+    let turnNum = 0
+    let playerB = []
+    let playerA = []
 
-    const Xtext = `<svg viewBox="0 0 130.2 130.2" xmlns="http://www.w3.org/2000/svg">
-    <line class="path line" fill="none" stroke="#167072" stroke-width="6" stroke-linecap="round" x1="34.4" y1="38" x2="95.8" y2="92.3"/>
-    <line class="path line second" fill="none" stroke="#167072" stroke-width="6" stroke-linecap="round" x1="95.8" y1="38" x2="34.4" y2="92.3"/>
-    </svg>`
+    let winCombo = [
+        ["0", "1", "2"],
+        ["3", "4", "5"],
+        ["6", "7", "8"],
+        ["0", "4", "8"],
+        ["2", "4", "6"],
+        ["2", "5", "8"],
+        ["1", "4", "7"],
+        ["0", "3", "6"]
+    ]
 
-    document.getElementsByClassName("tile").addEventListener("click", function() {
-        function setup(){
-        tiles = $(".tile")
-        }
-    })
+    // FUNCTION: boxClick() --> triggers game tile play
+    function boxClick(event) {
+        let tileCheck = event.target
 
-    tiles.forEach(tile => {
+        //// if box already played, alert user to find a new game tile to play
+        if (tileCheck.classList.contains("playedB") || tileCheck.classList.contains("playedA")) {
+            displayTurn.innerText = "Tile already played. Click again."
+        } else {
+            //// check for X or O turn and display message in displayTurn box accordingly
+            if (turnNum % 2 === 0) {
+                // LOGIC FOR PLAYER X's TURN
 
-        if (!gameOver) {
-            if ($(this).html() == "") {
-                $(this).html(player == "o" ? Otext : Xtext)
-                $(this).val(player)
-                player = player == "o" ? "x" : "o"
-                $(".turn").html(player.toUpperCase() + "'s Turn")
+                // add the playedX class to target div
+                tileCheck.classList.add("playedB")
+
+                // update innerText of target div to say X
+                tileCheck.innerText = "🇧🇷"
+
+                // update the text for player's turn to be played next (O's turn)
+                displayTurn.innerText = "Argentina's turn."
+
+                // update turnNum by 1
+                turnNum++
+
+                // push to array played tile #id
+                playerB.push(tileCheck.id)
+            } else {
+                // LOGIC FOR PLAYER O's TURN
+                tileCheck.classList.add("playedA")
+                tileCheck.innerText = "🇦🇷"
+                displayTurn.innerText = "Brazil's turn."
+                turnNum++
+
+                // push play id
+                playerA.push(tileCheck.id)
             }
-            if (winner()) {
-                $(".winner").html("Winner: " + winner().toUpperCase())
-                gameOver = true;
+            //// if greater than 4 turns then check to see if game has been won
+            if (turnNum >= 4) {
+                gameOver()
             }
-        }
-    });
 
-    document.getElementById("reset").addEventListener("click", function() {
-    function reset() {
-        gameOver = false
-        player = "x"
-        for (let t of tiles) {
-            t = $(t)
-            t.val("")
-            t.html("")
-        }
-        $(".winner").html("")
-        $(".turn").html("X's Turn")
-    }
+            //// check and see if there have been x9 turns but no winner; call tied game
+            if (turnNum === 9) {
+                gameOver()
 
-    function getvals() {
-        let r = []
-        for (let t of tiles) {
-            r.push($(t).val())
-        }
-        return r
-    }
-    
-
-    function winner() {
-        const board = getvals()
-
-        //first diagonal check
-        let first = board[0]
-        let diagonal = first != ""
-        for (let i = 0; i < 3; i++) {
-            if (board[index(i, i)] != first) {
-                diagonal = false
-                break
-            }
-        }
-        if (diagonal) return first
-
-        //second diagonal check
-        first = board[index(0, 2)]
-        let back_diag = first != ""
-        for (let i = 1; i < 4; i++) {
-            if (board[index(i - 1, 3 - i)] != first) {
-                back_diag = false
-                break
-            }
-        }
-        if (back_diag) return first
-
-        //horizontal and vertical checks
-        for (let i = 0; i < 3; i++) {
-            first = board[index(i, 0)]
-            let sideways = first != ""
-            for (let j = 0; j < 3; j++) {
-                if (board[index(i, j)] != first) {
-                    sideways = false
-                    break
+                if (displayTurn.innerText !== "") {
+                     displayResults.innerText = "GAME TIED"
+                    displayTurn.innerText = "PLAY AGAIN"
+                    stopGame()
                 }
-            }
-            if (sideways) return first
-        }
 
-        for (let i = 0; i < 3; i++) {
-            first = board[index(0, i)]
-            let sideways = first != ""
-            for (let j = 0; j < 3; j++) {
-                if (board[index(j, i)] != first) {
-                    sideways = false
-                    break
-                }
             }
-            if (sideways) return first
         }
-
-        return board.filter(v => v == "").length == 0 ? "tie" : undefined;
     }
+    // add event listener on user click of game tile
+    gameboard.addEventListener("click", boxClick)
+
+    // FUNCTION: gameOver() --> checks if winner of game
+    //// check and see if there is a winner on every turn
+    function gameOver() {
+        // loop through all possible win combos (winCombo array)
+        for (let i = 0; i < winCombo.length; i++) {
+          // console.log(`Current array: ${winCombo[i]}`)
+      
+          // if user has had any matches for x or O so far AKA declare count for x and O
+          let winFoundB = 0
+          let winFoundA = 0
+          // loop through individual win combo
+          for (let y = 0; y < winCombo[i].length; y++) {
+            //console.log(`Current id: ${winCombo[i][y]}`)
+            if (playerB.includes(winCombo[i][y])){
+              winFoundB++
+              if (winFoundB === 3) {
+                ////// send message to displayResults div and displayTurn div
+                displayResults.innerText = "Brazil has won the game!"
+                displayTurn.innerText = ""
+                //// if player has won, run stopGame()
+                stopGame()
+              }
+            }
+      
+            if (playerA.includes(winCombo[i][y])) {
+              winFoundA++
+              if (winFoundA === 3) {
+                displayResults.innerText = "Argentina has won the game!"
+                displayTurn.innerText = ""
+                stopGame()
+              }
+            }
+          }
+        }
+      }
+
+    // FUNCTION: stopGame() --> doesn't allow for further game play once game won
+    function stopGame() {
+        gameboard.removeEventListener("click", boxClick)
+    }
+
+    // FUNCTION: resetGameboard() --> add event listener to #reset button on click
+    resetBtn.addEventListener("click", function resetGameboard() {
+        
+        // loop through all gameboard tiles and remove any classes that are not gameTile
+        let gameTiles = document.getElementsByClassName("gameTile")
+
+        for (let i = 0; i <gameTiles.length; i++) {
+            gameTiles[i].classList = "gameTile" 
+            gameTiles[i].innerText = ""
+        }
+
+        // update player turn message as if game has re-started; player x plays first
+        displayTurn.innerText = "Brazil goes first."
+        displayResults.innerText = ""
+
+        // re-set all starting variables to initial values
+         turnNum = 0
+         playerB = []
+         playerA = []
+
+        // add back click event listener to gameboard
+        gameboard.addEventListener("click", boxClick)
     })
 })
